f89eb556c2c914fd94a5e9568feaa8ab79682394
mdiallo98 <mamadou1237@gmail.com>
2022-03-26 23:54:24 -0400

    stack implementation push done

--- DataStructure/Stack.py
+++ DataStructure/Stack.py
@@ -10,4 +10,18 @@ class Node:
 
 class MyStackLinkedList:
 
-    def __init__(self, data):
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def push(self, data):
+        newItem = Node(data)
+        if self.head is None:
+            self.head = newItem
+            self.tail = newItem
+
+        else:
+            self.tail.next = newItem
+            self.tail = newItem
+            self.length += 1
