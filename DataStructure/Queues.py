3607df703cb94257378eb190eac7475f697c5718
mdiallo98 <mamadou1237@gmail.com>
2022-03-28 22:54:36 -0400

    dequeue remove changed

--- DataStructure/Queues.py
+++ DataStructure/Queues.py
@@ -2,14 +2,17 @@
 
 # FIFO: First In, First out.
 
+from re import M
+
+
 class QueueArray:
     def __init__(self):
         self.items = []
 
-    def append_elem(self, data):
+    def enqueue(self, data):
         self.items.append(data)
 
-    def remove(self):
+    def denqueue(self):
         item = self.items[0]
         self.items.pop(0)
         return item
@@ -18,14 +21,19 @@ class QueueArray:
         for i in self.items:
             print(i)
 
+    def is_empty(self):
+        if self.items == []:
+            return True
+        return False
+
 
 myQueue = QueueArray()
-myQueue.append_elem("first added: 1")
-myQueue.append_elem(45)
-myQueue.append_elem(123)
-myQueue.append_elem(78)
-myQueue.append_elem(5)
-myQueue.append_elem('last added: 0')
+myQueue.enqueue("first added: 1")
+myQueue.enqueue(45)
+myQueue.enqueue(123)
+myQueue.enqueue(78)
+myQueue.enqueue(5)
+myQueue.enqueue('last added: 0')
 myQueue.print_items()
 myQueue.remove()
 # myQueue.remove()
@@ -33,4 +41,5 @@ myQueue.remove()
 # myQueue.remove()
 # myQueue.remove()
 print('AFTER REMOVE METHODE')
+print(myQueue.is_empty())
 myQueue.print_items()
