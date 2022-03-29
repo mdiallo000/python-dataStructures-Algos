5c53448514d719d54df926b6be02d98091e83146
mdiallo98 <mamadou1237@gmail.com>
2022-03-28 22:36:09 -0400

    queue imple

--- LeetcodeQuestions/JuniorQuestions/ValidParenthesis.py
+++ LeetcodeQuestions/JuniorQuestions/ValidParenthesis.py
@@ -1,11 +1,31 @@
 
 # Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 
+# To solve this problem we can use both a stack and a dictionary
+# The dictionary will keep track of the brackets
 class Solution:
     def isValid(self, s: str):
-        index = 0
-        symbol = s[index]
-        my_char = '('
-        if my_char is symbol:
-            return True
-        return False
+        # s= ex:'(){}[]' 
+      
+        stack = []
+        closed_brackets = {
+            ")" :"(",
+            "]" : "[",
+            "}" : "{"
+        }
+        
+        for char in s:
+         
+         if char in closed_brackets:
+            
+            if stack and stack[-1] == closed_brackets[char]:
+                stack.pop()
+            
+            else: return False
+         
+         else:
+             stack.append(char)
+    
+        return True if stack == [] else False 
+            
+            
