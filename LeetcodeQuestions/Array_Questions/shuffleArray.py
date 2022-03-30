9364aea77ec870b684c27c73317a9a5c4f61360a
mdiallo98 <mamadou1237@gmail.com>
2022-03-29 22:18:43 -0400

    reverse arr

--- LeetcodeQuestions/Array_Questions/shuffleArray.py
+++ LeetcodeQuestions/Array_Questions/shuffleArray.py
@@ -2,11 +2,8 @@
 
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        for i in nums:
-            left = i+1
-            right = len(nums)-1
-            while left < right:
-                nums[left], nums[right] = nums[right], nums[left]
-                left += 1
-                right -= 1
-        return nums
+        result = []
+        for i in range(n):
+            result.append(nums[i])
+            result.append(nums[i+n])
+        return result
