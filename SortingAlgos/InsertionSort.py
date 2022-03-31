699ba75a25572333d491532772536968dad6bd42
mdiallo98 <mamadou1237@gmail.com>
2022-03-31 05:02:59 -0400

    insertion sort changed

--- SortingAlgos/InsertionSort.py
+++ SortingAlgos/InsertionSort.py
@@ -1,10 +1,15 @@
 
 def selection_sort(arr: int):
     for i in range(1, len(arr)):
-        key = arr[i+1]
+        key = arr[i]
         j = i-1
-        while j > 0 and arr[j] > key:
+        while j >= 0 and arr[j] > key:
             arr[j+1] = arr[j]
             j -= 1
         arr[j+1] = key
     return arr
+
+
+arr = [45, 78, 12, 66, 45, 79, 2, 31, 45, 9]
+
+print(selection_sort(arr))
