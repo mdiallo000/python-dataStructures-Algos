025d6b3d1a5a3b06e29bc3a2e05f12534a228a0c
mdiallo98 <mamadou1237@gmail.com>
2022-04-01 04:13:49 -0400

    binary tree

--- DataStructure/Binary_Tree.py
+++ DataStructure/Binary_Tree.py
@@ -4,3 +4,9 @@ class BinaryTree:
     def __init__(self, data, children=[]):
         self.data = data
         self.children = children
+
+    def __str__(self, level=0):
+        ret = " " * level + str(self.data) + '\n'
+        for child in self.children:
+            ret += child.__str__(level+1)
+        return ret
