7ee3e768cbc8a7a28a07a5d642d392ca7d8a77fe
mdiallo98 <mamadou1237@gmail.com>
2022-04-01 04:11:33 -0400

    binary tree

--- DataStructure/Binary_Tree.py
+++ DataStructure/Binary_Tree.py
@@ -1,4 +1,6 @@
 #  A binary search Tree is an hierichal abstract data structure which offers some Unique benefits.
 
 class BinaryTree:
-    def __init__(self, data, childer=[]):
+    def __init__(self, data, children=[]):
+        self.data = data
+        self.children = children
