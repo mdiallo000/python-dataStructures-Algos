f90679a3bdfe60a86ff3efbd5e7849ea16e45f67
mdiallo98 <mamadou1237@gmail.com>
2022-04-02 05:02:12 -0400

    anagram

--- LeetcodeQuestions/String_Problems/valid_anagram.py
+++ LeetcodeQuestions/String_Problems/valid_anagram.py
@@ -2,19 +2,23 @@
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # my_collection = {}
-        # for char in s:
-        #     my_collection[char] = 0
-
-        # for T in t:
-        #     if T in my_collection:
-        #         return True
-        # return False
-        my_set = ()
+        if len(s) != len(t):
+            return False
+        my_collection = {}
         for char in s:
-            my_set.add(char)
+            my_collection[char] = 0
 
         for T in t:
-            if T in my_set:
+            if T in my_collection:
                 return True
+            else:
+                return False
         return False
+        # my_set = ()
+        # for char in s:
+        #     my_set.add(char)
+
+        # for T in t:
+        #     if T in my_set:
+        #         return True
+        # return False
