9e4796a2692dc35e5b9a93fb9dd8a410c1cc4044
mdiallo98 <mamadou1237@gmail.com>
2022-04-02 06:35:31 -0400

    palindrome

--- LeetcodeQuestions/String_Problems/is_palindromic.py
+++ LeetcodeQuestions/String_Problems/is_palindromic.py
@@ -8,13 +8,16 @@ from hashlib import new
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-       allowedWord = '0123456789qwertyuiopasdfghjklzxcvbnm'
-       newStr = ''
-       for char in s:
-           if char not in allowedWord:
-              continue
-           else:
+        allowedWord = '0123456789qwertyuiopasdfghjklzxcvbnm'
+        newStr = ''
+        for char in s:
+            if char not in allowedWord:
+                continue
+            else:
                 newStr += char
 
         left = 0
-        right= len(s)
+        right = len(s)
+        while left < right:
+            if s[left] != s[right]:
+                return False
