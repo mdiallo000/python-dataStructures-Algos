dc832e840baaa1f372c42f016950a92a92df2eb1
mdiallo98 <mamadou1237@gmail.com>
2022-04-02 19:34:09 -0400

    palindrome

--- LeetcodeQuestions/String_Problems/valid_anagram.py
+++ LeetcodeQuestions/String_Problems/valid_anagram.py
@@ -2,28 +2,7 @@
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        my_collection = {}
-        for char in s:
-            my_collection[char] = 0
 
-        for T in t:
-            if T in my_collection:
-                return True
-            else:
-                return False
-        return False
-
-    def is_Valid(self, s: str, t: str) -> bool:
-        s = ''.join(sorted(s))
-        t = ''.join(sorted(t))
-        for char in s:
-            if T in t == char:
-                return True
-        return False
-
-    def valid_anagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
         s_dict = {}
