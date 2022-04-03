73fc5e86ff144a15ebca09496b8da919029164f0
mdiallo98 <mamadou1237@gmail.com>
2022-04-03 06:08:44 -0400

    palindrome for linked list

--- LeetcodeQuestions/linked_list/Palimdrome_linked_list.py
+++ LeetcodeQuestions/linked_list/Palimdrome_linked_list.py
@@ -20,4 +20,17 @@ class Solution:
             r -= 1
         return True
 
+    def efficient_Palindrome(self, head):
+
+        fast, slow = head, head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        prev = None
+        curr = slow
+        while curr:
+            tmp = curr.next
+            curr.next = None
 # This solution gets it done but it is has a bIG O(n) time complexity and a Space complexity O(n) since we are using extra memory for our Stack and its size may increase as our list Updates.
