a0b9b6f34c3345abc6286077b7a00b5ebe842e77
mdiallo98 <mamadou1237@gmail.com>
2022-04-04 05:53:50 -0400

    group anagrams

---
+++ LeetcodeQuestions/Hash_Table_Dict/Group_anagrams.py
@@ -0,0 +1,23 @@
+# Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+# Input: strs = ["eat","tea","tan","ate","nat","bat"]
+# Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+# Input: strs = [""]
+# Output: [[""]]
+
+# Input: strs = ["a"]
+# Output: [["a"]]
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashmap = {}
+
+        for elem in strs:
+            key = ''.join(sorted(elem))
+            if key in hashmap:
+                hashmap[key].append(elem)
+            else:
+                hashmap[key] = []
+                hashmap[key].append(elem)
+        return list(hashmap.values())
