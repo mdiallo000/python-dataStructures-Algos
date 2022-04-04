a712b4c38d2766ad82db5dce722e133a9def507f
mdiallo98 <mamadou1237@gmail.com>
2022-04-04 07:18:30 -0400

    two sum 2

---
+++ LeetcodeQuestions/Hash_Table_Dict/Two_SumII.py
@@ -0,0 +1,14 @@
+# Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+
+        myMap = {}
+
+        for i in range(len(numbers)):
+            curr = numbers[i]
+            difference = target - curr
+            if difference in myMap:
+                return [i+1, myMap[difference] + 1]
+            else:
+                myMap[curr] = i
