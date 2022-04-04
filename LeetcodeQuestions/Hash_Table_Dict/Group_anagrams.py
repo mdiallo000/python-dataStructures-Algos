abc4af578ac2c260a6fa92f0b49fc5946aeb6b1f
mdiallo98 <mamadou1237@gmail.com>
2022-04-04 06:15:21 -0400

    group anagrams

--- LeetcodeQuestions/Hash_Table_Dict/Group_anagrams.py
+++ LeetcodeQuestions/Hash_Table_Dict/Group_anagrams.py
@@ -11,13 +11,18 @@
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        #  Initiate a HashMap
         hashmap = {}
-
+        # For each element in our array we will take an element sort it and join it back together ex: for "eat" key will be aet, for "ant" key will be ant so on
         for elem in strs:
             key = ''.join(sorted(elem))
+        #  Now for each key we will check if its not in our hashMap, since its not we will first initialize an array and then append it its corresponding element into the array.
+        #  The insight here is that since anagrams are have the same letter and frequency we will used the Keys as a way of inserting each elem in the correct array
             if key not in hashmap:
                 hashmap[key] = []
                 hashmap[key].append(elem)
             else:
+                #  If we already encountered the key then we just put the corresponding elem into the correct list so what we get is {"aet": ['eat', 'tea', aet]} and so on for each key
                 hashmap[key].append(elem)
+        # Finally we put all the values of our hashmap into an array giving us the desired outcome [["bat"],["nat","tan"],["ate","eat","tea"]]
         return list(hashmap.values())
