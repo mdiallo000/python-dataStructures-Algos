4c60b03e2b3bf5262a631f87804d3b75c0ccfada
mdiallo98 <mamadou1237@gmail.com>
2022-04-04 02:40:51 -0400

    duplicates brute force

--- LeetcodeQuestions/Array_Questions/contains_duplicate.py
+++ LeetcodeQuestions/Array_Questions/contains_duplicate.py
@@ -7,10 +7,33 @@
 # Output: false
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        my_set = {}
+        my_set = set()
         for elem in nums:
             if elem not in my_set:
                 my_set.add(elem)
             else:
                 return True
+        return False
+
+    def using_hashmap(self, nums: List[int]) -> bool:
+        hash_map = {}
+
+        for elem in nums:
+            if elem not in hash_map:
+                hash_map[elem] = 1
+            else:
+                hash_map[elem] += 1
+
+        for key in hash_map:
+            if hash_map[key] > 1:
+                return True
+        return False
+
+    def brute_force(self, nums: List[int]) -> bool:
+
+        for i in range(len(nums)-1):
+            elem = nums[i]
+            for j in range(i+1, len(nums)):
+                if elem == nums[j]:
+                    return True
         return False
