b03632f24bcd5c8c86b049c0d6e3f4a7fc2eaa64
mdiallo98 <mamadou1237@gmail.com>
2022-04-05 08:45:07 -0400

    top k frequent element

--- LeetcodeQuestions/Hash_Table_Dict/top_k_frequent_element.py
+++ LeetcodeQuestions/Hash_Table_Dict/top_k_frequent_element.py
@@ -1,4 +1,12 @@
-
+# Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+        myHashMap = {}
+
+        for elem in nums:
+            if elem not in myHashMap:
+                myHashMap[elem] = 1
+            else:
+                myHashMap[elem] += 1
