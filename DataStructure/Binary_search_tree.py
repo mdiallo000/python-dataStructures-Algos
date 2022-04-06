785815988d154dc81c121e46a9b6dbda84a6f654
mdiallo98 <mamadou1237@gmail.com>
2022-04-06 06:41:41 -0400

    binary search tree

--- DataStructure/Binary_search_tree.py
+++ DataStructure/Binary_search_tree.py
@@ -9,3 +9,25 @@ class Node:
 
 
 class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert_Node(self, data):
+        if self.root is None:
+            self.root = Node(data)
+        else:
+            self._insert(data, self.root)
+
+    def _insert(self, data, cur_node):
+
+        if data < cur_node.data:
+            if cur_node.left_child is None:
+                cur_node.left_child = Node(data)
+            else:
+                self._insert(data, cur_node.left_child)
+
+        elif data > cur_node.data:
+            if cur_node.right_child is None:
+                cur_node.right_child = Node(data)
+            else:
+                self._insert(data, cur_node.right_child)
