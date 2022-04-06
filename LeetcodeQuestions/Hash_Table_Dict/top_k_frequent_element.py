f0054d60a3608f54056cac6c3072ea9a255a0052
mdiallo98 <mamadou1237@gmail.com>
2022-04-06 04:00:17 -0400

    top k frequent element

--- LeetcodeQuestions/Hash_Table_Dict/top_k_frequent_element.py
+++ LeetcodeQuestions/Hash_Table_Dict/top_k_frequent_element.py
@@ -4,9 +4,15 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
 
         myHashMap = {}
-
+        store = []
+        if len(nums) == 1:
+            return store.append(nums)
         for elem in nums:
             if elem not in myHashMap:
                 myHashMap[elem] = 1
             else:
                 myHashMap[elem] += 1
+        for key in myHashMap:
+            if myHashMap[key] > 1:
+                store.append(key)
+        return store
