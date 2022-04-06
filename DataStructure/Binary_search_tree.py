4cdd658915b05b6b045823425a106298b3330ef0
mdiallo98 <mamadou1237@gmail.com>
2022-04-06 07:09:47 -0400

    binary search tree

--- DataStructure/Binary_search_tree.py
+++ DataStructure/Binary_search_tree.py
@@ -33,3 +33,30 @@ class BST:
                 self._insert(data, cur_node.right_child)
         else:
             print("No Duplicates Allowed")
+
+    def find_Node(self, data):
+        if self.root:
+            is_found = self._find(data, self.root)
+            if is_found:
+                return True
+            return False
+        else:
+            return None
+
+    def _find(self, data, cur_node):
+        if data > cur_node.data and cur_node.right_child:
+            return self._find(data, cur_node.right_child)
+        elif data < cur_node.data and cur_node.left_child:
+            return self._find(data, cur_node.left_child)
+        if data == cur_node.data:
+            return True
+
+
+dummy = BST()
+dummy.insert_Node(78)
+dummy.insert_Node(12)
+dummy.insert_Node(45)
+dummy.insert_Node(10)
+dummy.insert_Node(42)
+dummy.insert_Node(8)
+print(dummy.find_Node(89))
