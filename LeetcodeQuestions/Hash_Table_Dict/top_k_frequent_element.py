bb1be192f0f163e29d59b86390a29817390bf457
mdiallo98 <mamadou1237@gmail.com>
2022-04-06 05:18:05 -0400

    binary search tree

--- LeetcodeQuestions/Hash_Table_Dict/top_k_frequent_element.py
+++ LeetcodeQuestions/Hash_Table_Dict/top_k_frequent_element.py
@@ -1,15 +1,15 @@
 # Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
 
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    # def topKFrequent(self, nums: List[int], k: int) -> List[int]:
 
-        myHashMap = {}
-        store = []
-        # if len(nums) == 1:
-        #     return store.append(nums)
-        for elem in nums:
-            if elem not in myHashMap:
-                myHashMap[elem] = 1
-            else:
-                myHashMap[elem] += 1
-        for key in myHashMap:
+    #     myHashMap = {}
+    #     store = []
+    #     # if len(nums) == 1:
+    #     #     return store.append(nums)
+    #     for elem in nums:
+    #         if elem not in myHashMap:
+    #             myHashMap[elem] = 1
+    #         else:
+    #             myHashMap[elem] += 1
+    #     for key in myHashMap:
