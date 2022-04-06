3f928c7eb626e95a3ec4094e03dc8122789547f5
mdiallo98 <mamadou1237@gmail.com>
2022-04-06 04:31:14 -0400

    improved twoSum2

--- LeetcodeQuestions/Hash_Table_Dict/Two_SumII.py
+++ LeetcodeQuestions/Hash_Table_Dict/Two_SumII.py
@@ -12,3 +12,14 @@ class Solution:
                 return [myMap[difference] + 1, i+1]
             else:
                 myMap[curr] = i
+
+    def with_Binary_search(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers)-1
+
+        while l < r:
+            if numbers[l] + numbers[r] == target:
+                return (l+1, r+1)
+            elif numbers[l] + numbers[r] < target:
+                l += 1
+            else:
+                r -= 1
