3c40f5c81f430c1bd05793560c04c0ca47c0f010
mdiallo98 <mamadou1237@gmail.com>
2022-04-08 04:17:28 -0400

    string permutation

---
+++ LeetcodeQuestions/SlidingWindow/Permutation_in_string.py
@@ -0,0 +1,10 @@
+# Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+
+# In other words, return true if one of s1's permutations is the substring of s2.
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        l, r = 0, 1
+
+        while r <= len(s2):
