b75697f530d9a536423853e80729d69a57f09b36
mdiallo98 <mamadou1237@gmail.com>
2022-04-09 07:12:01 -0400

    product array except itself

--- LeetcodeQuestions/linked_list/merge_two_linked_list.py
+++ LeetcodeQuestions/linked_list/merge_two_linked_list.py
@@ -8,12 +8,17 @@ class Solution:
 
         dummy = ListNode()
 
-        while list1 and list2:
-            if list1 < list2:
+        while list1.val and list2.val:
+            if list1.val < list2.val:
                 dummy.next = list1
-            elif list2 < list1:
-                dummy.next = list2
+                list1 = list.next
             else:
-                dummy.next = list1
                 dummy.next = list2
+                list2 = list2.next
+            dummy = dummy.next
+        if list1:
+            dummy.next = list1
+        if list2:
+            dummy.next = list2
+
         return dummy
