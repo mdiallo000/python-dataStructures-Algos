717f937a02ad83603fed8fc6805c22f0e212ac75
mdiallo98 <mamadou1237@gmail.com>
2022-04-10 03:54:14 -0400

    add two numbers

---
+++ LeetcodeQuestions/linked_list/add_two_numbers.py
@@ -0,0 +1,5 @@
+# You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
