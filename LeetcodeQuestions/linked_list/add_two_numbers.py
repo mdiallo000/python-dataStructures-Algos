8384787c78d8748b2247ad3a760a7559cb3b2ecf
mdiallo98 <mamadou1237@gmail.com>
2022-04-10 04:14:38 -0400

    add two numbers

--- LeetcodeQuestions/linked_list/add_two_numbers.py
+++ LeetcodeQuestions/linked_list/add_two_numbers.py
@@ -1,5 +1,16 @@
 # You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
 
 
+from multiprocessing import dummy
+
+
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy_link = ListNode()
+        tail = dummy_link
+
+        while l1 and l2:
+            tail.next = l1.val + l2.val
+            l1 = l1.next
+            l2 = l2.next
+        return dummy_link.next
