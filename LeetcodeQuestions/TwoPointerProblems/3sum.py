59316681acf9fd99f31b147186ed2e36a07e00d4
mdiallo98 <mamadou1237@gmail.com>
2022-04-12 08:21:30 -0400

    3sum

--- LeetcodeQuestions/TwoPointerProblems/3sum.py
+++ LeetcodeQuestions/TwoPointerProblems/3sum.py
@@ -5,9 +5,15 @@ class Solution:
         # Sort the array and now we can apply the same technique we used in 2sumTwo
         nums.sort()
 #
-        for index, curr in enumerate(nums):
+        for cur_index, curr in enumerate(nums):
             #       now we can make sure that we arent looking at duplicates since its sorted, a duplacte means if the curr elem is the same as its next
-            if index > 0 and curr == nums[index - 1]:
+            if cur_index > 0 and curr == nums[index - 1]:
                 continue
 
             # Now that we have skipped the duplicate we can began using our Two pointer method
+            l, r = cur_index + 1, len(nums)-1
+            while l < r:
+                sum3 = curr + nums[l] + nums[r]
+                if sum3 < 0:
+                    l += 1
+                elif sum3
