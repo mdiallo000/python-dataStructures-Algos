45a0b2d19c89d0d36a18156d376595bb65bb07e2
mdiallo98 <mamadou1237@gmail.com>
2022-04-13 05:00:36 -0400

    find duplicate number

--- LeetcodeQuestions/linked_list/find_duplicate_number.py
+++ LeetcodeQuestions/linked_list/find_duplicate_number.py
@@ -8,10 +8,18 @@ class Solution:
         #         if curr == elem_ahead:
         #             return curr
 
-        mySet = set()
+        # mySet = set()
+
+        # for elem in nums:
+        #     if elem not in mySet:
+        #         mySet.add(elem)
+        #     else:
+        #         return elem
+        myMap = {}
+        for elem in nums:
+            if elem in myMap:
+                myMap[elem] += 1 or 1
 
         for elem in nums:
-            if elem not in mySet:
-                mySet.add(elem)
-            else:
+            if myMap[elem] > 1:
                 return elem
