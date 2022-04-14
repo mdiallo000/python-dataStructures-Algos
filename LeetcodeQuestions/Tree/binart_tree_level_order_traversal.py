7b55efe3b4c475358797e898e4ffa291f555867a
mdiallo98 <mamadou1237@gmail.com>
2022-04-14 09:35:20 -0400

    Binary search tree

--- LeetcodeQuestions/Tree/binart_tree_level_order_traversal.py
+++ LeetcodeQuestions/Tree/binart_tree_level_order_traversal.py
@@ -10,3 +10,13 @@ class Solution:
 
         while queue is not None:
             #  measure the length of our queue currently, we will use this to loop through the elements at the current level only
+            lenght_queue = len(queue)
+            # at each level we will store the nodes into a list
+            levels = []
+            for i in range(lenght_queue):
+                #  now we store the node currently at the front of the queue by poping it out
+                curr_node = queue.popleft()
+            # Once we pop it out we can store it into the levels array and then we will add its left and child node into the queue
+                levels.append(curr_node.val)
+                queue.append(curr.node.left)
+                queue.append(curr.node.right)
