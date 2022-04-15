1a3f9d41d07d8aa8d2ac9e972011ee588b3cf905
mdiallo98 <mamadou1237@gmail.com>
2022-04-15 15:05:47 -0400

    max depth

--- LeetcodeQuestions/Tree/max_depth_of_binary_tree.py
+++ LeetcodeQuestions/Tree/max_depth_of_binary_tree.py
@@ -19,7 +19,8 @@ class Solution:
         return 1 + max(left, right)
 
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-
+        if not root:
+            return []
         result = []
         queue = collections.deque()
         queue.append(root)
