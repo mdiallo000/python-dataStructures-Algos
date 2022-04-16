2a7f5fef1bf993c31bdf325ac3b21c06d777ca70
mdiallo98 <mamadou1237@gmail.com>
2022-04-15 21:50:41 -0400

    diameter binary tree

---
+++ LeetcodeQuestions/Tree/diameter_binary_tree.py
@@ -0,0 +1,14 @@
+
+
+from turtle import right
+
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        res = [0]
+
+        def dfs(root):
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            res[0] = max(res[0], left, right)
