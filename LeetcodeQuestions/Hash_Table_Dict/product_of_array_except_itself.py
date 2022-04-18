8689046a35ea78e086d20e88f7469c70f7710b0a
mdiallo98 <mamadou1237@gmail.com>
2022-04-18 15:52:29 -0400

    top k frequent element

--- LeetcodeQuestions/Hash_Table_Dict/product_of_array_except_itself.py
+++ LeetcodeQuestions/Hash_Table_Dict/product_of_array_except_itself.py
@@ -5,3 +5,11 @@ class Solution:
         result = [1] * len(nums)
 
         prefix = 1
+        for i in range(len(nums)):
+            result[i] = prefix
+            prefix *= nums[i]
+
+        postfix = 1
+        for i in range(len(nums)-1, -1, -1):
+            result[i] *= postfix
+            postfix *= nums[i]
