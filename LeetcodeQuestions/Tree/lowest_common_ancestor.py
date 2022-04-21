0af4673d9f9b01ad7926f938003b7d5cc94a629d
mdiallo98 <mamadou1237@gmail.com>
2022-04-20 23:35:17 -0400

    lowest common ancestor

--- LeetcodeQuestions/Tree/lowest_common_ancestor.py
+++ LeetcodeQuestions/Tree/lowest_common_ancestor.py
@@ -2,3 +2,7 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+
+        if root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.left)
+        elif root.val > p.val and root.val > q.val:
