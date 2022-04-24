1feff8d0217523c2d8988bf4ddc93e6ae35ed4aa
mdiallo98 <mamadou1237@gmail.com>
2022-04-24 15:51:25 -0400

    longestchar substring replacement

--- LeetcodeQuestions/SlidingWindow/longest_reapr_char_replacement.py
+++ LeetcodeQuestions/SlidingWindow/longest_reapr_char_replacement.py
@@ -1,2 +1,12 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+
+        r, l, count = 0
+        charMap = {}
+
+        while r < len(s)-1:
+            char = s[r]
+            if char not in charMap:
+                charMap[char] = 1
+            else:
+                charMap[char] += 1
