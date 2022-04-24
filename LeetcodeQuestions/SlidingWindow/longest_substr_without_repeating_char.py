5bbafa942e5b2805d80a51246c27b830c10206c4
mdiallo98 <mamadou1237@gmail.com>
2022-04-24 14:15:58 -0400

    longestchar substring without repeat

--- LeetcodeQuestions/SlidingWindow/longest_substr_without_repeating_char.py
+++ LeetcodeQuestions/SlidingWindow/longest_substr_without_repeating_char.py
@@ -1,4 +1,21 @@
 
 
+from email import charset
+
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+
+        charSet = set()
+        l, r = 0
+        sub_string_count = 0
+        # abcabc
+        # l  r
+        #   [a b c ]
+        for r in range(len(s)):
+
+            while s[r] in charSet:
+                charset.remove(s[l])
+                l += 1
+            sub_string_count = max(sub_string_count, r-l + 1)
+        return sub_string_count
