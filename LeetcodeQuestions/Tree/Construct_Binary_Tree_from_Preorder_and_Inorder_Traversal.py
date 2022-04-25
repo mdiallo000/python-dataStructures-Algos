ec0e79abad1a75bfe1761a8df932aae1096bf43f
mdiallo98 <mamadou1237@gmail.com>
2022-04-25 18:31:31 -0400

    reconstruct from in and pre

--- LeetcodeQuestions/Tree/Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.py
+++ LeetcodeQuestions/Tree/Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.py
@@ -12,11 +12,13 @@ class Solution:
 
         # Lets find the root in our Preodered list since the first element [0] is the root
 
-        root = preorder[0]
+        root = TreeNode(preorder[0])
         # Now lets find all the elments to the left of our root and to the right of it
 
-        mid = inorder.index(root)
+        mid = inorder.index(preorder[0])
 
         #  now we can reconstruct the tree starting from the root, we will built left side first
         root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
         root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+
+        return root
