53464e98303d6cf2d3eb006f594face8981ab9de
mdiallo98 <mamadou1237@gmail.com>
2022-04-25 18:26:00 -0400

    reconstruct from in and pre

---
+++ LeetcodeQuestions/Tree/Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+
+        if not preorder and not inorder:
+            return None
+
+        # Lets find the root in our Preodered list since the first element [0] is the root
+
+        root = preorder[0]
+        # Now lets find all the elments to the left of our root and to the right of it
+
+        mid =
