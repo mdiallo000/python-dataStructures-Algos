12e1b54ef2be3c6b1af179e975c60bc2f3112bd2
mdiallo98 <mamadou1237@gmail.com>
2022-04-26 13:52:17 -0400

    sub tree

--- LeetcodeQuestions/Tree/subtree_of_another_tree.py
+++ LeetcodeQuestions/Tree/subtree_of_another_tree.py
@@ -3,3 +3,15 @@
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         # we need to validate first wether where we currently are
+        if not subRoot:
+            return True
+        if not root:
+            return False
+
+        def isSame(p, t):
+            if not p and t:
+                return True
+
+            if p and t and t.val == p.val:
+                return self.isSame(p.left, t.left) and self.isSame(p.right, t.right)
+            return False
