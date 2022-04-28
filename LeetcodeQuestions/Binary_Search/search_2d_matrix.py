414955e8d444ceb2380a6bfba7c20a50e52a172b
mdiallo98 <mamadou1237@gmail.com>
2022-04-28 18:45:08 -0400

    2d matrix

--- LeetcodeQuestions/Binary_Search/search_2d_matrix.py
+++ LeetcodeQuestions/Binary_Search/search_2d_matrix.py
@@ -1,17 +1,33 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
 
-        row_size = 0
-        col_size = len(matrix[0]) - 1
+        row_pos = 0
+        colum_pos = len(matrix[0]) - 1
 
-        while(col_size >= 0 and row_size <= len(matrix)-1):
-            if matrix[row_size][col_size] < target:
-                row_size += 1
+        while colum_pos >= 0 and row_pos <= len(matrix) - 1:
 
-            elif matrix[row_size][col_size] > target:
-                col_size -= 1
+            if matrix[row_pos][colum_pos] > target:
+                row_pos += 1
+
+            elif matrix[row_pos][colum_pos] < target:
+                colum_pos -= 1
 
             else:
                 return True
 
         return False
+
+        # row_size = 0
+        # col_size = len(matrix[0]) - 1
+
+        # while(col_size >= 0 and row_size <= len(matrix)-1):
+        #     if matrix[row_size][col_size] < target:
+        #         row_size += 1
+
+        #     elif matrix[row_size][col_size] > target:
+        #         col_size -= 1
+
+        #     else:
+        #         return True
+
+        # return False
