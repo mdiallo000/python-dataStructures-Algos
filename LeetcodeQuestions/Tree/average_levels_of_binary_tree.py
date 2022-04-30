5988bd099103718b52c6acf9d9d4fdd53354008f
mdiallo98 <mamadou1237@gmail.com>
2022-04-30 11:11:48 -0400

    average levels

--- LeetcodeQuestions/Tree/average_levels_of_binary_tree.py
+++ LeetcodeQuestions/Tree/average_levels_of_binary_tree.py
@@ -1,4 +1,24 @@
 
 
+import collections
+from gc import collect
+
+
 class Solution:
     def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+
+        res = []
+        queue = collections.deque()
+        queue.append(root)
+
+        while queue:
+            queue_length = len(queue)
+            sum_level = 0
+            for i in range(queue_length):
+                node = queue.popleft()
+                if node:
+                    sum_level += node.val
+                    queue.append(node.left)
+                    queue.append(node.right)
+            res.append(sum_level//queue_length)
+        return res
