94129d5fe0a4bfc63410aa3383495c85d053aaeb
mdiallo98 <mamadou1237@gmail.com>
2022-04-29 21:40:42 -0400

    target indices

--- LeetcodeQuestions/Binary_Search/find-targ_indices_after_sorting.py
+++ LeetcodeQuestions/Binary_Search/find-targ_indices_after_sorting.py
@@ -2,3 +2,6 @@
 # A target index is an index i such that nums[i] == target.
 
 # Return a list of the target indices of nums after sorting nums in non-decreasing order. If there are no target indices, return an empty list. The returned list must be sorted in increasing order.
+
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
