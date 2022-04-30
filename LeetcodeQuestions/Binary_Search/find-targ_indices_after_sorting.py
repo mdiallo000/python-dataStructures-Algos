abb6b2063e83207b1b7138cf0edc9b9c8e040b4e
mdiallo98 <mamadou1237@gmail.com>
2022-04-29 22:05:17 -0400

    target indices

--- LeetcodeQuestions/Binary_Search/find-targ_indices_after_sorting.py
+++ LeetcodeQuestions/Binary_Search/find-targ_indices_after_sorting.py
@@ -21,3 +21,10 @@ class Solution:
         while l < r:
             mid = (r+l)//2
             if nums[mid] == target:
+                res.append(mid)
+            elif nums[mid] > target:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        return res
