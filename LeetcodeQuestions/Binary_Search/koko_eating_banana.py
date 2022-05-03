5faf4aa58cce5e02aabf875841b085660c66d40e
mdiallo98 <mamadou1237@gmail.com>
2022-05-03 16:09:51 -0400

    find min k

--- LeetcodeQuestions/Binary_Search/koko_eating_banana.py
+++ LeetcodeQuestions/Binary_Search/koko_eating_banana.py
@@ -1,2 +1,5 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # our goal is to find the minum number of bananas that kooko can eat before the guard come back at H hours. so we will be return total time
+
+        l = 0
