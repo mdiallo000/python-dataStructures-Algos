31df2d14d87213b306db2f730a2aedfa2e9a598c
mdiallo98 <mamadou1237@gmail.com>
2022-05-03 13:37:36 -0400

    mergesort

--- SortingAlgos/MergeSort.py
+++ SortingAlgos/MergeSort.py
@@ -2,32 +2,6 @@
 
 # This Algo needs a helper function that will merge two arrays into one
 
-def merge(arrOne, arrTwo):
-    result = []
-    # for i, j in zip(arrOne, arrTwo):
-    #     if i < j:
-    #         result.append(i)
-    #     else:
-    #         result.append(j)
+def mergesort(arr):
 
-    i = 0
-    j = 0
-    arr1 = len(arrOne)-1
-    arr2 = len(arrTwo)-1
-    while j <= range(len(arrOne)-1) and i <= range(len(arrTwo)-1):
-        if arrOne[j] < arrTwo[i]:
-            result.append(arrOne[j])
-            j += 1
-        if arrOne[j] > arrTwo[i]:
-            result.append(arrTwo[i])
-            i += 1
-
-    print(result)
-    return result
-
-
-items = [3, 5, 6, 7]
-itmes2 = [1, 2, 4, 8]
-
-# [1,2,4,7]
-merge(items, itmes2)
+    if len(arr) > 1:
