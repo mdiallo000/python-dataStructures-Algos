696c9f615046496cd44bfe81d906987d2df7bfd5
mdiallo98 <mamadou1237@gmail.com>
2022-05-04 23:41:23 -0400

    min stack

--- LeetcodeQuestions/Stack/Min_stack.py
+++ LeetcodeQuestions/Stack/Min_stack.py
@@ -10,15 +10,14 @@ class MinStack:
     def push(self, val: int):
         self.stack.append(val)
         val = min(val, self.minStack[-1] if self.minStack else 0)
+        self.minStack.append(val)
 
     def pop(self):
         self.stack.pop()
+        self.minStack.pop()
 
     def top(self):
-        topval = 0
-        topval = self.pop()
-        return topval
+        return self.stack.pop()
 
     def getMin(self):
-        minVal = 0
-        minVal = self.stack.pop(0)
+        return self.minStack.pop()
