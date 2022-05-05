1af8cce5577acda2c147b2473264674175ed97a0
mdiallo98 <mamadou1237@gmail.com>
2022-05-05 14:59:27 -0400

    Reverse P N

--- LeetcodeQuestions/Stack/reverse_polish_notation.py
+++ LeetcodeQuestions/Stack/reverse_polish_notation.py
@@ -10,7 +10,9 @@ class Solution:
             elif char == '-':
                 a, b = stack.pop(), stack.pop()
                 stack.append(b-a)
-            elif char == '/'
+            elif char == '/':
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(b/a))
 
             else:
                 stack.append(int(char))
