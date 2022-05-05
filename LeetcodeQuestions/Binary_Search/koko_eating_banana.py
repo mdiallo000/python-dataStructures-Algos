56c6c577cda972289567c5016f6daed2f64da316
mdiallo98 <mamadou1237@gmail.com>
2022-05-04 22:31:43 -0400

    string permutation

--- LeetcodeQuestions/Binary_Search/koko_eating_banana.py
+++ LeetcodeQuestions/Binary_Search/koko_eating_banana.py
@@ -1,5 +1,22 @@
+from math import ceil
+
+
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         # our goal is to find the minum number of bananas that kooko can eat before the guard come back at H hours. so we will be return total time
 
-        l = 0
+        l, r = 1, max(piles)
+        k = 0
+
+        while l <= r:
+            mid = (l+r)//2
+            total_time = 0
+            for pile in piles:
+                total_time = (pile - 1 // 2) + 1
+
+                if total_time <= h:
+                    k = mid
+                    r = mid - 1
+                else:
+                    l = mid + 1
+        return k
