b27dc403c115651633000fd6699a5f8ea295f131
mdiallo98 <mamadou1237@gmail.com>
2022-05-06 18:29:27 -0400

    last stone weight

--- LeetcodeQuestions/Heap-Priority-Queue/last_stone_weight.py
+++ LeetcodeQuestions/Heap-Priority-Queue/last_stone_weight.py
@@ -2,3 +2,9 @@ class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         #  very simple solution if you understand the intuition
         #  we nee to put all the stones in a max heap to help us get the largerts weighing stones on top
+        #  USE LISt comprehension to convert all the values to negatives
+        # before = [2,7,4,1,8,1]
+
+        stones = [-s for s in stones]
+
+        #after = [-2,-7,-4,-1,-8,-1]
