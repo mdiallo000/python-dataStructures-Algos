6420ba7da9b70136da70a531dbc05560bff9513c
mdiallo98 <mamadou1237@gmail.com>
2022-05-06 18:25:11 -0400

    last stone weight

--- LeetcodeQuestions/Heap-Priority-Queue/last_stone_weight.py
+++ LeetcodeQuestions/Heap-Priority-Queue/last_stone_weight.py
@@ -1,2 +1,4 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
+        #  very simple solution if you understand the intuition
+        #  we nee to put all the stones in a max heap to help us get the largerts weighing stones on top
