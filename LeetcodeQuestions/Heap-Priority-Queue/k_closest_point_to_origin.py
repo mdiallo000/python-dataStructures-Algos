357c6872fa62c1158f5087a04c5f2ffc57212b47
mdiallo98 <mamadou1237@gmail.com>
2022-05-07 23:44:12 -0400

    k closest point to origin

---
+++ LeetcodeQuestions/Heap-Priority-Queue/k_closest_point_to_origin.py
@@ -0,0 +1,7 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        #  points = [[3,3],[5,-1],[-2,4]]
+
+        pts = []
+
+        for x, y in points:
