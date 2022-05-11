fdd7012f6d653ff0314f8871d8d08b51dcf02e81
mdiallo98 <mamadou1237@gmail.com>
2022-05-10 22:36:38 -0400

    find duplicate number

--- LeetcodeQuestions/linked_list/find_duplicate_number.py
+++ LeetcodeQuestions/linked_list/find_duplicate_number.py
@@ -1,3 +1,17 @@
 
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
+
+        slow = fast = 0
+
+        while True:
+
+            slow = nums[slow]
+            fast = fast[nums[fast]]
+
+            if slow == fast:
+                break
+
+        new_slow = 0
+
+        while True:
