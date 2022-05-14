9af70d96222485a17f976afe08f7f9e99cf07cbe
mdiallo98 <mamadou1237@gmail.com>
2022-05-14 17:30:55 -0400

    has valid path

--- LeetcodeQuestions/Graphs/has_valid_path.py
+++ LeetcodeQuestions/Graphs/has_valid_path.py
@@ -1,2 +1,9 @@
+from collections import defaultdict
+from email.policy import default
+
+
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        #  first we need to create the graph based on the edges we were given
+        neighbors = defaultdict(list)
+    #  neighbors = {:[], :[]}
