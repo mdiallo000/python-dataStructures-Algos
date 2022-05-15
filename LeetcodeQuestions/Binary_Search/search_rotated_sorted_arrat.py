03c3f70cd88620b4b0e7a0175ad37c52bc289df3
mdiallo98 <mamadou1237@gmail.com>
2022-05-15 15:24:50 -0400

    search rotated sorted array

--- LeetcodeQuestions/Binary_Search/search_rotated_sorted_arrat.py
+++ LeetcodeQuestions/Binary_Search/search_rotated_sorted_arrat.py
@@ -1,2 +1,14 @@
+from tkinter import W
+
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        # [4,5,6,7,0,1,2,3] tar = 0
+
+        l, r = 0, len(nums)-1
+
+        while l <= r:
+            mid = (l+r) // 2
+
+            if nums[mid] == target:
+                return mid
