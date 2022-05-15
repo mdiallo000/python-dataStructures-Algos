01b528ef5ffb081d5ca9ca9620ed3f3eb464be96
mdiallo98 <mamadou1237@gmail.com>
2022-05-15 13:10:20 -0400

    has valid path

--- LeetcodeQuestions/Graphs/has_valid_path.py
+++ LeetcodeQuestions/Graphs/has_valid_path.py
@@ -6,4 +6,22 @@ class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
         #  first we need to create the graph based on the edges we were given
         neighbors = defaultdict(list)
-    #  neighbors = {:[], :[]}
+        seen = set()
+    #   neighbors = {:[], :[]}
+        for n1, n2 in edges:
+            neighbors[n1].append(n2)
+            neighbors[n2].append(n1)
+
+        def dfs(graph, start, end):
+            if start == end:
+                return True
+            if start in seen:
+                return False
+
+            for vertex in graph[start]:
+                if dfs(graph, vertex, end):
+                    return True
+
+            return False
+
+        return dfs(neighbors, source, destination)
