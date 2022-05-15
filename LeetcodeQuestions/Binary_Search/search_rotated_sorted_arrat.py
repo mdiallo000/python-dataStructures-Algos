f23a33b7a78696fcfe244d8ad8f8e2f5b5fc62c1
mdiallo98 <mamadou1237@gmail.com>
2022-05-15 15:35:29 -0400

    search rotated sorted array

--- LeetcodeQuestions/Binary_Search/search_rotated_sorted_arrat.py
+++ LeetcodeQuestions/Binary_Search/search_rotated_sorted_arrat.py
@@ -3,8 +3,8 @@ from tkinter import W
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # [4,5,6,7,0,1,2,3] tar = 0
-
+        # [4,5,6,7,0,1,2] tar = 0
+        # [1,2,3,4,5,6]
         l, r = 0, len(nums)-1
 
         while l <= r:
@@ -12,3 +12,18 @@ class Solution:
 
             if nums[mid] == target:
                 return mid
+
+            # NOW WE chech whether or not does our target value falls bettwen the boundaris of l<= targ <= mid
+
+            if nums[l] < nums[mid]:
+                if target >= nums[l] and target <= nums[mid]:
+                    #  in other words our target falls into the left sorted portion
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if target <= nums[r] and target >= nums[mid]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return -1
