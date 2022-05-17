9a36d2659e802a5af8c649065498bef786bc5ce2
mdiallo98 <mamadou1237@gmail.com>
2022-05-16 23:23:12 -0400

    design twitter

--- LeetcodeQuestions/Heap-Priority-Queue/design_twitter.py
+++ LeetcodeQuestions/Heap-Priority-Queue/design_twitter.py
@@ -9,11 +9,14 @@ class Twitter:
         self.Tweets = defaultdict(list)
 
     def postTweet(self, userId: int, tweetId: int) -> None:
+        # whenever a user posts a tweet we need a identifier which will be the userID and we also need the tweet
 
     def getNewsFeed(self, userId: int) -> List[int]:
 
     def follow(self, followerId: int, followeeId: int) -> None:
         # how would we follow a user? Well we would create a connection between one user and the other users they want to follow.
-        self.FollowerMap
+        self.FollowerMap[followeeId].add(followeeId)
 
     def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId in self.FollowerMap:
+            self.FollowerMap[followerId].remove(followeeId)
