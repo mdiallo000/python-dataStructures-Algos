a5228b620dce12c3544e3f8854f32d41340866f5
mdiallo98 <mamadou1237@gmail.com>
2022-05-16 22:18:36 -0400

    design twitter

---
+++ LeetcodeQuestions/Heap-Priority-Queue/design_twitter.py
@@ -0,0 +1,11 @@
+class Twitter:
+
+    def __init__(self):
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
