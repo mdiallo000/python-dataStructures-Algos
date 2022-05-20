e681e35b6c628a01127e91820399dffb043d196f
mdiallo98 <mamadou1237@gmail.com>
2022-05-19 22:40:59 -0400

    copy random list

---
+++ LeetcodeQuestions/linked_list/copy_list_with_random_pointer.py
@@ -0,0 +1,14 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+
+        Oldcopies = {None: None}
