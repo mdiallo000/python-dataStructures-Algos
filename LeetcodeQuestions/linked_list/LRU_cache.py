e4e0c6ba46a22d60abcfbca8de8e676f62cb9243
mdiallo98 <mamadou1237@gmail.com>
2022-05-19 23:33:25 -0400

    lru cache

---
+++ LeetcodeQuestions/linked_list/LRU_cache.py
@@ -0,0 +1,14 @@
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.next, self.prev = None
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+
+    def get(self, key: int) -> int:
+
+    def put(self, key: int, value: int) -> None:
