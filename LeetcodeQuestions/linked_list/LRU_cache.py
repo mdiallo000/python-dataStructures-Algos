aa3e2af06d1313570b9d1644ce2dd72af8fafce6
mdiallo98 <mamadou1237@gmail.com>
2022-05-20 00:19:45 -0400

    lru cache

--- LeetcodeQuestions/linked_list/LRU_cache.py
+++ LeetcodeQuestions/linked_list/LRU_cache.py
@@ -2,14 +2,14 @@ class Node:
     def __init__(self, key, val):
         self.key = key
         self.val = val
-        self.next, self.prev = None
+        self.next = self.prev = None
 
 
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.cache = {}
-        self.capicity = capacity
+        self.capacity = capacity
         self.right, self.left = Node(0, 0), Node(0, 0)
         self.left.next = self.right
         self.right.prev = self.left
@@ -29,11 +29,10 @@ class LRUCache:
     def get(self, key: int) -> int:
         #  if the key exists we not only return its value we also update it to be the most recently accessed
         if key in self.cache:
-            node = self.cache[key]
-            self.remove(node)
-            self.remove(node)
-            #  we remove it from its current position since it is being used and we re instate it back in the front of the list since its been recently used
-            return node.val
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+
+            return self.cache[key].val
         return -1
 
     def put(self, key: int, value: int):
