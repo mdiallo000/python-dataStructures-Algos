f6671843dee2b215f29915d2fc7831184d099abe
mdiallo98 <mamadou1237@gmail.com>
2022-05-20 19:19:12 -0400

    graaphs

--- DataStructure/graphs.py
+++ DataStructure/graphs.py
@@ -1,1 +1,18 @@
 class Vertex:
+    def __init__(self, node):
+        self.id = node
+        self.adjacent = {}
+        #  we set the distance between node to be inf
+        self.distance = float('inf')
+        #  All nodes are thus far unvisited
+        self.visited = False
+        self.previous = None
+
+    def add_neighbor(self, neighbor, weight=0):
+        self.adjacent[neighbor] = weight
+
+
+class Graph:
+    def __int__(self):
+        self.vertDictionary = {}
+        self.numVertices = 0
