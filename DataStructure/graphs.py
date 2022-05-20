6bfc084e01addb30aea0611b32b6b06be3ac23ed
mdiallo98 <mamadou1237@gmail.com>
2022-05-20 19:26:03 -0400

    graaphs

--- DataStructure/graphs.py
+++ DataStructure/graphs.py
@@ -11,6 +11,27 @@ class Vertex:
     def add_neighbor(self, neighbor, weight=0):
         self.adjacent[neighbor] = weight
 
+    def get_connection(self):
+        return self.adjacent.keys()
+
+    def get_vertex_value(self):
+        return self.id
+
+    def get_weight(self, neighbor):
+        return self.adjacent[neighbor]
+
+    def set_distance(self, dist):
+        self.distance = dist
+
+    def set_previous(self, prev):
+        self.previous = prev
+
+    def set_visted(self):
+        self.Visted = True
+
+    def __str__(self):
+        return str(self.id) + 'adjacent:' + str([x.id for x in self.adjacent])
+
 
 class Graph:
     def __int__(self):
