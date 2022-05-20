752524156f01558e65f40694a96d2da4072add46
mdiallo98 <mamadou1237@gmail.com>
2022-05-20 00:09:46 -0400

    lru cache

--- LeetcodeQuestions/linked_list/LRU_cache.py
+++ LeetcodeQuestions/linked_list/LRU_cache.py
@@ -14,9 +14,16 @@ class LRUCache:
         self.left.next = self.right
         self.right.prev = self.left
     # create Helper function that will delete and add new nodes into cache
+
     def insert(self, node):
+        previous = self.right.prev
+        node.prev = previous
+        node.next = self.right
+        self.right.prev = node
 
     def remove(self, node):
+        prev, nextNode = node.prev, node.next
+        prev.next, nextNode.prev = nextNode, prev
 
     def get(self, key: int) -> int:
         #  if the key exists we not only return its value we also update it to be the most recently accessed
@@ -29,3 +36,13 @@ class LRUCache:
         return -1
 
     def put(self, key: int, value: int):
+
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+        # if we go over the allowed capacity of our cache then we need to remove the LRU
+        if len(self.cache) > self.capacity:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
