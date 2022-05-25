7b68058d7d3df8af2d442f9c1496e751cec54b71
mdiallo98 <mamadou1237@gmail.com>
2022-05-25 17:25:53 -0400

    surrounded regions

--- LeetcodeQuestions/Graphs/find_redundant_connection.py
+++ LeetcodeQuestions/Graphs/find_redundant_connection.py
@@ -6,14 +6,33 @@ class Solution:
 
         adjcency_List = defaultdict(list)
         visited = set()
+        res = []
+
+        def DFS(start, end):
+
+            if start in visited:
+                return False
+
+            if start == end:
+                return True
+
+            visited.add(start)
+
+            for neig in adjcency_List[start]:
+                if DFS(neig, end):
+                    return True
+            return False
+
         for n1, n2 in edges:
+            if DFS(n1, n2):
+                res = [n1, n2]
+
             adjcency_List[n1].append(n2)
             adjcency_List[n2].append(n1)
         # ex:
-
+        return res[-1]
         # {
         #     1:[2,3]
-        #     2:[1,2]
+        #     2:[1,3]
         #     3:[2]
         # }
-        def DFS(graph, start):
