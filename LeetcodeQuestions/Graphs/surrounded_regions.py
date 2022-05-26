0d5632f50504b70859c6074a7005f6130704330a
mdiallo98 <mamadou1237@gmail.com>
2022-05-25 23:47:43 -0400

    surrounded regions

--- LeetcodeQuestions/Graphs/surrounded_regions.py
+++ LeetcodeQuestions/Graphs/surrounded_regions.py
@@ -1,3 +1,6 @@
+from tkinter.tix import ROW
+
+
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
         """
@@ -17,3 +20,12 @@ class Solution:
             Traverse(r, c+1)
             Traverse(r, c-1)
     #  Now we run this traversal algo on the edges of our board to change the unsorrounded O into another char
+        for r in range(ROWS):
+            for c in range(COLUMS):
+                if board[r][c] == "O" and (r in [0, ROWS - 1]) and (c in [0, COLUMS - 1]):
+                    Traverse(r, c)
+
+                #  NOW ALL OF THE O NEXT TO THE BORDER OF THE BOARD WILL CHANGE INTO ANOTHER CHAR
+
+    #  The Next step is change the surronded O's in to X's
+        for r in range(ROWS)
