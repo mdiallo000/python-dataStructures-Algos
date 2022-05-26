8bf1eda230302dd14f08298b6ee1845cfdfb12fb
mdiallo98 <mamadou1237@gmail.com>
2022-05-25 23:44:14 -0400

    surrounded regions

--- LeetcodeQuestions/Graphs/surrounded_regions.py
+++ LeetcodeQuestions/Graphs/surrounded_regions.py
@@ -8,3 +8,12 @@ class Solution:
 
     #  Now we will write the Traversal algorithm that will
         def Traverse(r, c):
+            #  this base case will return once we no longer me the conditions
+            if 0 < r >= ROWS or 0 < c >= COLUMS or board[r][c] != "O":
+                return
+            board[r][c] = "T"
+            Traverse(r+1, c)
+            Traverse(r-1, c)
+            Traverse(r, c+1)
+            Traverse(r, c-1)
+    #  Now we run this traversal algo on the edges of our board to change the unsorrounded O into another char
