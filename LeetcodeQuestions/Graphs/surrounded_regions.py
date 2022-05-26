0cc3fea171adc6115a7d084c472ba72bdf79fb05
mdiallo98 <mamadou1237@gmail.com>
2022-05-25 23:40:31 -0400

    surrounded regions

--- LeetcodeQuestions/Graphs/surrounded_regions.py
+++ LeetcodeQuestions/Graphs/surrounded_regions.py
@@ -3,3 +3,8 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
+    # Get the dimensions of the board we are working with
+        ROWS, COLUMS = len(board), len(board[0])
+
+    #  Now we will write the Traversal algorithm that will
+        def Traverse(r, c):
