d7ee777d61caf184b281ea961aed4fe2205071ad
mdiallo98 <mamadou1237@gmail.com>
2022-05-27 21:21:08 -0400

    ALL Path from source to target

--- LeetcodeQuestions/Graphs/rotting_oranges.py
+++ LeetcodeQuestions/Graphs/rotting_oranges.py
@@ -1,6 +1,21 @@
+
+
+import collections
+
+
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
 
         ROWS, COLUMS = len(ROWS), len(COLUMS)
         time_count, fresh = 0, 0
-        # def traverse(r,c):
+        queue = collections.deque()
+
+        #  We will loop through the entire grid to check how many fresh oranges we have as well ass add the rotten oranges to our queue
+        for r in range(ROWS):
+            for c in range(COLUMS):
+                if grid[r][c] == 2:
+                    queue.append([r, c])
+                if grid[r][c] == 1:
+                    fresh_orange += 1
+
+        # now that we know how many fresh oranges we have we can tell wheter or not we managed to rotten all the oranges
