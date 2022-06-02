150faf51a87970886f985a6c55f574536ee165c6
mdiallo98 <mamadou1237@gmail.com>
2022-06-02 19:57:07 -0400

    design an underground system

--- BloombergQBank/design_underground_sys.py
+++ BloombergQBank/design_underground_sys.py
@@ -8,6 +8,9 @@ class UndergroundSystem:
         checkOutReg = defaultdict(list)
 
     def checkIn(self, id: int, stationName: str, t: int) -> None:
+        self.person = id
+        if self.checkInReg[self.person][0] != stationName:
+            self.checkInReg[self.person].append(stationName, t)
 
     def checkOut(self, id: int, stationName: str, t: int) -> None:
 
