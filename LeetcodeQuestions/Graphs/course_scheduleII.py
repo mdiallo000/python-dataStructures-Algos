ff74c032f0e41275aef6d19edc67cd01d1370b99
mdiallo98 <mamadou1237@gmail.com>
2022-06-01 20:47:44 -0400

    course schedule II

--- LeetcodeQuestions/Graphs/course_scheduleII.py
+++ LeetcodeQuestions/Graphs/course_scheduleII.py
@@ -1,3 +1,10 @@
+from collections import defaultdict
+from email.policy import default
+
+
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         graph = defaultdict(list)
+
+        for crs1, crs2 in prerequisites:
+            graph[crs1].append(crs2)
