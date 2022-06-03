1450d52baa5d12c6f98eabcfd99b83cb791c1b17
mdiallo98 <mamadou1237@gmail.com>
2022-06-02 20:27:43 -0400

    design an underground system

--- BloombergQBank/design_underground_sys.py
+++ BloombergQBank/design_underground_sys.py
@@ -14,5 +14,11 @@ class UndergroundSystem:
     def checkOut(self, id: int, stationName: str, t: int) -> None:
         # If someone leaves the station than we should erase them from the database to conserve money
         start_station, start_time = self.checkInReg[id].pop()
+        end_station = stationName
+        end_time = t
+        #  we take the out info on where the passanger intially gone on their journey and at what time the started this journey
+
+        self.journeyTracker[(start_station, end_station)
+                            ][0] = (end_time - start_time)
 
     def getAverageTime(self, startStation: str, endStation: str) -> float:
