039d5a7706f21889a0966f18910c37e71687e500
mdiallo98 <mamadou1237@gmail.com>
2022-06-06 20:34:33 -0400

    subsets

--- LeetcodeQuestions/backtracking/subsets.py
+++ LeetcodeQuestions/backtracking/subsets.py
@@ -4,3 +4,4 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
 
         #  Initially i did this question by using a whole lot of parameters but there is a simpler way of doing the prblem
+        #  since we are interested in all the subsets of a particular list we need generate them recursively by adding elements in of part of the recursive tree and removing them in other parts
