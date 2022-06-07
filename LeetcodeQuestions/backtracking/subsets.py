2b4b15ca0924e19a0df314e1142ccc4e5d0f2ac7
mdiallo98 <mamadou1237@gmail.com>
2022-06-06 20:33:13 -0400

    subsets

--- LeetcodeQuestions/backtracking/subsets.py
+++ LeetcodeQuestions/backtracking/subsets.py
@@ -1,2 +1,6 @@
+
+
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+
+        #  Initially i did this question by using a whole lot of parameters but there is a simpler way of doing the prblem
