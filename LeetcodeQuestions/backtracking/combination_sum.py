1ef444205c0c868a646e8cab347bab733495829c
mdiallo98 <mamadou1237@gmail.com>
2022-06-06 22:26:16 -0400

    subsets

--- LeetcodeQuestions/backtracking/combination_sum.py
+++ LeetcodeQuestions/backtracking/combination_sum.py
@@ -3,15 +3,19 @@ class Solution:
         # Solving these problems will follow a similar behavior like finding the subsets of a range of numbers
         res = []
 
-        def finSum(idx, seqs, total):
+        def findSum(idx, seqs, total):
 
             # establish base case
-            if idx >= len(candidates):
+            if idx >= len(candidates) or total > target:
                 if total == target:
                     res.append(seqs.copy())
+                    return
                 return
 
             seqs.append(candidates[idx])
-            findSum(idx+1, seqs, total + candidates[idx])
+            findSum(idx, seqs, total + candidates[idx])
             seqs.pop()
-            findSum(idx+1, seqs, total + candidates[idx])
+            findSum(idx+1, seqs, total)
+
+        findSum(0, [], 0)
+        return res
