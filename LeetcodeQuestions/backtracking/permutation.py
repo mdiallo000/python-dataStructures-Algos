4bf2a2cb2404b820b55f9f751f3070455a45e30e
mdiallo98 <mamadou1237@gmail.com>
2022-06-08 21:30:48 -0400

    Combination sum II

--- LeetcodeQuestions/backtracking/permutation.py
+++ LeetcodeQuestions/backtracking/permutation.py
@@ -1,2 +1,20 @@
+from itertools import permutations
+
+
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+
+        res = []
+        permutation = []
+        freq = [False] * len(nums)
+
+        def generate():
+
+            if len(permutation) >= len(nums):
+                res.append(permutation.copy())
+                return
+
+            for i in range(nums):
+                if i not in freq:
+                    permutation.append(nums[i])
+                    freq[i] = True
