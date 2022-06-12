475e8f84ac4ba3a270040cf8feda229c78386da5
mdiallo98 <mamadou1237@gmail.com>
2022-06-11 23:12:18 -0400

    combination sum II

--- LeetcodeQuestions/backtracking/combination_sumII.py
+++ LeetcodeQuestions/backtracking/combination_sumII.py
@@ -2,3 +2,12 @@ class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         # first we need to sort the given list to make it posssible to skip duplicates
         candidates.sort()
+        res = []
+
+        def generate(combination, idx, total):
+
+            if total == 0:
+                res.append(combination[:])
+            #  if our total somehow goes pass zero that means we have overshot the target and need to cease pursing this path
+            if total <= 0:
+                return
