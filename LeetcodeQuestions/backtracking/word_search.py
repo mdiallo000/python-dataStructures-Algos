69a7b56f3a36fb58a4ae197834ab99312267a748
mdiallo98 <mamadou1237@gmail.com>
2022-06-13 19:31:52 -0400

    word search

--- LeetcodeQuestions/backtracking/word_search.py
+++ LeetcodeQuestions/backtracking/word_search.py
@@ -19,7 +19,17 @@ class Solution:
             if r < 0 or r == ROWS or c < 0 or c == COLUMS or (r, c) in path or word[idx] != board[r][c]:
                 return False
             path.add((r, c))
-            res = pathfinder(r+1, c, idx+1),
-            pathfinder(r-1, c, idx+1),
-            pathfinder(r, c+1, idx+1),
-            pathfinder(r, c-1, idx+1)
+            res = (pathfinder(r+1, c, idx+1) or
+                   pathfinder(r-1, c, idx+1) or
+                   pathfinder(r, c+1, idx+1) or
+                   pathfinder(r, c-1, idx+1))
+            path.remove((r, c))
+            return res
+
+        for r in range(ROWS):
+            for c in range(COLUMS):
+                if pathfinder(r, c, 0):
+                    return True
+        return False
+#  Time complexity will be O (n*m*pathfinder)
+#  O of pathfindder is 4^N since we are calling the function fours times which will depend on the lenght of our word N
