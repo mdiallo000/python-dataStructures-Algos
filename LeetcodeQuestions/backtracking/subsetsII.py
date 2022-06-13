c636f261c75285d49e91d1965db67fee9bd4c52d
mdiallo98 <mamadou1237@gmail.com>
2022-06-13 18:46:58 -0400

    subsets II

--- LeetcodeQuestions/backtracking/subsetsII.py
+++ LeetcodeQuestions/backtracking/subsetsII.py
@@ -1,13 +1,13 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         res = []
+        #  we need to sort the input array so that we can later apply logic that will prevent the inclusion of duplicates
+        nums.sort()
+        #  sorting will cost a time complexity of O(logN)
 
         def generate(subsets, idx):
-
-            if idx >= len(nums):
+            #  what is our base case?
+            #  when our idx is the same as the len(nums) that means we have reached the end of our options
+            if idx == len(nums):
                 res.append(subsets[:])
                 return
-            prev = -1
-            for i in range(idx, len(nums)):
-                if nums[i] == prev:
-                    continue
