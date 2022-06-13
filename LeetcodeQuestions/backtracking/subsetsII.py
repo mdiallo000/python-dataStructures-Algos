0a80cbf04c530e9a83b79e4f3c94fa26fff92b76
mdiallo98 <mamadou1237@gmail.com>
2022-06-13 18:52:30 -0400

    subsets II

--- LeetcodeQuestions/backtracking/subsetsII.py
+++ LeetcodeQuestions/backtracking/subsetsII.py
@@ -11,3 +11,15 @@ class Solution:
             if idx == len(nums):
                 res.append(subsets[:])
                 return
+            #  Now we can add this element into our subset and generate its subsets
+            subsets.append(nums[idx])
+            #  we now call our function again which will generate the subsets for this path
+            generate(subsets, idx+1)
+            #  Next we pop out the element to create the other variance
+            subsets.pop()
+
+            # Now here comes the real difference maker between this problem and its first version
+            # We will need some type a loop that will move the idx whenever we encounter an element that was already chosen previously, this will help us avoid generating duplicate subests
+            while idx < len(nums) and nums[idx] == nums[idx+1]:
+                idx += 1
+            # Now we can call the function again and continue on in our decision tree
