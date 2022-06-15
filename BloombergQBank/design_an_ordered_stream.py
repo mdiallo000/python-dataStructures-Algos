d7700e955713dcb001a3398c2ec9d2a5e31ed5d4
mdiallo98 <mamadou1237@gmail.com>
2022-06-14 20:34:35 -0400

    Design an ordered stream

--- BloombergQBank/design_an_ordered_stream.py
+++ BloombergQBank/design_an_ordered_stream.py
@@ -5,3 +5,10 @@ class OrderedStream:
         self.position = 0
 
     def insert(self, idKey: int, value: str) -> List[str]:
+        res = []
+        self.stream[idKey - 1] = value
+
+        while self.position < len(self.stream) and self.stream[self.position] != None:
+            res.append(self.stream[self.position])
+            self.position += 1
+        return res
