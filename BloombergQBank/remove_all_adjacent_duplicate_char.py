f6de361d25ff224ee083da3823c6440ec60b5698
mdiallo98 <mamadou1237@gmail.com>
2022-06-15 22:21:33 -0400

    remove duplicate chars

---
+++ BloombergQBank/remove_all_adjacent_duplicate_char.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+
+        stack = []
+
+        for char in s:
+            if stack and stack[-1][0] == char:
+                stack[-1][1] += 1
+            else:
+                stack.append([char, 1])
