8dc357126fe9c97bfb44f2e65e9aa70b7079070e
mdiallo98 <mamadou1237@gmail.com>
2022-06-15 22:23:14 -0400

    remove duplicate chars

--- BloombergQBank/remove_all_adjacent_duplicate_char.py
+++ BloombergQBank/remove_all_adjacent_duplicate_char.py
@@ -8,3 +8,14 @@ class Solution:
                 stack[-1][1] += 1
             else:
                 stack.append([char, 1])
+
+            if stack[-1][1] == k:
+                stack.pop()
+
+        res = ""
+
+        for char, count in stack:
+            res += (char * count)
+        return res
+
+        #  Time complexity will be O(N + n)
