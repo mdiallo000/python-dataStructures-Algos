265d7be1531a84c32102771c8e113bf503774329
mdiallo98 <mamadou1237@gmail.com>
2022-06-21 17:08:00 -0400

    valid sudoku

--- LeetcodeQuestions/Hash_Table_Dict/valid_Sudoku.py
+++ LeetcodeQuestions/Hash_Table_Dict/valid_Sudoku.py
@@ -10,3 +10,11 @@ class Solution:
 
         for r in range(len(board)):
             for c in range(len(board[0])):
+                #  if the value at this current cell is not a number then we will skip it
+                if board[r][c] == ".":
+                    continue
+                # next we check if this current cell is already been found in our rows, colum or square
+
+                if (board[r][c] in row[r] or board[r][c] in colum[c] or board[r][c] in square[(r//3, c//3)]):
+                    #  we will return false since this is not a valid sudoku anymore
+                    return False
