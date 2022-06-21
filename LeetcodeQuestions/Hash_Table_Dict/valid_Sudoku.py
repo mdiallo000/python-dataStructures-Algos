ac47b11cfb16d4ab7f2717f48134a6ff47d6b24d
mdiallo98 <mamadou1237@gmail.com>
2022-06-21 17:04:11 -0400

    valid sudoku

--- LeetcodeQuestions/Hash_Table_Dict/valid_Sudoku.py
+++ LeetcodeQuestions/Hash_Table_Dict/valid_Sudoku.py
@@ -7,3 +7,6 @@ class Solution:
         row = defaultdict(set)
         colum = defaultdict(set)
         square = defaultdict(set)
+
+        for r in range(len(board)):
+            for c in range(len(board[0])):
