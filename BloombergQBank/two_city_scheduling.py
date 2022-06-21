9018aa2c054ea8a30d282988fc27783eecb84b6f
mdiallo98 <mamadou1237@gmail.com>
2022-06-21 18:33:04 -0400

    two city scheduling

--- BloombergQBank/two_city_scheduling.py
+++ BloombergQBank/two_city_scheduling.py
@@ -1,2 +1,16 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+
+        difference = []
+        for c1, c2 in costs:
+            difference.append(c2-c1, c1, c2)
+        difference.sort()
+        total = 0
+
+        for i in range(len(difference)):
+
+            if len(difference) // 2:
+                total += difference[i][2]
+            else:
+                total += difference[i][2]
+        return total
