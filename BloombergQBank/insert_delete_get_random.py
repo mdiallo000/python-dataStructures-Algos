802f596d5c5ff4c7c934f88d0f613b2361e71565
mdiallo98 <mamadou1237@gmail.com>
2022-06-21 20:37:08 -0400

    randomizedSet

--- BloombergQBank/insert_delete_get_random.py
+++ BloombergQBank/insert_delete_get_random.py
@@ -5,7 +5,14 @@ class RandomizedSet:
         self.list = []
 
     def insert(self, val: int) -> bool:
+        if val in self.dict:
+            return False
+        self.list.append(val)
+        self.dict[val] = len(self.list)-1
+        return True
 
     def remove(self, val: int) -> bool:
+        if val in self.dict:
+            last_elem,
 
     def getRandom(self) -> int:
