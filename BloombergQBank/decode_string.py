c2776a6e9fefd5a4cebd9cdb164969289a3577a9
mdiallo98 <mamadou1237@gmail.com>
2022-06-24 23:29:11 -0400

    decode string

--- BloombergQBank/decode_string.py
+++ BloombergQBank/decode_string.py
@@ -1,2 +1,14 @@
 class Solution:
     def decodeString(self, s: str) -> str:
+
+        stack = []
+
+        for i in range(len(s)):
+
+            if s[i] != ']':
+                stack.append(s[i])
+            else:
+                substr = ''
+
+                while stack[-1] != "[":
+                    substr = stack.popleft() + substr
