dc1dc87a438ca8bc335ee3c31f5b88ba392f43b9
mdiallo98 <mamadou1237@gmail.com>
2022-06-29 22:48:29 -0400

    all elem in bst

--- BloombergQBank/all_elem_in_BST.py
+++ BloombergQBank/all_elem_in_BST.py
@@ -1,2 +1,10 @@
 class Solution:
     def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
+        res = []
+
+        def inorder(node):
+            if not node:
+                return None
+            inorder(node.left)
+            res.append(node.val)
+            inorder(node.right)
