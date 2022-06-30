df4acdc90484daecae6e86912c2c36c4486f5839
mdiallo98 <mamadou1237@gmail.com>
2022-06-29 23:36:04 -0400

    invalid transactions

--- BloombergQBank/invalid_transactions.py
+++ BloombergQBank/invalid_transactions.py
@@ -1,2 +1,16 @@
+from collections import defaultdict
+from email.policy import default
+
+
 class Solution:
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
+
+        # Input: transactions = ["alice,20,800,mtv","alice,50,100,beijing"]
+        # Output: ["alice,20,800,mtv","alice,50,100,beijing"]
+
+        result = []
+        userInfo = defaultdict(lambda: defaultdict(set))
+
+        for t in transactions:
+            name, time, amount, city = t.split(",")
+            userInfo[name][time].add(time)
