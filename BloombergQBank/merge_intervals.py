00d8720268fc4f9172eba9fc33f8658bb1d9c3d1
mdiallo98 <mamadou1237@gmail.com>
2022-06-30 22:26:02 -0400

    merge interval

--- BloombergQBank/merge_intervals.py
+++ BloombergQBank/merge_intervals.py
@@ -1,2 +1,11 @@
+from curses import keyname
+
+
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        # test case = [[1,5],[7,8],[12,17],[2,4]]
+        #  First we sort the orignal list by the first elem in each sublist
+        intervals.sort(key=lambda i: intervals[0][1])
+        #  now are list is [[[1,5],[2,4],[7,8],[12,17],]
+        #  now we just want to add the first elem into our array since we will use this to find intervals
+        result = [intervals[0]]
