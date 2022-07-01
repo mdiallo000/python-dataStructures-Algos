34462f834f12972aed438aed43ad087e7f45eed4
mdiallo98 <mamadou1237@gmail.com>
2022-06-30 22:33:05 -0400

    merge interval

--- BloombergQBank/merge_intervals.py
+++ BloombergQBank/merge_intervals.py
@@ -9,8 +9,13 @@ class Solution:
         #  now are list is [[[1,5],[2,4],[7,8],[12,17],]
         #  now we just want to add the first elem into our array since we will use this to find intervals
         result = [intervals[0]]
-
+        #  result =  [[1,5] ]
         #  now we iterate through our list and determin where there may be intervals
         #  we want to start looking after the first elmen since we are comparing the incoming with the previous
         for start, end in intervals[1:]:
-            previous_end = intervals[0][1]
+            previous_end = intervals[-1][1]
+
+            if start <= previous_end:
+                result[-1][1] = max(end, previous_end)
+            else:
+                result.append([start, end])
