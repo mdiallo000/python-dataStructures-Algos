52f905e09160a415da91e485759aac9f6627ed7e
mdiallo98 <mamadou1237@gmail.com>
2022-07-01 23:33:48 -0400

    vertical order traversal

--- BloombergQBank/vertical_order_binary_tree.py
+++ BloombergQBank/vertical_order_binary_tree.py
@@ -1,3 +1,4 @@
+from collections import deque
 from email.policy import default
 
 
@@ -5,3 +6,17 @@ class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
 
         columTable = default(list)
+        q = deque()
+        q.append([(root, 0)])
+        result = []
+
+        while q:
+            size = len(q)
+            for i in range(size):
+                node, columPos = q.popleft()
+
+                if node:
+                    columTable[columPos].append(node.val)
+                    q.append([(node.left, columPos - 1)])
+                    q.append([(node.right, columPos + 1)])
+        return [columTable[x] for x in sorted(key=columTable.keys())]
