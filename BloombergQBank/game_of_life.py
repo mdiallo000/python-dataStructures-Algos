6107b3f5caf536b79e4dc0fa38525b6468680e99
mdiallo98 <mamadou1237@gmail.com>
2022-07-02 23:07:50 -0400

    game of life

--- BloombergQBank/game_of_life.py
+++ BloombergQBank/game_of_life.py
@@ -7,6 +7,12 @@ class Solution:
         COLUMS = len(board[0])
         directions = [[1, 0], [-1, 0], [0, 1], [0, -1],
                       [1, 1], [-1, 1], [1, -1], [-1, -1]]
+        copyboard = [[board[r][c] for c in range(ROWS)] for r in range(COLUMS)]
+        for row in range(ROWS):
+            for col in range(COLUMS):
+                alive = 0
+                for dr, dc in directions:
+                    r, c = row + dr, dc + col
 
-        for r in range(ROWS)
-        for c in range(COLUMS)
+                    if r >= 0 or r < ROWS or c >= 0 or c < COLUMS or board[r][c] == 1:
+                        alive += 1
