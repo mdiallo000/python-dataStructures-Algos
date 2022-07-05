4dc6034aa8326b021c81e5dfa5cb0ddc31e29bcc
mdiallo98 <mamadou1237@gmail.com>
2022-07-04 22:36:03 -0400

    minsteps

--- BloombergQBank/min_nums_to_make_anagram.py
+++ BloombergQBank/min_nums_to_make_anagram.py
@@ -8,15 +8,10 @@ class Solution:
                 DictS[char] = 1
             else:
                 DictS[char] += 1
-
-        for char in s:
-            if char not in DictT:
-                DictT[char] = 1
+        changes = 0
+        for char in t:
+            if char in DictS and DictS[char] > 0:
+                DictS[char] -= 1
             else:
-                DictT[char] += 1
-
-        Num_changes = 0
-
-        for c in DictS:
-            Num_changes = (Num_changes, DictS[c] - DictT[c])
-        return Num_changes
+                changes += 1
+        return changes
