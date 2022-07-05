c6ada2c6a546ff6e2315b44f8b5d8a851a9fcf92
mdiallo98 <mamadou1237@gmail.com>
2022-07-04 22:23:48 -0400

    minsteps

--- BloombergQBank/min_nums_to_make_anagram.py
+++ BloombergQBank/min_nums_to_make_anagram.py
@@ -2,3 +2,14 @@ class Solution:
     def minSteps(self, s: str, t: str) -> int:
 
         DictT, DictS = {}, {}
+
+        for char in s:
+            if char not in DictS:
+                DictS[char] = 1
+            else:
+                DictS[char] += 1
+        for char in s:
+            if char not in DictS:
+                DictS[char] = 1
+            else:
+                DictS[char] += 1
