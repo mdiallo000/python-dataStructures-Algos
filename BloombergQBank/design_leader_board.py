75e169ec6a5bf71172d0a035a0416fe6e2b1b34a
mdiallo98 <mamadou1237@gmail.com>
2022-07-07 23:24:30 -0400

    design leaderboad

--- BloombergQBank/design_leader_board.py
+++ BloombergQBank/design_leader_board.py
@@ -1,3 +1,6 @@
+import heapq
+
+
 class Leaderboard:
 
     def __init__(self):
@@ -5,11 +8,23 @@ class Leaderboard:
 
         # Update the leaderboard by adding score to the given player's score. If there is no player with such id in the leaderboard, add him to the leaderboard with the given score.
     def addScore(self, playerId: int, score: int) -> None:
-        self.database[palyerId] = score
+        if playerId not in self.database:
+            self.database[playerId] = score
         # Return the score sum of the top K players.
+        self.database[playerId] += score
 
     def top(self, K: int) -> int:
+        heap = []
+        for val in self.database.values():
+            heapq.heappush(heap, val)
+            if len(heap) > K:
+                heapq.heappop(heap)
+        Top_scores = 0
+        while heap:
+            Top_scores += heapq.heappop(heap)
+        return Top_scores
 
         # Reset the score of the player with the given id to 0 (in other words erase it from the leaderboard). It is guaranteed that the player was added to the leaderboard before calling this function.
 
     def reset(self, playerId: int) -> None:
+        del self.database[playerId]
