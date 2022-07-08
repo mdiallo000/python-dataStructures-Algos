b4ae5f9c2ab69fb2354a52ea3a9b31743a8ec344
mdiallo98 <mamadou1237@gmail.com>
2022-07-07 22:45:01 -0400

    design leaderboad

--- BloombergQBank/design_leader_board.py
+++ BloombergQBank/design_leader_board.py
@@ -1,9 +1,15 @@
 class Leaderboard:
 
     def __init__(self):
+        self.database = {}
 
+        # Update the leaderboard by adding score to the given player's score. If there is no player with such id in the leaderboard, add him to the leaderboard with the given score.
     def addScore(self, playerId: int, score: int) -> None:
+        self.database[palyerId] = score
+        # Return the score sum of the top K players.
 
     def top(self, K: int) -> int:
+
+        # Reset the score of the player with the given id to 0 (in other words erase it from the leaderboard). It is guaranteed that the player was added to the leaderboard before calling this function.
 
     def reset(self, playerId: int) -> None:
