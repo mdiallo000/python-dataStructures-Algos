84a43450953464ca3be9d2983d6bc1a477a43075
mdiallo98 <mamadou1237@gmail.com>
2022-07-11 23:49:39 -0400

    arranging coins

--- QBank/arranging_coins.py
+++ QBank/arranging_coins.py
@@ -5,3 +5,11 @@ class Solution:
         while left <= right:
             k = (l + r) // 2
             curr = k * (k + 1)//2
+
+            if curr == n:
+                return k
+
+            if n < curr:
+                r = k - 1
+            else:
+                l = k + 1
