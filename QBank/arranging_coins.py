9bc143cfc8a9a7e4a40ef50e357576f6dcf55ed2
mdiallo98 <mamadou1237@gmail.com>
2022-07-11 23:48:50 -0400

    arranging coins

--- QBank/arranging_coins.py
+++ QBank/arranging_coins.py
@@ -1,7 +1,7 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        left , right = 0, n 
-        
+        left, right = 0, n
+
         while left <= right:
-            
-        
+            k = (l + r) // 2
+            curr = k * (k + 1)//2
