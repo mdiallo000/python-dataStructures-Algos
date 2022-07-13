80972576a589a5f5125af5f0387383cb09c841fd
mdiallo98 <mamadou1237@gmail.com>
2022-07-12 23:59:08 -0400

    insert Intervals

--- QBank/insert_intervals.py
+++ QBank/insert_intervals.py
@@ -1,2 +1,20 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        #  we are lucky to be given
+
+        result = []
+
+        for i in range(len(intervals)):
+
+            if newInterval[1] < intervals[i][0]:
+                result.append(newInterval)
+                return result + intervals[i:]
+
+            elif newInterval[0] > intervals[i][1]:
+                result.append(intervals[i])
+            else:
+                newInterval = [min(newInterval[0], intervals[i][0]), max(
+                    newInterval[1], intervals[i][1])]
+        result.append(newInterval)
+
+        return result
