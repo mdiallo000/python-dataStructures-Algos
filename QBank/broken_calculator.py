0827663899675a29c9c24b45a04b788ef2aca491
mdiallo98 <mamadou1237@gmail.com>
2022-07-14 16:23:23 -0400

    broken calculator

--- QBank/broken_calculator.py
+++ QBank/broken_calculator.py
@@ -1,2 +1,17 @@
 class Solution:
     def brokenCalc(self, startValue: int, target: int) -> int:
+
+        steps = 0
+
+        while startValue != target:
+
+            if target < startValue:
+                steps = startValue - target
+                target = startValue
+            else:
+                if target % 2 == 0:
+                    target = target // 2
+                else:
+                    target += 1
+                steps += 1
+        return steps
