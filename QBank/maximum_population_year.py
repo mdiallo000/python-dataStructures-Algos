a19afa9b0c9af64c32c314509372bbfb92736e16
mdiallo98 <mamadou1237@gmail.com>
2022-07-15 22:30:10 -0400

    maximum Population year

--- QBank/maximum_population_year.py
+++ QBank/maximum_population_year.py
@@ -6,7 +6,11 @@ class Solution:
         popCount = 0
 
         res = [logs[0]]
-
+        store_year = []
         for start, end in logs[1:]:
 
             prevEnd = res[-1][1]
+
+            if start - 1 < prevEnd:
+                popCount += 1
+                store_year.append([prevEnd, popCount])
