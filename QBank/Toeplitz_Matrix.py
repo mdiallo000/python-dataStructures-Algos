92c006e8b6c93a7f13cd6486a2aee149e5de8a3a
mdiallo98 <mamadou1237@gmail.com>
2022-07-15 22:12:29 -0400

    maximum Population year

--- QBank/Toeplitz_Matrix.py
+++ QBank/Toeplitz_Matrix.py
@@ -1,15 +1,16 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+
         ROWS = len(matrix)
         COLUMS = len(matrix[0])
-        
+
         if ROWS < 2 and COLUMS < 2:
-            return True 
-        
-        for r in range(ROWS -1):
-            for c in range(COLUMS -1):
-                
+            return True
+
+        for r in range(ROWS - 1):
+            for c in range(COLUMS - 1):
+
                 if matrix[r][c] != matrix[r+1][c+1]:
-                    return False 
-                
-        return True 
+                    return False
+
+        return True
