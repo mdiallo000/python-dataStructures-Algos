914f359fd187f4218172e6f120413357ea07c5e0
mdiallo98 <mamadou1237@gmail.com>
2022-07-15 22:27:07 -0400

    maximum Population year

--- QBank/maximum_population_year.py
+++ QBank/maximum_population_year.py
@@ -1,2 +1,12 @@
 class Solution:
     def maximumPopulation(self, logs: List[List[int]]) -> int:
+
+        logs.sort(key=lambda i: i[0])
+
+        popCount = 0
+
+        res = [logs[0]]
+
+        for start, end in logs[1:]:
+
+            prevEnd = res[-1][1]
