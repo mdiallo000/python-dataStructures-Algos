a5bcf1ed2588e27523746a0e4c62af3d5473f961
mdiallo98 <mamadou1237@gmail.com>
2022-07-17 20:52:03 -0400

    custom sorting string

--- QBank/custom_sort_string.py
+++ QBank/custom_sort_string.py
@@ -1,2 +1,10 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
+
+        count = Counter(s)
+        ans = []
+        # "cbad"
+        #  i
+        for char in order:
+            ans.append(char * count[char])
+            count[char] = 0
