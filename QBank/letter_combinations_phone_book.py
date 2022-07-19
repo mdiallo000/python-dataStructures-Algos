0a54630b136fd115e04e135303809c007693090e
mdiallo98 <mamadou1237@gmail.com>
2022-07-18 20:55:43 -0400

    letter combinations

---
+++ QBank/letter_combinations_phone_book.py
@@ -0,0 +1,14 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+
+        charMap = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "qprs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
