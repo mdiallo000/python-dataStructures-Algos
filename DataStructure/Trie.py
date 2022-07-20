42aca24183b547577788b0b4ebab719e1d1aabc0
mdiallo98 <mamadou1237@gmail.com>
2022-07-19 22:54:32 -0400

    Trie

--- DataStructure/Trie.py
+++ DataStructure/Trie.py
@@ -8,8 +8,16 @@ class TrieNode:
 class Trie:
 
     def __init__(self):
+        self.root = TrieNode()
 
     def insert(self, word: str) -> None:
+        curr = self.root
+
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.endWord = True
 
     def search(self, word: str) -> bool:
 
