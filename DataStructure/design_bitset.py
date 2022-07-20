f0cfc9d5de46ef745576cff0f895351f2a2f643f
mdiallo98 <mamadou1237@gmail.com>
2022-07-19 21:36:53 -0400

    desgin bit

---
+++ DataStructure/design_bitset.py
@@ -0,0 +1,17 @@
+class Bitset:
+
+    def __init__(self, size: int):
+
+    def fix(self, idx: int) -> None:
+
+    def unfix(self, idx: int) -> None:
+
+    def flip(self) -> None:
+
+    def all(self) -> bool:
+
+    def one(self) -> bool:
+
+    def count(self) -> int:
+
+    def toString(self) -> str:
