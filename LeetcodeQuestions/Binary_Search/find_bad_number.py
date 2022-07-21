810314540c538cb2627fc57e70c173255de107f4
mdiallo98 <mamadou1237@gmail.com>
2022-07-20 23:29:28 -0400

    find_bad_number

--- LeetcodeQuestions/Binary_Search/find_bad_number.py
+++ LeetcodeQuestions/Binary_Search/find_bad_number.py
@@ -1,3 +1,6 @@
+from re import L
+
+
 class Solution:
     def firstBadVersion(self, n: int) -> int:
         l, r = 1, n
@@ -5,3 +8,9 @@ class Solution:
         while l < r:
 
             mid = l + (r - l)//2
+
+            if isBadVersion(mid):
+                r = mid
+            else:
+                l = mid + 1
+        return left
