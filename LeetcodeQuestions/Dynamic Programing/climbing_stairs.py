84914ab068a7c5f9fb5d627f68a271329c3adf30
mdiallo98 <mamadou1237@gmail.com>
2022-07-21 20:07:50 -0400

    climbing_stairs

--- LeetcodeQuestions/Dynamic Programing/climbing_stairs.py
+++ LeetcodeQuestions/Dynamic Programing/climbing_stairs.py
@@ -1,17 +1,5 @@
-from itertools import starmap
-
-from pkg_resources import cleanup_resources
 
 
 class Solution:
     def climbStairs(self, n: int) -> int:
-
-        # Appenrantly its all fibnocci
-        def climb(stairs):
-            if stairs == 1:
-                return 1
-            if stairs == 2:
-                return stairs
-
-            return climb(stairs - 1) + climb(stairs - 2)
-        climb(n)
+        one, two
