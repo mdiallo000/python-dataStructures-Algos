9e89e1c46db798c8d032c9e6312799c5fd95a4ce
mdiallo98 <mamadou1237@gmail.com>
2022-07-22 01:25:07 -0400

    coin change

--- LeetcodeQuestions/Dynamic Programing/coin_change.py
+++ LeetcodeQuestions/Dynamic Programing/coin_change.py
@@ -2,7 +2,15 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         #  we will take a bottom up approach to solving this problem
         tab = [amount + 1] * amount + 1
-
+        #  [0,12,12,12,12,12,12,12]
+        #  [1,3,4,5]
         tab[0] = 0
 
         for i in range(1, len(tab)-1):
+            for coin in coins:
+                if tab[i] - coin >= 0:
+                    tab[i] = min(tab[i], tab[i - coin])
+        if tab[amount] == amount + 1:
+            return -1
+        else:
+            return tab[amount]
