b312863f004dcfd0d86b70fa5485b985b3f5484a
mdiallo98 <mamadou1237@gmail.com>
2022-07-21 20:13:46 -0400

    min cost climbing_stairs

--- LeetcodeQuestions/Dynamic Programing/min_cost_climbing_stairs.py
+++ LeetcodeQuestions/Dynamic Programing/min_cost_climbing_stairs.py
@@ -7,3 +7,5 @@ class Solution:
         #  test case [10,15,20, 0]
 
         for i in range(len(cost)-3, -1, -1):
+            cost[i] = min(cost[i] + cost[i+1], cost[i] + cost[i+2])
+        return min(cost[0], cost[1])
