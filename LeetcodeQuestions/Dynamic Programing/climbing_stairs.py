a070f8d7fc716665acd9cc4a97447af828b45770
mdiallo98 <mamadou1237@gmail.com>
2022-07-21 20:10:04 -0400

    climbing_stairs

--- LeetcodeQuestions/Dynamic Programing/climbing_stairs.py
+++ LeetcodeQuestions/Dynamic Programing/climbing_stairs.py
@@ -2,4 +2,9 @@
 
 class Solution:
     def climbStairs(self, n: int) -> int:
-        one, two
+        one, two = 1, 1
+
+        for _ in range(n-1):
+            temp = one
+            one = one + two
+            two = temp
