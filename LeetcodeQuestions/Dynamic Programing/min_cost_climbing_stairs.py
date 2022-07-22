10a6719ce5449edebe655436f01235fbb7b1ede1
mdiallo98 <mamadou1237@gmail.com>
2022-07-21 20:11:23 -0400

    min cost climbing_stairs

--- LeetcodeQuestions/Dynamic Programing/min_cost_climbing_stairs.py
+++ LeetcodeQuestions/Dynamic Programing/min_cost_climbing_stairs.py
@@ -1,2 +1,4 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
+        #  Another DP question
+        #  we will first append 0 to our list to make the computation possible
