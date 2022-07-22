d4af80b2cc2a122b2e3e74ea51f3ce36c00c8c7a
mdiallo98 <mamadou1237@gmail.com>
2022-07-22 01:16:16 -0400

    coin change

--- LeetcodeQuestions/Dynamic Programing/coin_change.py
+++ LeetcodeQuestions/Dynamic Programing/coin_change.py
@@ -2,3 +2,5 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         #  we will take a bottom up approach to solving this problem
         tab = [amount + 1] * amount + 1
+
+        tab[0] = 0
