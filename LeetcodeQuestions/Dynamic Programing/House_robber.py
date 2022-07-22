7ca1c88f7a66739f0deeab3b1d1b5b93172eff0e
mdiallo98 <mamadou1237@gmail.com>
2022-07-21 21:50:46 -0400

    house robber

--- LeetcodeQuestions/Dynamic Programing/House_robber.py
+++ LeetcodeQuestions/Dynamic Programing/House_robber.py
@@ -7,4 +7,16 @@ class Solution:
 
         return self.rob_from(0, nums)
 
-    def rob_from(self, idx, houses)
+    def rob_from(self, idx, houses):
+        #  establish base case, which will be if we run out of houses
+
+        if idx >= len(houses):
+            return 0
+
+        if idx in self.memo:
+            return self.memo[idx]
+
+        ans = max(self.rob_from(idx + 1, houses), self.rob_from(idx+2, houses) + houses[idx])
+
+        self.memo[idx] = ans
+        return ans
