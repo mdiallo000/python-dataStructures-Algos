ef1059ec33b280916e105f1c372585c5d059cf3f
mdiallo98 <mamadou1237@gmail.com>
2022-07-23 21:15:44 -0400

    max product

---
+++ LeetcodeQuestions/Tree/lowest_common_ancestor_btree.py
@@ -0,0 +1,7 @@
+class Solution:
+    def __init__(self):
+            self.ans = None 
+    
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+
+        
