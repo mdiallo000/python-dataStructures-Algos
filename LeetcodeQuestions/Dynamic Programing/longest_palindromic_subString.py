5b37849dff1f04a5bf984ea5c6b136506541b37f
mdiallo98 <mamadou1237@gmail.com>
2022-07-24 10:13:49 -0400

    longest palindrome substrng

--- LeetcodeQuestions/Dynamic Programing/longest_palindromic_subString.py
+++ LeetcodeQuestions/Dynamic Programing/longest_palindromic_subString.py
@@ -6,7 +6,7 @@ class Solution:
         for i in range(len(s)):
             l, r = i, i
 
-            while l >= 0 and r <= len(s) and s[l] == s[r]:
+            while l >= 0 and r < len(s) and s[l] == s[r]:
                 if (r-l + 1) > resLen:
                     res = s[l:r+1]
                     resLen = (r-l + 1)
@@ -15,7 +15,7 @@ class Solution:
 
             l, r = i, i+1
 
-            while l >= 0 and r <= len(s) and s[l] == s[r]:
+            while l >= 0 and r < len(s) and s[l] == s[r]:
                 if (r-l + 1) > resLen:
                     res = s[l:r+1]
                     resLen = (r-l + 1)
