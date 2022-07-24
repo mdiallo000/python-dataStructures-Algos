f678c46570e9d5d86b7198f33756ddf20b6cd4b1
mdiallo98 <mamadou1237@gmail.com>
2022-07-24 10:49:52 -0400

    generate parentheses

--- LeetcodeQuestions/backtracking/generate_parentheses.py
+++ LeetcodeQuestions/backtracking/generate_parentheses.py
@@ -1,2 +1,12 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
+        #  the parameters to generate parentheses is this:
+        #  if at any point we have the same ammount of closed, open parenthese as required by n
+        #  we cant keep adding open brakets if its surpassing n and we can add closed brakets if it will surpass the amount of closing brakets we have
+
+        stack = []
+        res = []
+
+        def generate(openB, closedB)
+
+        if openB == closedB == N:
