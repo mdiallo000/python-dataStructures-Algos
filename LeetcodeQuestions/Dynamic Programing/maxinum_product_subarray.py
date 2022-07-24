80bd4fa3ccc95454604fc3dae49b356df4175409
mdiallo98 <mamadou1237@gmail.com>
2022-07-23 21:17:53 -0400

    max product

--- LeetcodeQuestions/Dynamic Programing/maxinum_product_subarray.py
+++ LeetcodeQuestions/Dynamic Programing/maxinum_product_subarray.py
@@ -2,3 +2,11 @@ class Solution:
     def maxProduct(self, nums: List[int]) -> int:
 
         res = max(nums)
+        curMax, curMin = 1, 1
+
+        for n in nums:
+            if n == 0:
+                continue
+            temp = curMax * n
+            curMax = max(curMax * n, curMin*n, n)
+            curMin = min(temp, curMin * n, n)
