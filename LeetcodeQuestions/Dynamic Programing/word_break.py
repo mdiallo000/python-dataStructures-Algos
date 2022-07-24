f20bb56cf3fe76ac6de89246c833a82e613982a8
mdiallo98 <mamadou1237@gmail.com>
2022-07-23 22:33:44 -0400

    word break

--- LeetcodeQuestions/Dynamic Programing/word_break.py
+++ LeetcodeQuestions/Dynamic Programing/word_break.py
@@ -2,8 +2,15 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
 
         dp = [False] * (len(s) + 1)
-        dp[len(s) + 1] = True
+        dp[-1] = True
 
         for i in range(len(s)-1, -1, -1):
 
-            for word in wordDict
+            for word in wordDict:
+
+                if i + len(word) <= len(s) and s[i: i + len(word)] == word:
+                    dp[i] = dp[i + len(word)]
+                if dp[i]:
+                    break
+
+        return dp[0]
