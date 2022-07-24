7788caef0f178a78117e2ebdb5e371b461a68819
mdiallo98 <mamadou1237@gmail.com>
2022-07-24 09:08:10 -0400

    longest increasing sequence

--- LeetcodeQuestions/Dynamic Programing/longest_increasing_sequence.py
+++ LeetcodeQuestions/Dynamic Programing/longest_increasing_sequence.py
@@ -3,8 +3,8 @@ class Solution:
 
         lis = [1] * len(nums)
 
-        for i in range(len(s)-1, -1, -1):
-            for j in range(i + 1, i):
+        for i in range(len(nums)-1, -1, -1):
+            for j in range(i + 1, len(nums)):
                 if nums[i] < nums[j]:
-                    lis[i] = max(lis[i], lis[j:])
+                    lis[i] = max(lis[i], 1 + lis[j])
         return max(lis)
