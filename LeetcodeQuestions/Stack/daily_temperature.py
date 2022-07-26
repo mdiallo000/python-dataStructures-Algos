fc0e6d6e971a4d00f1338074e1e48a6517dbd199
mdiallo98 <mamadou1237@gmail.com>
2022-07-25 23:02:37 -0400

    rotate list

--- LeetcodeQuestions/Stack/daily_temperature.py
+++ LeetcodeQuestions/Stack/daily_temperature.py
@@ -1,3 +1,13 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         res = [0] * len(temperatures)
+        stack = []
+
+        for idx, elem in enumerate(temperatures):
+
+            while stack and elem > stack[-1][1]:
+                i, e = stack.pop()
+                pos = idx - i
+                res[i] = pos
+            stack.append([idx, elem])
+        return res
---
+++ LeetcodeQuestions/linked_list/rotate_list.py
@@ -0,0 +1,2 @@
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
