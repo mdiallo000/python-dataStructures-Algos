8edde876f34f2c0f6dcb87991c4985d61d2d8c49
mdiallo98 <mamadou1237@gmail.com>
2022-07-25 23:27:39 -0400

    rotate list

--- LeetcodeQuestions/linked_list/rotate_list.py
+++ LeetcodeQuestions/linked_list/rotate_list.py
@@ -8,8 +8,15 @@ class Solution:
         if not head.next:
             return head
         #  Now lets identify our current tail which will become the old tail since the Node at n -k will become the tail
+        #  We also keep a count of the amount of nodes in our linked list in order to know where the new tail will be
+        num_nodes = 0
         old_tail = head
         while old_tail.next:
             old_tail = old_tail.next
         #  when we find the old tail now lets connect it to the head of the linked list
         old_tail.next = head
+
+        #  now its time to find the new tail:
+        new_tail = head
+
+        while ()
