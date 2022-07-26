7151134eb2e2171f6ec5e02b1ac0d89a744435d8
mdiallo98 <mamadou1237@gmail.com>
2022-07-25 23:22:12 -0400

    rotate list

--- LeetcodeQuestions/linked_list/rotate_list.py
+++ LeetcodeQuestions/linked_list/rotate_list.py
@@ -4,3 +4,8 @@ class Solution:
         #  first base case
         if not head:
             return None
+
+        if not head.next:
+            return head
+        #  Now lets identify our current tail which will become the old tail since the Node at n -k will become the tail
+        old_tail = head
