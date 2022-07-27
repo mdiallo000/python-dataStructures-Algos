2bc8f5d1a6ce62af562c20c7baf363c36a6c1a5d
mdiallo98 <mamadou1237@gmail.com>
2022-07-27 15:44:55 -0400

    triangle number

--- QBank/valid_triangle_number.py
+++ QBank/valid_triangle_number.py
@@ -1,2 +1,10 @@
 class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+
+        count = 0
+
+        for i in range(len(nums)-1, 1, -1):
+            curr = nums[i]
+            start = 0
+            end = i - 1
