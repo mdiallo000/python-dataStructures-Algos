a38f831ec58a10e9935ea07972f8bf3e86873f84
mdiallo98 <mamadou1237@gmail.com>
2022-07-28 16:00:36 -0400

    all paths from source to target

--- QBank/all_path_from_source_target.py
+++ QBank/all_path_from_source_target.py
@@ -1,2 +1,11 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+
+        # since we want to go from  0 to => N-1, target node should be
+        target = len(graph) - 1
+
+        result = []
+
+        def DFS(curr, target)
+
+        # best case for when we should return
