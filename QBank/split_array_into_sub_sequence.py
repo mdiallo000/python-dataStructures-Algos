b1efe86933ac786c63ea145f741ef1a3eb872223
mdiallo98 <mamadou1237@gmail.com>
2022-07-29 23:14:48 -0400

    split array into subsequence

--- QBank/split_array_into_sub_sequence.py
+++ QBank/split_array_into_sub_sequence.py
@@ -12,3 +12,12 @@ class Solution:
             # first check is to establish if we still have this number left in our frequency maping
 
             if Frequency[n]:
+                Frequency[n] -= 1
+
+                #  Now if this number can be merged into the previous than we can
+                if end[n - 1]:
+                    end[n] += 1
+                    end[n - 1] -= 1
+
+                elif Frequency[n+1] and Frequency[n+2]:
+                    Frequency[n+1]
