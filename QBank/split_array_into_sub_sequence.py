3143a0dfdd6542eb4048209f770f0f0d6d0506c0
mdiallo98 <mamadou1237@gmail.com>
2022-07-29 23:03:49 -0400

    split array into subsequence

--- QBank/split_array_into_sub_sequence.py
+++ QBank/split_array_into_sub_sequence.py
@@ -1,2 +1,14 @@
+from collections import defaultdict
+from email.policy import default
+
+
 class Solution:
     def isPossible(self, nums: List[int]) -> bool:
+
+        Frequency = Counter(nums)
+        end = defaultdict()
+
+        for n in nums:
+            # first check is to establish if we still have this number left in our frequency maping
+
+            if Frequency[n]:
