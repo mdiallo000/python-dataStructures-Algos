d79177878b04d0318d7be8492c0bc9426ca74840
mdiallo98 <mamadou1237@gmail.com>
2022-07-30 22:59:19 -0400

    max stack

--- QBank/Max_Stack.py
+++ QBank/Max_Stack.py
@@ -1,1 +1,14 @@
+
 class MaxStack(list):
+
+    def __init__(self):
+
+    def push(self, x: int) -> None:
+
+    def pop(self) -> int:
+
+    def top(self) -> int:
+
+    def peekMax(self) -> int:
+
+    def popMax(self) -> int:
