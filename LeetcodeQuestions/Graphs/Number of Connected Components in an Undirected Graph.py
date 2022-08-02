6b12a4c36b3d01b2b1215b7e80d06ad4b2cffe58
mdiallo98 <mamadou1237@gmail.com>
2022-08-01 22:59:56 -0400

    connected component

--- LeetcodeQuestions/Graphs/Number of Connected Components in an Undirected Graph.py
+++ LeetcodeQuestions/Graphs/Number of Connected Components in an Undirected Graph.py
@@ -4,3 +4,6 @@ from collections import defaultdict
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
         graph = defaultdict(list)
+        for n, e in edges:
+            graph[n].append(e)
+            graph[e].append(n)
