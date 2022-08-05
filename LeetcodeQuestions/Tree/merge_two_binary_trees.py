e5e23a59aff99454483b12fc1403bdcf94182c02
mdiallo98 <mamadou1237@gmail.com>
2022-08-04 22:06:37 -0400

    merge two binary trees

--- LeetcodeQuestions/Tree/merge_two_binary_trees.py
+++ LeetcodeQuestions/Tree/merge_two_binary_trees.py
@@ -1,1 +1,10 @@
+from logging import root
+
+
 class Solution:
+    def mergeTwoBinaryTree(root1, root2):
+
+        if not root1:
+            return root2
+        if not root2:
+            return root1
