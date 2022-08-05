8e676807bb2d1397ba0ebfeec3e5aea1e334d24f
mdiallo98 <mamadou1237@gmail.com>
2022-08-04 22:08:49 -0400

    merge two binary trees

--- LeetcodeQuestions/Tree/merge_two_binary_trees.py
+++ LeetcodeQuestions/Tree/merge_two_binary_trees.py
@@ -8,3 +8,10 @@ class Solution:
             return root2
         if not root2:
             return root1
+
+        root1.val += root2
+
+        root1.left = self.mergeTwoBinaryTree(root1.left, root2.left)
+        root1.right = self.mergeTwoBinaryTree(root1.right, root2.right)
+
+        return root1
