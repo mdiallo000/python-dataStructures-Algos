4ac1f2af1fb75c56a8a29d545fe2a4a9e3be3738
mdiallo98 <mamadou1237@gmail.com>
2022-08-12 15:38:56 -0400

    N-queen

--- LeetcodeQuestions/backtracking/N-Queen.py
+++ LeetcodeQuestions/backtracking/N-Queen.py
@@ -17,3 +17,10 @@ class Solution:
 
                 if c in colum or (r + c) in posDiag or (r - c) in negDiag:
                     continue
+                colum.add(c)
+                posDiag.add(r + c)
+                negDiag.add(r-c)
+                Backtrack(c + 1)
+                colum.remove(c)
+                posDiag.remove(r + c)
+                negDiag.remove(r-c)
