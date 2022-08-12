8a7df90ac193b32cb683b53dd18fec2245a9c2f7
mdiallo98 <mamadou1237@gmail.com>
2022-08-11 22:00:43 -0400

    sort the sentence

--- LeetcodeQuestions/String_Problems/sorting_the_sentence.py
+++ LeetcodeQuestions/String_Problems/sorting_the_sentence.py
@@ -1,2 +1,8 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
+        #  we are giving a sentence, each word in the sentence ends with a number which indicates the odering of that word if the sentence was to be sorted
+        #  Our objective is to capture the last char in each word and use it to place the other words in the sentence
+        #  i can use a dictionary, take the last char and use it as the key, finally i will map each word excluding the last char as the value
+
+        words = s.split(' ')
+        Map = {}
