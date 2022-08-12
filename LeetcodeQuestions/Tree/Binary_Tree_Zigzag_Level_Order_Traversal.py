ff12efe93e3294440ff5a5387a6c2e2ceb23d9b5
mdiallo98 <mamadou1237@gmail.com>
2022-08-11 21:57:17 -0400

    sort the sentence

--- LeetcodeQuestions/Tree/Binary_Tree_Zigzag_Level_Order_Traversal.py
+++ LeetcodeQuestions/Tree/Binary_Tree_Zigzag_Level_Order_Traversal.py
@@ -11,11 +11,23 @@ class Solution:
 
         left_right = True
         node_queue = deque([root, None])
-        while node_queue:
+        while len(node_queue) > 0:
             curr = node_queue.popleft()
             if curr:
                 if left_right:
                     level.append(curr.val)
                 else:
                     level.appendleft(curr.val)
+                if curr.left:
+                    node_queue.append(curr.left)
+                if curr.right:
+                    node_queue.append(curr.right)
+
             else:
+                res.append(level)
+                if len(node_queue) > 0:
+                    node_queue.append(None)
+
+                level = deque()
+                left_right = not left_right
+        return res
