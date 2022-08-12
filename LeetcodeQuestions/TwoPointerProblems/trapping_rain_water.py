6f197b3a2be5e1af7fbe778fe8eea265d9bfac8c
mdiallo98 <mamadou1237@gmail.com>
2022-08-12 14:16:51 -0400

    trapping rain water

--- LeetcodeQuestions/TwoPointerProblems/trapping_rain_water.py
+++ LeetcodeQuestions/TwoPointerProblems/trapping_rain_water.py
@@ -1,5 +1,11 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
 
-        minHeight = []
-        maxHeight = []
+        maxLeft = []
+        maxRight = []
+        maxH = 0
+
+        for i in range(len(height)):
+
+            maxH = max(maxH, height[i])
+            maxLeft.append(maxH)
