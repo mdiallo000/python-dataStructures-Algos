12133d542ecde25fa693f048b2e50d6df64ccbea
mdiallo98 <mamadou1237@gmail.com>
2022-08-12 15:37:28 -0400

    N-queen

--- LeetcodeQuestions/backtracking/N-Queen.py
+++ LeetcodeQuestions/backtracking/N-Queen.py
@@ -1,2 +1,19 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
+
+        colum = set()
+        posDiag = set()
+        negDiag = set()
+        res = []
+        board = [[""] * n for i in range(n)]
+
+        def Backtrack(r):
+            if r == n:
+                copy = ["".join(row) for row in range(r)]
+                res.append(copy)
+                return
+
+            for c in range(n):
+
+                if c in colum or (r + c) in posDiag or (r - c) in negDiag:
+                    continue
