a46edfbe98d09207a64cf82b2ae874eb102b1fdb
mdiallo98 <mamadou1237@gmail.com>
2022-08-12 01:21:10 -0400

    sum root to leaf number

--- LeetcodeQuestions/Tree/sum_root_to_leaf_number.py
+++ LeetcodeQuestions/Tree/sum_root_to_leaf_number.py
@@ -1,4 +1,11 @@
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        total = 0
 
         def preorder(node, curr_number):
+            nonlocal total
+            if node:
+
+                curr_number = curr_number * 10 + node.val
+
+                if not node.left or node.right:
