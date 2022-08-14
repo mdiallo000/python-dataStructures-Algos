e76c8defe80030720fed5a2b0cdad4b6e5bdd88b
mdiallo98 <mamadou1237@gmail.com>
2022-08-13 23:09:26 -0400

    min sub array sum

--- LeetcodeQuestions/SlidingWindow/min_subarray_sum.py
+++ LeetcodeQuestions/SlidingWindow/min_subarray_sum.py
@@ -4,5 +4,11 @@ class Solution:
         l, r = 0, 0
         res = 0
         count = 0
+        total = 0
 
         while r < len(nums):
+            total += nums[r]
+            if total < target:
+                r += 1
+            else:
+                res = min(res, r - l + 1)
