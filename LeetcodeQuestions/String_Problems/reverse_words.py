a144cd729b219ddaaa614c4ac7fdf6dccdaaace6
mdiallo98 <mamadou1237@gmail.com>
2022-08-26 23:25:46 -0400

    reverse word

--- LeetcodeQuestions/String_Problems/reverse_words.py
+++ LeetcodeQuestions/String_Problems/reverse_words.py
@@ -1,2 +1,8 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
+        res = s.split()
+        ans = []
+
+        for i in range(len(res)-1, -1, -1):
+            ans.append(res[i])
+        return ' '.join(ans)
