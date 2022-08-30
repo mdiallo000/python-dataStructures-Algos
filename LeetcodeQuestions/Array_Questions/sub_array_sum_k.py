c3f2422e1ef577f0d30db79141ae38307446f655
mdiallo98 <mamadou1237@gmail.com>
2022-08-30 14:47:54 -0400

    subarray sum k

--- LeetcodeQuestions/Array_Questions/sub_array_sum_k.py
+++ LeetcodeQuestions/Array_Questions/sub_array_sum_k.py
@@ -1,2 +1,17 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        start = 0
+        end = -1
+        total = 0
+        maxLength = 0
+        while start < n:
+            while end + 1 < n and total + arr[end + 1] <= k:
+                total += arr[end += 1]
+
+            if total == k:
+                maxLength = max(maxLength, (end - start + 1))
+
+            total -= arr[start]
+            start++
+
+        return maxLength
