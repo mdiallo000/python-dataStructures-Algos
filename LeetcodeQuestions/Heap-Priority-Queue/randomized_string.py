ec5fe7227e3d60215a313ce68a5b6fba5cd9f24a
mdiallo98 <mamadou1237@gmail.com>
2022-08-29 23:16:55 -0400

    reorganize string

--- LeetcodeQuestions/Heap-Priority-Queue/randomized_string.py
+++ LeetcodeQuestions/Heap-Priority-Queue/randomized_string.py
@@ -1,5 +1,12 @@
+from collections import deque
+import heapq
+import deque
+
+
 class Solution:
     def randomizedString(s):
         count = Counter(s)
         heap = [(-val, key) for key, val in count.items()]
         heapq.heapify(heap)
+
+        q = deque()
