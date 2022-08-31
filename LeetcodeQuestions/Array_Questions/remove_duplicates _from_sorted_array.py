45be3f876788633121c7f948ae281d15af998574
mdiallo98 <mamadou1237@gmail.com>
2022-08-31 19:46:48 -0400

    remove duplicates in sorted array

--- LeetcodeQuestions/Array_Questions/remove_duplicates _from_sorted_array.py
+++ LeetcodeQuestions/Array_Questions/remove_duplicates _from_sorted_array.py
@@ -2,5 +2,11 @@ class Solution:
     #  The ultimate goal is to use O(1) extra space
     #  If this requirement didnt exist then we could simply use a set and filter out the duplicate values
     def removeDuplicates(self, nums: List[int]) -> int:
-
+        i = 0
         for j in range(1, len(nums)):
+
+            if nums[i] != nums[j]:
+                i += 1
+                nums[i] = nums[j]
+
+        return i + 1
