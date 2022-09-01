3519cff6c289536b7172b031886fa8a4edb3fc8f
mdiallo98 <mamadou1237@gmail.com>
2022-08-31 22:50:03 -0400

    Union of two sorted arrays

---
+++ LeetcodeQuestions/Array_Questions/Union_of_Two_Sorted_Arrays.py
@@ -0,0 +1,5 @@
+class Solution:
+    def findUnion(self, arr1, arr2):
+        # Problem Statement: Given two sorted arrays, arr1 and arr2 of size n and m. Find the union of two sorted arrays.
+        set1 = set(arr1)
+        set2 = set(arr2)
