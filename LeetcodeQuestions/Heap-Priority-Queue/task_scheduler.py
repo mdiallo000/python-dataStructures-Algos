995d2857971f7f41fb4acf686d266abde6afdf6a
mdiallo98 <mamadou1237@gmail.com>
2022-09-01 10:49:40 -0400

    task scheduler

--- LeetcodeQuestions/Heap-Priority-Queue/task_scheduler.py
+++ LeetcodeQuestions/Heap-Priority-Queue/task_scheduler.py
@@ -1,4 +1,11 @@
+from collections import deque
+
+
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
 
         count = Counter(tasks)
+        maxHeap = [-cnt for cnt in count.values()]
+        heapq.heapify(maxheap)
+        time = 0
+        q = deque()
