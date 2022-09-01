bc512fd69fdc3fc9f5a7e9eba5c0cfbaac009805
mdiallo98 <mamadou1237@gmail.com>
2022-08-31 20:11:44 -0400

    remove duplicates in sorted array

--- LeetcodeQuestions/Array_Questions/determine_if_array_is_sorted.py
+++ LeetcodeQuestions/Array_Questions/determine_if_array_is_sorted.py
@@ -1,2 +1,4 @@
 class Solution:
     def check(self, nums: List[int]) -> bool:
+        # Determine wheter or not the array is sorted even after being rotated
+        # The approach i took was to use pointers and partion the list in different havves and determine whether the sorting is consistent
