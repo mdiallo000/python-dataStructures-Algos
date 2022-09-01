924c2e0c7f15421d322319d4c3cac4c50c37db79
mdiallo98 <mamadou1237@gmail.com>
2022-08-31 20:15:46 -0400

    is rotated array sorted

--- LeetcodeQuestions/Array_Questions/determine_if_array_is_sorted.py
+++ LeetcodeQuestions/Array_Questions/determine_if_array_is_sorted.py
@@ -2,3 +2,12 @@ class Solution:
     def check(self, nums: List[int]) -> bool:
         # Determine wheter or not the array is sorted even after being rotated
         # The approach i took was to use pointers and partion the list in different havves and determine whether the sorting is consistent
+
+        count = 0
+        size = len(nums)
+        for i in range(size):
+
+            if nums[i] > nums[i+1 % size-1]:
+                count += 1
+
+        return False if count > 1 else True
