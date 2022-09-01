da40a385ee414380713890e52e0225bc0eb420c4
mdiallo98 <mamadou1237@gmail.com>
2022-09-01 00:27:29 -0400

    reorder linked list

--- LeetcodeQuestions/linked_list/reorder_list.py
+++ LeetcodeQuestions/linked_list/reorder_list.py
@@ -2,9 +2,19 @@
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
 
-        # phase one find middle
+        # phase one: find middle
         slow, fast = head, head.next
 
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
+
+        # phase two: reverse the second half
+        middle = slow.next
+        prev = slow.next = None
+
+        while middle:
+            tmp = middle.next
+            middle.next = prev
+            prev = middle
+            middle = tmp
