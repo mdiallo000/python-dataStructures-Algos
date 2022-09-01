1c29e9b775dea5d294bd94fff2c79fb2add3864a
mdiallo98 <mamadou1237@gmail.com>
2022-09-01 10:56:47 -0400

    task scheduler

--- LeetcodeQuestions/Heap-Priority-Queue/task_scheduler.py
+++ LeetcodeQuestions/Heap-Priority-Queue/task_scheduler.py
@@ -8,12 +8,17 @@ class Solution:
 
         count = Counter(tasks)
         maxHeap = [-cnt for cnt in count.values()]
-        heapq.heapify(maxheap)
+        heapq.heapify(maxHeap)
         time = 0
         q = deque()
 
-        while heap or q:
+        while maxHeap or q:
             time += 1
 
             if maxHeap:
                 cnt = 1 + heapq.heappop(maxHeap)
+                if cnt:
+                    q.append([cnt, n + time])
+            if q and q[0][1] == time:
+                heapq.heappush(maxHeap, q.popleft()[0])
+        return time
