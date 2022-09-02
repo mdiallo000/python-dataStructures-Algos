9280adbec0d04c4e070a6f37cae56129987aa748
mdiallo98 <mamadou1237@gmail.com>
2022-09-02 13:51:31 -0400

    find median in stream

--- LeetcodeQuestions/Heap-Priority-Queue/find_median_in_stream.py
+++ LeetcodeQuestions/Heap-Priority-Queue/find_median_in_stream.py
@@ -1,3 +1,6 @@
+import heapq
+
+
 class MedianFinder:
 
     def __init__(self):
@@ -6,6 +9,10 @@ class MedianFinder:
     def addNum(self, num: int) -> None:
         #  in order to maintain the heaps in a way where the median can be easily found we need to change the conditions of both heaps depending on whether the max heap has two more elements then the min heap and vice-versa
         heapq.heappush(self.small, -1 * num)
+
+        if (self.small and self.large and -1 * self.small[0] < self.large[0]):
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
 
     def findMedian(self) -> float:
         if self.heap
