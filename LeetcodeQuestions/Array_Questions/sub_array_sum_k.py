b779f806ce03f021f44ad0f2e2d8ae2184dfc15e
mdiallo98 <mamadou1237@gmail.com>
2022-09-03 13:05:29 -0400

    number of subarrays with sum of k

--- LeetcodeQuestions/Array_Questions/sub_array_sum_k.py
+++ LeetcodeQuestions/Array_Questions/sub_array_sum_k.py
@@ -5,9 +5,15 @@ from email.policy import default
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         preSum = 0
-        cache = defaultdict(int)
-
+        cache = {0: 1}
+        res = 0
         for num in nums:
             preSum += num
 
-            if preSum
+            if preSum - k in cache:
+                res += cache[preSum - k]
+            if preSum not in cache:
+                cache[preSum] = 1
+            else:
+                cache[preSum] = cache[preSum]+1
+        return res
