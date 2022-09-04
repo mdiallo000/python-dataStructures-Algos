b7a65b1938404606ee0a84f37d8d50b0b555d476
mdiallo98 <mamadou1237@gmail.com>
2022-09-04 15:59:02 -0400

    set matrix zeroes

--- LeetcodeQuestions/Array_Questions/set_matrix.py
+++ LeetcodeQuestions/Array_Questions/set_matrix.py
@@ -3,3 +3,18 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        RowSet = set()
+        ColumnSet = set()
+        row = len(matrix)
+        column = len(matrix[0])
+
+        for r in range(row):
+            for c in range(column):
+                if matrix[r][c] == 0:
+                    RowSet.add(r)
+                    ColumnSet.add(c)
+
+        for r in range(row):
+            for c in range(column):
+                if r in RowSet or c in ColumnSet:
+                    matrix[r][c] = 0
