7f091cc2ef8459b28aa23a3b6af62e6c949e5152
mdiallo98 <mamadou1237@gmail.com>
2022-09-04 15:09:46 -0400

    top k element

--- LeetcodeQuestions/Hash_Table_Dict/top_k_frequent_element.py
+++ LeetcodeQuestions/Hash_Table_Dict/top_k_frequent_element.py
@@ -4,40 +4,44 @@
 import heapq
 
 
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # sample array we get [1,1,1,2,3,3,4,5,5,5]
-        # goal is to bring back a K amount of the most freqent elements in the list
-        # So first step is to map the element to how frequent it appears in the list
+def topKFrequent(nums: List[int], k: int) -> List[int]:
+    # sample array we get [1,1,1,2,3,3,4,5,5,5]
+    # goal is to bring back a K amount of the most freqent elements in the list
+    # So first step is to map the element to how frequent it appears in the list
 
-        myMap = {}
-        myHeap = []
+    myMap = {}
+    myHeap = []
 
-        for elem in nums:
-            if elem not in myMap:
-                myMap[elem] = 1
-            else:
-                myMap[elem] += 1
-        #  Now that the array has been populated we can now use a short cut to return the largest frequencies determine by K
-        # return heapq.nlargest(k, myMap.keys(), key=myMap.get())
+    for elem in nums:
+        if elem not in myMap:
+            myMap[elem] = 1
+        else:
+            myMap[elem] += 1
+    #  Now that the array has been populated we can now use a short cut to return the largest frequencies determine by K
+    # return heapq.nlargest(k, myMap.keys(), key=myMap.get())
 
-        for elem, freq in myMap.items():
-            if len(myHeap) < K:
-                heapq.heappush(myHeap, [freq, elem])
-            else:
-                heapq.heappop(myHeap, [freq, elem])
+    for elem, freq in myMap.items():
+        if len(myHeap) < K:
+            heapq.heappush(myHeap, [freq, elem])
+        else:
+            heapq.heappop(myHeap, [freq, elem])
 
-        #  [[3,1],[3,5]]
-        return [elem for value, key in myHeap]
-    # Another approach this time using bucket sort
+    #  [[3,1],[3,5]]
+    return [elem for value, key in myHeap]
+# Another approach this time using bucket sort
 
-    def TopKElement(nums, k):
 
-        count = Counter(nums)
-        # [1,1,1,3,3,2,5]
-        bucket = [[] for i in range(len(nums) + 1)]
+def TopKElement(nums, k):
 
-        # [N,N,N,N,N,N]
-        #  0 1 2 3 4 5
-        for key, val in count.items():
-            bucket[val].append(key)
+    count = Counter(nums)
+    # [1,1,1,3,3,2,5]
+    bucket = [[] for i in range(len(nums) + 1)]
+
+    # [N,N,N,N,N,N]
+    #  0 1 2 3 4 5
+    for key, val in count.items():
+        bucket[val].append(key)
+    print(bucket)
+
+
+TopKElement([1, 1, 1, 3, 3, 2, 5], 2)
