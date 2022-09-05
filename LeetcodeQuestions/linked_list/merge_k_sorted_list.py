58186c5a6fd95078dc7c54a24b4a9bfa9d3d5ea6
mdiallo98 <mamadou1237@gmail.com>
2022-09-05 15:05:56 -0400

    merge K sorted List

--- LeetcodeQuestions/linked_list/merge_k_sorted_list.py
+++ LeetcodeQuestions/linked_list/merge_k_sorted_list.py
@@ -1,4 +1,16 @@
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        #  taking care of edge cases
+        if not lists or len(list) == 0:
+            return None
+        # now while we have lists remaining
+        while len(lists) > 1:
+            mergedLists = []
+
+            for i in range(0, len(lists), 2):
+                #  we increment by two since we want pairs
+                l1 = lists[i]
+                l2 = lists[i + 1] if (i + 1) < len(lists) else None
+                mergedLists.append(mergedLists(l1, l2))
 
     def mergeList(self, l1, l2)
