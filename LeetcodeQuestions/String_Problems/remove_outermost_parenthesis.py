8c84bdb6a576b9199eb9d8716b102d2300f82e02
mdiallo98 <mamadou1237@gmail.com>
2022-09-05 14:36:27 -0400

    remove outer parenthesis

--- LeetcodeQuestions/String_Problems/remove_outermost_parenthesis.py
+++ LeetcodeQuestions/String_Problems/remove_outermost_parenthesis.py
@@ -2,3 +2,9 @@ class Solution:
     def removeOuterParentheses(self, s: str) -> str:
         res = ""
         count = 0
+
+        for char in s:
+            if char == "(":
+                count += 1
+                if count > 1:
+                    res += char
