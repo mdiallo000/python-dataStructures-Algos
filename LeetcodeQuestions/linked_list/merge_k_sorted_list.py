5ad560154077389d513643055744b14e3b63a259
mdiallo98 <mamadou1237@gmail.com>
2022-09-05 15:10:13 -0400

    merge K sorted List

--- LeetcodeQuestions/linked_list/merge_k_sorted_list.py
+++ LeetcodeQuestions/linked_list/merge_k_sorted_list.py
@@ -12,5 +12,13 @@ class Solution:
                 l1 = lists[i]
                 l2 = lists[i + 1] if (i + 1) < len(lists) else None
                 mergedLists.append(mergedLists(l1, l2))
+            lists = mergedLists
+        return lists[0]
 
-    def mergeList(self, l1, l2)
+    def mergeList(self, l1, l2):
+        dummy = ListNode()
+        tail = dummy
+        while l1 or l2:
+            if l1.val < l2.val:
+                tail.next, l1 = l1, l1.next
+            else:
