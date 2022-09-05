5c7d6c215a62761e035984ca5e2bab8d74d3060e
mdiallo98 <mamadou1237@gmail.com>
2022-09-05 14:47:20 -0400

    remove outer parenthesis

--- LeetcodeQuestions/String_Problems/remove_outermost_parenthesis.py
+++ LeetcodeQuestions/String_Problems/remove_outermost_parenthesis.py
@@ -10,7 +10,9 @@ class Solution:
                     res += char
             if char == ")":
                 count -= 1
-                if count == 0:
+                if count > 0:
                     res += char
 
         return res
+    #  Lessons learned
+        #  we can solve the problem in linear time by keeping a count of the amount of closed and open brackets that we see, by doing so we can ignore the outer parentheses based on how balanced things are
