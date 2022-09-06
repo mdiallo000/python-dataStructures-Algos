a1a14a9dad2cf08c9b3172d53e31b25cbf9f5b24
mdiallo98 <mamadou1237@gmail.com>
2022-09-06 09:06:14 -0400

    endcode and decode string

---
+++ LeetcodeQuestions/Hash_Table_Dict/encode_and_decode_strings.py
@@ -0,0 +1,8 @@
+class Codec:
+    def encode(self, strs: List[str]) -> str:
+        """Encodes a list of strings to a single string.
+        """
+
+    def decode(self, s: str) -> List[str]:
+        """Decodes a single string to a list of strings.
+        """
--- spoj/life_the_universe_everything.py
+++ spoj/life_the_universe_everything.py
@@ -12,5 +12,15 @@ def main():
             print(val)
 
 
-if __name__ == 'main':
-    main()
+# if __name__ == '__main__':
+#     main()
+def anagrams(S):
+    d = {}
+    for word in S:
+        s = "".join(sorted(word))
+        if s in d:
+            d[s].append(word)
+        else:
+            d[s] = [word]
+
+    return [d[s] for s in d if len(d[s]) > 1]
