28bf3a7f5de3adb32d4f16c4893fda89fdd7c4c2
mdiallo98 <mamadou1237@gmail.com>
2022-09-06 09:43:49 -0400

    endcode and decode string

--- LeetcodeQuestions/Hash_Table_Dict/encode_and_decode_strings.py
+++ LeetcodeQuestions/Hash_Table_Dict/encode_and_decode_strings.py
@@ -4,7 +4,7 @@ class Codec:
         """
         #  we get a list of words our goal is to create a single word from the list of words in a way that it can be then decode into the original list. The only way this can happen is if we use a delimiter in between the words so that we can differentiate them
         # edge cases:
-        #  we can't just use any delimiter since it is possible that the character we choose to be the delimiter can also be presented in the original word, this of course can create a problem
+        #  we can't just use any delimiter since it is possible that the character we choose to be the delimiter can also be presented in the original word, this of course can create a problem. However we store information about the lenght of the word with the word then reconstructing it back will be much easier
         # exammple: ['neet', 'code']
         res = ""
 
@@ -15,7 +15,13 @@ class Codec:
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
-        # the decode method is meant to accept the string that was encoded and its goal is to return the original list of words that was used to create the string
+        # the decode method is meant to accept the string that was encoded and its goal is to return the original list of words that was used to create the string.
         res, i = [], 0
         while i < len(s):
-            j = 0
+            j = i
+            while s[j] != '#':
+                j += 1
+            lenght = int(s[i:j])
+            res.append(s[j+1: j+1 + lenght])
+            i = j + 1 + lenght
+        return res
