95d7a05035dba6db5480ed60866ad5e4335eb202
mdiallo98 <mamadou1237@gmail.com>
2022-09-07 23:20:03 -0400

    longest common prefix

--- LeetcodeQuestions/String_Problems/longest_common_prefix.py
+++ LeetcodeQuestions/String_Problems/longest_common_prefix.py
@@ -1,2 +1,12 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs) == 0:
+            return ''
+        res = ''
+        strs = sorted(strs)
+        for i in strs[0]:
+            if strs[-1].startswith(res+i):
+                res += i
+            else:
+                break
+        return res
