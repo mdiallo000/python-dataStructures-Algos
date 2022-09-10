0d696a2b8eec13c6bbf095c0679eead7f9dbb2b0
mdiallo98 <mamadou1237@gmail.com>
2022-09-10 15:56:17 -0400

    contiguous array

--- LeetcodeQuestions/Array_Questions/contiguous_array.py
+++ LeetcodeQuestions/Array_Questions/contiguous_array.py
@@ -1,17 +1,14 @@
 
-from curses import window
-
-
 def findMaxLength(nums):
-    r = l = 0
-    one_count = zero_count = 0
-    window = 0
-    while r < len(nums)-1:
+    count = 0
+    window_len = 0
+    hash_table = {}
+    for i in range(len(nums)):
+        if nums[i] == 0:
+            count -= 1
+        if nums[i] == 1:
+            count += 1
 
-        if nums[r] == 0:
-            zero_count += 1
-        if nums[r] == 1:
-            one_count += 1
-        if one_count > 1 and zero_count > 1 and zero_count == one_count:
-            window = max(window, r + 1)
-    return window
+        if count == 0:
+            window_len = i + 1
+        if count in hash_table:
