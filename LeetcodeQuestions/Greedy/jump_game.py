1a9ac8ce02f236f8c5318782530b1820303659cf
mdiallo98 <mamadou1237@gmail.com>
2022-09-15 21:51:42 -0400

    jump gameII

--- LeetcodeQuestions/Greedy/jump_game.py
+++ LeetcodeQuestions/Greedy/jump_game.py
@@ -2,9 +2,11 @@ class Solution:
     def canJump(self, nums: List[int]) -> bool:
 
         goal = len(nums) - 1
-
         for i in range(len(nums) - 1, -1, -1):
 
             if i + nums[i] >= goal:
                 goal = i
         return True if goal == 0 else False
+        # Time complexity for this solution is O(n) and constant space
+
+    #  Intuition
