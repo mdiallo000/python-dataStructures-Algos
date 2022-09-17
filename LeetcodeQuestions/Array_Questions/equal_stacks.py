9562c2ca609e950e30a2730214acd851139df69a
mdiallo98 <mamadou1237@gmail.com>
2022-09-17 12:44:58 -0400

    equal_stacks

--- LeetcodeQuestions/Array_Questions/equal_stacks.py
+++ LeetcodeQuestions/Array_Questions/equal_stacks.py
@@ -1,4 +1,13 @@
-class Solution:
+def equal_stacks(arr1, arr2, arr3):
+    h1, h2, h3 = sum(arr1), sum(arr2), sum(arr3)
+    minh = min(h1, h2, h3)
 
-    def equal_stacks(self, arr1, arr2, arr3):
-        h1, h2, h3 = sum(arr1), sum(arr2), sum(arr3)
+    while minh > 0:
+
+    print(h1, h2, h3)
+
+
+h1 = [3, 2, 1, 1, 1]
+h2 = [4, 3, 2]
+h3 = [1, 1, 4, 1]
+equal_stacks(h1, h2, h3)
