ef18528a93bbcede0740f207bc1ad1269347404d
mdiallo98 <mamadou1237@gmail.com>
2022-09-17 13:43:19 -0400

    jump gameII

--- LeetcodeQuestions/Greedy/jump_gameII.py
+++ LeetcodeQuestions/Greedy/jump_gameII.py
@@ -6,7 +6,7 @@ def jump(nums):
     while r < len(nums)-1:
         farthest = 0
         for i in range(l, r+1):
-            farthest = max(farthest, i + nums[r])
+            farthest = max(farthest, i + nums[i])
 
         l = r + 1
         r = farthest
