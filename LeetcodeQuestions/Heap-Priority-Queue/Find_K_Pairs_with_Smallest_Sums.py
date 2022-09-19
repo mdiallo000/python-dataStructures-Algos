ca8663bdd82ebd9bf5185a298aefefe8bc93688b
mdiallo98 <mamadou1237@gmail.com>
2022-09-19 18:23:19 -0400

    k smallest pairs

--- LeetcodeQuestions/Heap-Priority-Queue/Find_K_Pairs_with_Smallest_Sums.py
+++ LeetcodeQuestions/Heap-Priority-Queue/Find_K_Pairs_with_Smallest_Sums.py
@@ -3,17 +3,24 @@ import heapq
 
 
 def kSmallestPairs(nums1, nums2, k):
-    #  maybe two use heaps simoultanously
 
     heapq.heapify(nums1)
     heapq.heapify(nums2)
     res = []
     maxSum = 0
+
     while nums1 or nums2:
 
         for _ in range(k):
             one = heapq.heappop(nums1)
             second = heapq.heappop(nums2)
             maxSum = one + second
-
             res.append([one, second])
+    return res
+
+
+nums1 = [1, 7, 11]
+nums2 = [2, 4, 6]
+k = 3
+ans = kSmallestPairs(nums1, nums2, k)
+print(ans)
