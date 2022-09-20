b5227c7612eae67b77ab2bec2a14601c03b64d79
mdiallo98 <mamadou1237@gmail.com>
2022-09-20 14:39:12 -0400

    search Insert

--- LeetcodeQuestions/Binary_Search/search_insert_position.py
+++ LeetcodeQuestions/Binary_Search/search_insert_position.py
@@ -2,3 +2,17 @@ class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
 
         #  we use regular binary search but instead of returning -1 if the target found, we instead return the L pointer
+
+        l, r = 0, len(nums)-1
+
+        while l <= r:
+
+            mid = (r + l)//2
+
+            if target == nums[mid]:
+                return mid
+            elif target < nums[mid]:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return l
