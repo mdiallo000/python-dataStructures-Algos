894975c39a46917c96a710e04ca91087b995a860
mdiallo98 <mamadou1237@gmail.com>
2022-09-20 17:20:56 -0400

    time based key value store

--- LeetcodeQuestions/Binary_Search/time_based_key_value_store.py
+++ LeetcodeQuestions/Binary_Search/time_based_key_value_store.py
@@ -4,8 +4,19 @@ from collections import defaultdict
 class TimeMap:
 
     def __init__(self):
-        self.storage = defaultdict(dict)
+        self.storage = defaultdict(SortedDict())
 
     def set(self, key: str, value: str, timestamp: int) -> None:
 
+        self.storage[key][timestamp] = value
+
     def get(self, key: str, timestamp: int) -> str:
+
+        if key not in self.storage:
+            return ''
+
+        for curr in reversed(range(1, timestamp + 1)):
+            if curr in self.storage[key]:
+                return self.storage[key][curr]
+
+        return ""
