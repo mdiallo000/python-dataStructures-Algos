b3846a58193df18614fd0ffdcf974268204ebe5c
mdiallo98 <mamadou1237@gmail.com>
2022-09-20 15:01:36 -0400

    find first and last occurance

--- LeetcodeQuestions/Binary_Search/find_first_and_last_occurance.py
+++ LeetcodeQuestions/Binary_Search/find_first_and_last_occurance.py
@@ -1,2 +1,12 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
+        #  My intial woprking solution
+
+        l, r = 0, len(nums)-1
+
+        while l <= r:
+            mid = r + l // 2
+
+            if target == nums[mid]:
+                while nums[l] != target:
+                    l += 1
