2024b6cf05faa64460fddfeeb1ca9ed89777a169
mdiallo98 <mamadou1237@gmail.com>
2022-09-20 15:16:35 -0400

    find first and last occurance

--- LeetcodeQuestions/Binary_Search/find_first_and_last_occurance.py
+++ LeetcodeQuestions/Binary_Search/find_first_and_last_occurance.py
@@ -1,6 +1,6 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        #  My intial woprking solution
+        #  My initial working solution
 
         l, r = 0, len(nums)-1
 
@@ -18,3 +18,6 @@ class Solution:
             else:
                 l = mid + 1
         return [-1, -1]
+        # this would solve the problem but its really not a log(n) time complexity since some edge cases will lead us to linearly scan even after we have found the midpoint
+
+        # The best approach would be to use to binary searches. One to find the lower bound occurance and the other to find the upper bound.
