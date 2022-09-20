4e651b37cda6842678d7cad71beb38b10ba05977
mdiallo98 <mamadou1237@gmail.com>
2022-09-20 15:02:48 -0400

    find first and last occurance

--- LeetcodeQuestions/Binary_Search/find_first_and_last_occurance.py
+++ LeetcodeQuestions/Binary_Search/find_first_and_last_occurance.py
@@ -10,3 +10,11 @@ class Solution:
             if target == nums[mid]:
                 while nums[l] != target:
                     l += 1
+                while nums[r] != target:
+                    r -= 1
+                return [l, r]
+            elif target < nums[mid]:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return [-1, -1]
