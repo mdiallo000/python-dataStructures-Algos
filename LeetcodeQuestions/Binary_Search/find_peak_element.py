5e0fbd40940b4ffb767ac59ff9b72c5c1777aa36
mdiallo98 <mamadou1237@gmail.com>
2022-09-21 19:36:43 -0400

    find peak element

--- LeetcodeQuestions/Binary_Search/find_peak_element.py
+++ LeetcodeQuestions/Binary_Search/find_peak_element.py
@@ -1,6 +1,13 @@
 class Solution:
-    def findPeakElement(self, nums: List[int]) -> int:
-        count = 0
-        if len(nums) == 1:
-            return 1
-        for i in range(1, len(nums)):
+    def findPeakElementLinear(self, nums: List[int]) -> int:
+
+        #  the linear approach takes into consideration a couple scenarios in order to work
+
+        for i in range(len(nums)):
+            if i == len(nums)-1:
+                break
+            #  if the condition above isn't there then  we would get an index error
+
+            if nums[i] > nums[i+1]:
+                return i
+        return len(nums)-1
