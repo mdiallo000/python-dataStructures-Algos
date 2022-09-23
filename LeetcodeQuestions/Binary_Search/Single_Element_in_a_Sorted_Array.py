75f26a3dc9736b51b340006a66e4206a19b77b77
mdiallo98 <mamadou1237@gmail.com>
2022-09-23 15:25:09 -0400

    Capacity To Ship Packages Within D Days

---
+++ LeetcodeQuestions/Binary_Search/Single_Element_in_a_Sorted_Array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        #  You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once.
+
+        # Return the single element that appears only once.
+
+        # Your solution must run in O(log n) time and O(1) space.
