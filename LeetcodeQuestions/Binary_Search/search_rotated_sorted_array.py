63a30cd6f18f747d7b4c9e7081ab13430c35321c
mdiallo98 <mamadou1237@gmail.com>
2022-09-23 11:17:00 -0400

    search rotated sorted array II

--- LeetcodeQuestions/Binary_Search/search_rotated_sorted_array.py
+++
@@ -1,2 +0,0 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
---
+++ LeetcodeQuestions/Binary_Search/search_rotated_sorted_arrayII.py
@@ -0,0 +1,3 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        #  problems states we are given an array that is sorted but its then rotated at a random pivot, our job is to return Tr
