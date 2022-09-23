67e5a2a700836c430508c877fe8e846d9fa06957
mdiallo98 <mamadou1237@gmail.com>
2022-09-23 11:26:32 -0400

    search rotated sorted array II

--- LeetcodeQuestions/Binary_Search/search_rotated_sorted_arrayII.py
+++ LeetcodeQuestions/Binary_Search/search_rotated_sorted_arrayII.py
@@ -1,6 +1,7 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
         #  problems states we are given an array that is sorted but its then rotated at a random pivot, our job is to return True if we find this target or false if its not found.
+        #  This problem allows for duplicate values
 
         #  easy but suboptimal solution is to do a linear scan and return the corresponding bool, this will be O(n) but we can do better but taking the fact that one side of the array must be sorted. we can have an log(n) solution:
 
@@ -20,3 +21,13 @@ class Solution:
                     r = mid - 1
                 else:
                     l = mid + 1
+            #  the opposite case
+            # [5,6,7,0,1,2,4]
+            #  l     m      r
+            #  if the leftmost number is greater than our mid
+            else:
+                if nums[mid] <= target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return False
