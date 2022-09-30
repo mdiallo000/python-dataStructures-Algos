ca1f302ef5be928e6e5706a7f2fe3ab1b8cc96cb
mdiallo98 <mamadou1237@gmail.com>
2022-09-29 22:13:38 -0400

    verifying alien dictionary

--- LeetcodeQuestions/Graphs/verifying_alien_dictionary.py
+++ LeetcodeQuestions/Graphs/verifying_alien_dictionary.py
@@ -1,11 +1,11 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
 
-        ordDict = {c: i for c, i in enumerate(order)}
+        ordDict = {c: i for i, c in enumerate(order)}
 
         for i in range(len(words) - 1):
             wrd1, wrd2 = words[i], words[i+1]
-            for j in len(wrd1):
+            for j in range(len(wrd1)):
                 if j == len(wrd2):
                     return False
                 if wrd1[j] != wrd2[j]:
