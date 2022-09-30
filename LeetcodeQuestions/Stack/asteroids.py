7807613702dfe3a649a4b60ada12f7bcb6c083aa
mdiallo98 <mamadou1237@gmail.com>
2022-09-30 14:10:10 -0400

    asteroid

--- LeetcodeQuestions/Stack/asteroids.py
+++ LeetcodeQuestions/Stack/asteroids.py
@@ -6,3 +6,13 @@ class Solution:
         for a in asteroids:
             while stack and stack[-1] > 0 and a < 0:
                 diff = a + stack[-1]
+                if diff < 0:
+                    stack.pop()
+                elif diff > 0:
+                    a = 0
+                else:
+                    a = 0
+                    stack.pop
+            if a:
+                stack.append(a)
+        return stack
