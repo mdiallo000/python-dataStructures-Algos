ba2eb08bfed87a198ce1943f239013a4b8cdb9fc
mdiallo98 <mamadou1237@gmail.com>
2022-09-30 13:12:42 -0400

    Combinations

--- LeetcodeQuestions/backtracking/combinations.py
+++ LeetcodeQuestions/backtracking/combinations.py
@@ -1,2 +1,20 @@
+# Given two integers n and k, return all possible combinations of k numbers chosen from the range [1, n].
+
+# You may return the answer in any order.
+
 class Solution:
-    def combine(self, n: int, k: int) -> List[List[int]]:
+    def combine(self, n: int, k: int):
+        res = []
+
+        def dfs(start, arr):
+            if len(arr) == k:
+                res.append(arr[::])
+                return
+
+            for i in range(start, n+1):
+                arr.append(i)
+                dfs(i+1, arr)
+                arr.pop()
+
+        dfs(1, [])
+        return res
