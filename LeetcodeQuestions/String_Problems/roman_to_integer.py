f064c0e29641c77d0cf9d7a86c346ca2fcdf5c30
mdiallo98 <mamadou1237@gmail.com>
2022-09-29 20:36:00 -0400

    romain to int

--- LeetcodeQuestions/String_Problems/roman_to_integer.py
+++ LeetcodeQuestions/String_Problems/roman_to_integer.py
@@ -1,2 +1,11 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
+        values = {
+            "I":        1,
+            V             5
+            X             10
+            L             50
+            C             100
+            D             500
+            M             1000
+        }
