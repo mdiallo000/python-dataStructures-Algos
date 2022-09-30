ee0a823dac894c244c3a74a1fab620fcf08e6380
mdiallo98 <mamadou1237@gmail.com>
2022-09-29 20:49:13 -0400

    romain to int

--- LeetcodeQuestions/String_Problems/roman_to_integer.py
+++ LeetcodeQuestions/String_Problems/roman_to_integer.py
@@ -13,8 +13,13 @@ class Solution:
 
         for i in range(len(s)):
             curr = values[s[i]]
-            if i < len(s) and curr < values[s[i+1]]:
+            if i + 1 < len(s) and curr < values[s[i+1]]:
                 res += -curr
             else:
                 res += curr
         return res
+#  the whole trick to this problem boils down to whether or not the curr character is greater or smaller than the one next to it,
+#  if its greater than add it to the result, if its less than than subtract it to the result
+
+#  Time complexity O(n)
+#  Space complexity O(1)
