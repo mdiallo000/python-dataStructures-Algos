7bc10ef9bb6d5ff6046c2bb836e6720a0ee928ae
mdiallo98 <mamadou1237@gmail.com>
2022-09-29 20:46:32 -0400

    romain to int

--- LeetcodeQuestions/String_Problems/roman_to_integer.py
+++ LeetcodeQuestions/String_Problems/roman_to_integer.py
@@ -13,13 +13,8 @@ class Solution:
 
         for i in range(len(s)):
             curr = values[s[i]]
-            print(curr)
-            if i == len(s):
-                res = curr
-                break
-            if curr > values[s[i+1]]:
-                print(values[s[i+1]])
-                res += curr
-            else:
+            if i < len(s) and curr < values[s[i+1]]:
                 res += -curr
+            else:
+                res += curr
         return res
