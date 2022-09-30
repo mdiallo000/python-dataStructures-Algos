1b22d2b51fffa3135800b74b090e00044fc57df7
mdiallo98 <mamadou1237@gmail.com>
2022-09-29 21:34:34 -0400

    islandPerimeter

--- LeetcodeQuestions/Graphs/island_perimeter.py
+++ LeetcodeQuestions/Graphs/island_perimeter.py
@@ -4,3 +4,11 @@ class Solution:
         Rows = len(grid)
         Columns = len(grid[0])
         visited = set()
+        res = 0
+
+        def dfs(r, c):
+            if r >= Rows or c >= Columns or r < 0 or c < 0 or grid[r][c] == 0:
+                return 1
+
+            if (r, c) in visited:
+                return 0
