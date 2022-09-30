df38c6b5758a06727e1ca2006d57759a73e3f856
mdiallo98 <mamadou1237@gmail.com>
2022-09-30 12:08:02 -0400

    Replace Elements with Greatest Element on Right Side

--- LeetcodeQuestions/Hash_Table_Dict/Replace Elements with Greatest Element on Right Side.py
+++ LeetcodeQuestions/Hash_Table_Dict/Replace Elements with Greatest Element on Right Side.py
@@ -1,2 +1,12 @@
 # Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
 # After doing so, return the array.
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+
+        Map = {}
+        for i in range(len(arr) - 1):
+            curr_max = max(arr[i:])
+            Map[i] = curr_max
+        arr[-1] = -1
+
+        for key, val in
