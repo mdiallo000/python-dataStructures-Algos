8531a8ea09f96fe05d3cbfaa0db17962fd93fafc
mdiallo98 <mamadou1237@gmail.com>
2022-09-30 14:06:42 -0400

    asteroid

--- LeetcodeQuestions/Stack/asteroids.py
+++ LeetcodeQuestions/Stack/asteroids.py
@@ -1,2 +1,8 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+
+        stack = 0
+
+        for a in asteroids:
+            while stack and stack[-1] > 0 and a < 0:
+                diff = a + stack[-1]
