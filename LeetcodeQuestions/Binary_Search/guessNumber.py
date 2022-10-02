a32771d96088ee139e7aa9ab161667ed84dacab1
mdiallo98 <mamadou1237@gmail.com>
2022-10-01 21:12:52 -0400

    guess number

--- LeetcodeQuestions/Binary_Search/guessNumber.py
+++ LeetcodeQuestions/Binary_Search/guessNumber.py
@@ -4,5 +4,12 @@ class Solution:
         nums = [i for i in range(1, n+1)]
         l, r = 0, len(nums)-1
         while l <= r:
-            mid = (l+r) // 2
+            mid = r + l // 2
             curr = nums[mid]
+            ans = guess(curr)
+            if ans > 0:
+                mid = l + 1
+            elif ans < 0:
+                r = mid - 1
+            else:
+                return curr
