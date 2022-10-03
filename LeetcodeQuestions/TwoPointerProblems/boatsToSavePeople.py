0da54ad4e1bafef638c44c4a1e1bc2c6056a6910
mdiallo98 <mamadou1237@gmail.com>
2022-10-02 22:19:12 -0400

    boatsToSavePeople

---
+++ LeetcodeQuestions/TwoPointerProblems/boatsToSavePeople.py
@@ -0,0 +1,5 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        # You are given an array people where people[i] is the weight of the ith person, and an infinite number of boats where each boat can carry a maximum weight of limit. Each boat carries at most two people at the same time, provided the sum of the weight of those people is at most limit.
+
+        # Return the minimum number of boats to carry every given person.
