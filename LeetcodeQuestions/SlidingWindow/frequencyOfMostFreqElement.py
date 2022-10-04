cd0b2e213cb1c682e7790b5173b81ccf1aa262b4
mdiallo98 <mamadou1237@gmail.com>
2022-10-03 22:45:13 -0400

    frequencyOfMostFreqElement

--- LeetcodeQuestions/SlidingWindow/frequencyOfMostFreqElement.py
+++ LeetcodeQuestions/SlidingWindow/frequencyOfMostFreqElement.py
@@ -4,4 +4,14 @@ class Solution:
         nums.sort()
         currMax = 0
         l, r = 0, 0
-        while l <= r:
+        total = 0
+        while r <= len(nums)-1:
+            total += nums[r]
+
+            while nums[r] * (l+r) > total + k:
+                total -= nums[l]
+                l += 1
+            currMax = max(currMax, (l+r))
+            r += 1
+        print(currMax)
+        return currMax
