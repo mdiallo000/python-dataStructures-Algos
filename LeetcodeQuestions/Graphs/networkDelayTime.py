85a7ff91317b01f9a8787204b1532754b33e0917
mdiallo98 <mamadou1237@gmail.com>
2022-10-05 19:20:00 -0400

    networkDelay

--- LeetcodeQuestions/Graphs/networkDelayTime.py
+++ LeetcodeQuestions/Graphs/networkDelayTime.py
@@ -1,4 +1,8 @@
+from collections import defaultdict
+
+
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        graph = {}
+        graph = defaultdict()
         for u, v, w in times:
+            graph[u].append((v, w))
