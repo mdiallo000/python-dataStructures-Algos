4284e78e3b4970ba4d167b4ccfef71b7a5cf33bb
mdiallo98 <mamadou1237@gmail.com>
2022-10-04 20:26:41 -0400

    maxNumOfBalloons

---
+++ LeetcodeQuestions/Hash_Table_Dict/maxNumOfBalloons.py
@@ -0,0 +1,9 @@
+from calendar import c
+
+
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        #  Given a string text, you want to use the characters of text to form as many instances of the word "balloon" as possible.
+
+        # You can use each character in text at most once. Return the maximum number of instances that can be formed.
+        count = Counter(text)
