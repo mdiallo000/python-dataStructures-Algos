25b7353712855962cd94af92de180e684ecaaa89
mdiallo98 <mamadou1237@gmail.com>
2022-10-07 15:45:06 -0400

    carPooling

--- LeetcodeQuestions/Heap-Priority-Queue/carPooling.py
+++ LeetcodeQuestions/Heap-Priority-Queue/carPooling.py
@@ -1,3 +1,6 @@
+import heapq
+
+
 class Solution:
     def carPooling(self, trips, capacity):
         trips.sort(key=lambda t: t[1])
@@ -5,3 +8,7 @@ class Solution:
         currPass = 0
 
         for numPass, start, end in trips:
+            while minHeap and minHeap[0][0] <= start:
+                currPass -=
+                heapq.heappop(minHeap)
+            currPass += numPass
