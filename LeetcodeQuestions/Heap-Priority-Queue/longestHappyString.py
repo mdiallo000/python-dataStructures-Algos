eb7969752ad4f8a58a325af222344f9b273be107
mdiallo98 <mamadou1237@gmail.com>
2022-10-07 13:22:43 -0400

    longestHappyString

--- LeetcodeQuestions/Heap-Priority-Queue/longestHappyString.py
+++ LeetcodeQuestions/Heap-Priority-Queue/longestHappyString.py
@@ -1,5 +1,14 @@
+import heapq
+
+
 class Solution:
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
         maxHeap = []
         res = ""
         wrd = [(-a, "a"), (-b, "b"), (-c, 'c')]
+
+        for count, char in wrd:
+            if count != 0:
+                heapq.heappush(maxHeap, (count, char))
+
+        while maxHeap:
