a00c54f8f0da9c359a8a856bf3632307100b4e09
mdiallo98 <mamadou1237@gmail.com>
2022-10-07 15:43:44 -0400

    carPooling

--- LeetcodeQuestions/Heap-Priority-Queue/carPooling.py
+++ LeetcodeQuestions/Heap-Priority-Queue/carPooling.py
@@ -1,2 +1,7 @@
 class Solution:
     def carPooling(self, trips, capacity):
+        trips.sort(key=lambda t: t[1])
+        minHeap = []
+        currPass = 0
+
+        for numPass, start, end in trips:
