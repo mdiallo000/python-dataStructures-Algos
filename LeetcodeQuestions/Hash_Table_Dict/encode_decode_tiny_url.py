a90e807e2c24ff0650ba837b01f596ab9bba222c
mdiallo98 <mamadou1237@gmail.com>
2022-10-08 15:10:56 -0400

    encode_decode_tiny_url

--- LeetcodeQuestions/Hash_Table_Dict/encode_decode_tiny_url.py
+++ LeetcodeQuestions/Hash_Table_Dict/encode_decode_tiny_url.py
@@ -1,9 +1,13 @@
 class Codec:
     def __init__(self):
+        self.encode = {}
+        self.decode = {}
+        self.template = 'http://tinyurl.com'
 
     def encode(self, longUrl: str) -> str:
         """Encodes a URL to a shortened URL.
         """
+        if longUrl not in self.encode:
 
     def decode(self, shortUrl: str) -> str:
         """Decodes a shortened URL to its original URL.
