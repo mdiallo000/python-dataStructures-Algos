81da52536f4c29f63acc3f7050328bf5f1b703f7
mdiallo98 <mamadou1237@gmail.com>
2022-10-13 21:35:27 -0400

    qucksort

--- SortingAlgos/QuickSort.py
+++ SortingAlgos/QuickSort.py
@@ -9,3 +9,18 @@ def quick_sort(arr, s, e):
 
     for i in range(s, e):
         if arr[i] < pivot:
+            arr[left] = arr[i]
+            arr[i] = tmp
+            left += 1
+
+    # Move pivot in-between left & right sides
+    arr[e] = arr[left]
+    arr[left] = pivot
+
+    # Quick sort left side
+    quickSort(arr, s, left - 1)
+
+    # Quick sort right side
+    quickSort(arr, left + 1, e)
+
+    return arr
