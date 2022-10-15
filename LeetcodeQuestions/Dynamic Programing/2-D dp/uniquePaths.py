6bfeff29694f077e6e426056bbeebac618057894
mdiallo98 <mamadou1237@gmail.com>
2022-10-14 22:12:51 -0400

    unique paths

--- LeetcodeQuestions/Dynamic Programing/2-D dp/uniquePaths.py
+++ LeetcodeQuestions/Dynamic Programing/2-D dp/uniquePaths.py
@@ -5,4 +5,7 @@ class Solution:
 
         for _ in range(m - 1):
             newRow = [1] * m
-            for j in range(n - 2, -1, -1)
+            for j in range(n - 2, -1, -1):
+                newRow[j] = newRow[j+1] + row[j]
+            row = newRow
+        return row
