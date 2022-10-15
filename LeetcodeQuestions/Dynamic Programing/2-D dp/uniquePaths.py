5a91bec0e52cdfa9c6371dbab7d7d4c0e26df362
mdiallo98 <mamadou1237@gmail.com>
2022-10-14 22:21:56 -0400

    unique paths

--- LeetcodeQuestions/Dynamic Programing/2-D dp/uniquePaths.py
+++ LeetcodeQuestions/Dynamic Programing/2-D dp/uniquePaths.py
@@ -1,11 +1,11 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
 
-        row = [1] * m
+        row = [1] * n
 
         for _ in range(m - 1):
             newRow = [1] * m
             for j in range(n - 2, -1, -1):
                 newRow[j] = newRow[j+1] + row[j]
             row = newRow
-        return row
+        return row[0]
