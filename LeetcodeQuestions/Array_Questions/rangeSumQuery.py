0882797ab1f0ca5e5c3caba50e3e9a9b563c5b07
mdiallo98 <mamadou1237@gmail.com>
2022-10-17 02:09:13 -0400

    kthSmallestNodeInBst

--- LeetcodeQuestions/Array_Questions/rangeSumQuery.py
+++ LeetcodeQuestions/Array_Questions/rangeSumQuery.py
@@ -8,3 +8,6 @@ class NumArray:
             self.prefix.append(t)
 
     def sumRange(self, left: int, right: int) -> int:
+        preRight = self.prefix[right]
+        preLeft = self.prefix[left - 1] if left > 0 else 0
+        return preRight - preLeft
--- LeetcodeQuestions/Dynamic Programing/2-D dp/uniquePaths.py
+++ LeetcodeQuestions/Dynamic Programing/2-D dp/uniquePaths.py
@@ -2,7 +2,6 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
 
         row = [1] * n
-
         for _ in range(m - 1):
             newRow = [1] * m
             for j in range(n - 2, -1, -1):
