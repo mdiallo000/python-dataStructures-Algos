b85b67a62da48c4822bba95b3eeec998c53eb4c1
mdiallo98 <mamadou1237@gmail.com>
2022-10-19 21:33:44 -0400

    randomPickWithWeight

--- DecodingTheCodingInterviews/Netflix/groupSimilarTitles.py
+++ DecodingTheCodingInterviews/Netflix/groupSimilarTitles.py
@@ -1,2 +1,3 @@
 # Goal of Feature:
 # Feature # 1: We want to enable users to see relevant search results despite minor typos.
+def groupSimilarTitles(strs):
---
+++ Meta/randomPickWithWeight.py
@@ -0,0 +1,5 @@
+class Solution:
+
+    def __init__(self, w: List[int]):
+
+    def pickIndex(self) -> int:
