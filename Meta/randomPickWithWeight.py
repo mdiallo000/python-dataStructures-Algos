47bbbb95a2a48367221e436a83c15b6516ad7a41
mdiallo98 <mamadou1237@gmail.com>
2022-10-19 22:14:44 -0400

    randomPickWithWeight

--- Meta/randomPickWithWeight.py
+++ Meta/randomPickWithWeight.py
@@ -1,6 +1,12 @@
 class Solution:
 
     def __init__(self, w: List[int]):
-        self.lst = w
+        self.lst = []
+
+        prefix = 0
+        for n in w:
+            prefix += n
+            self.lst.append(prefix)
+        self.total = prefix
 
     def pickIndex(self) -> int:
