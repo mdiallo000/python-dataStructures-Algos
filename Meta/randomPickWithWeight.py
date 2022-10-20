cb3f6ef8085620ba402b79a2585b8d8dfd02a4c4
mdiallo98 <mamadou1237@gmail.com>
2022-10-19 22:24:33 -0400

    randomPickWithWeight

--- Meta/randomPickWithWeight.py
+++ Meta/randomPickWithWeight.py
@@ -17,5 +17,10 @@ class Solution:
 
         l, r = 0, len(self.lst) - 1
 
-        while l <= r:
+        while l < r:
             mid = (l + r - 1) // 2
+            if target > self.lst[mid]:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return l
