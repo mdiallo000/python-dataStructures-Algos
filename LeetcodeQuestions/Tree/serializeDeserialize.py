d62dd15414e22f1459a2c7d7a20163ef79babd23
mdiallo98 <mamadou1237@gmail.com>
2022-10-20 20:27:37 -0400

    serialize-deserialize

---
+++ LeetcodeQuestions/Tree/serializeDeserialize.py
@@ -0,0 +1,16 @@
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+
+        :type root: TreeNode
+        :rtype: str
+        """
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: TreeNode
+        """
