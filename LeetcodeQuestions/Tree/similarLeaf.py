1ef6b9c8524d6cd80b6323ca3cab4c496607a41e
mdiallo98 <mamadou1237@gmail.com>
2022-10-23 21:48:42 -0400

    similar leaf

--- LeetcodeQuestions/Tree/similarLeaf.py
+++ LeetcodeQuestions/Tree/similarLeaf.py
@@ -1,1 +1,11 @@
-class Solution
+from logging import root
+
+
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+
+        def determine(root1, root2):
+            if not root and not root2:
+                return True
+            elif root2 and root1:
+                return (root1.val == root2.val and determine(root1.left, root2.right))
