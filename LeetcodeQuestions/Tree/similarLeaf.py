1cb8a06b574d7ddfaf598a60276f7305d620fd0b
mdiallo98 <mamadou1237@gmail.com>
2022-10-23 21:52:18 -0400

    similar leaf

--- LeetcodeQuestions/Tree/similarLeaf.py
+++ LeetcodeQuestions/Tree/similarLeaf.py
@@ -13,4 +13,13 @@ class Solution:
                 return False
         sum1 = [0]
         sum2 = [0]
-        def similarTree(root):
+
+        def similarTree(root, s):
+            if not root.left and not root.right:
+                s[0] += root.val
+                return
+            similarTree(root.left, s)
+            similarTree(root.right, s)
+            return s
+        similarTree(roo1, sum1)
+        similarTree(root2, sum2)
