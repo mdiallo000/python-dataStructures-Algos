31fad96cffc96247b58a9d1690f7411a5f32701b
mdiallo98 <mamadou1237@gmail.com>
2022-10-27 14:28:11 -0400

    Kth Node in Tree

--- LeetcodeQuestions/Tree/kthNodeInTree.py
+++ LeetcodeQuestions/Tree/kthNodeInTree.py
@@ -5,3 +5,9 @@ class Solution:
         self.res = None
 
         def dfs(node):
+            if not node:
+                return None
+            dfs(node.left)
+            self.pos += 1
+            if self.pos == k:
+                self.res = node.val
