d0b3494aa71392f6be9b1187883925f57fae04c5
mdiallo98 <mamadou1237@gmail.com>
2022-10-29 23:01:39 -0400

    uniqueBinarySearchTree

--- LeetcodeQuestions/Tree/uniqueBinarySearchTree.py
+++ LeetcodeQuestions/Tree/uniqueBinarySearchTree.py
@@ -6,7 +6,6 @@ class Solution:
     def numTrees(self, n: int) -> int:
 
         numTree = [1] * n + 1
-
         for nodes in range(2, n+1):
             total = 0
             for root in range(1, root + 1):
