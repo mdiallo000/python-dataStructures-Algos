66fbc7e85d6667042b0bbfd85bb693a9d8af9ae6
mdiallo98 <mamadou1237@gmail.com>
2022-10-29 21:49:59 -0400

    uniqueBinarySearchTree

--- LeetcodeQuestions/Tree/uniqueBinarySearchTree.py
+++ LeetcodeQuestions/Tree/uniqueBinarySearchTree.py
@@ -1,3 +1,6 @@
+from turtle import right
+
+
 class Solution:
 
     def numTrees(self, n: int) -> int:
@@ -6,3 +9,6 @@ class Solution:
 
         for nodes in range(2, n+1):
             total = 0
+            for root in range(root, n + 1):
+                left = root - 1
+                right = nodes - root
