e31814a382d4d7bd9f5b86cffa9d1832835d22e6
mdiallo98 <mamadou1237@gmail.com>
2022-11-02 17:02:00 -0400

    convertBst to greater tree

---
+++ LeetcodeQuestions/Tree/convertBSTtoGreaterTree.py
@@ -0,0 +1,15 @@
+class Solution:
+    def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        self.currSum = 0
+
+        def dfs(node):
+            if not node:
+                return 0
+
+            dfs(node.right)
+            tmp = self.currSum
+            node.val += self.currSum
+            self.currSum = node.val
+            dfs(node.left)
+            return node
+        return dfs(root)
