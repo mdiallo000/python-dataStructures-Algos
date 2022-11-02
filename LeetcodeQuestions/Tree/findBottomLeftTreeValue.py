47727c24d4b340b95b2cbebe9baa6c4e847ed5d6
mdiallo98 <mamadou1237@gmail.com>
2022-11-02 17:22:42 -0400

    findBottomLeftreeValue

--- LeetcodeQuestions/Tree/findBottomLeftTreeValue.py
+++ LeetcodeQuestions/Tree/findBottomLeftTreeValue.py
@@ -1,2 +1,16 @@
+from collections import deque
+
+
 class Solution:
     def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+
+        q = deque()
+        q.append(root)
+
+        while q:
+            size = len(q)
+            leftmost = None
+            for _ in range(size):
+                node = q.popleft()
+
+                if node:
