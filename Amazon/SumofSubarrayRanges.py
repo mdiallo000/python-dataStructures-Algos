4957332f22f8a6718f2c83fcbb97335338518809
mdiallo98 <mamadou1237@gmail.com>
2022-11-03 23:36:07 -0400

    sum of subarray ranges

--- Amazon/SumofSubarrayRanges.py
+++ Amazon/SumofSubarrayRanges.py
@@ -3,4 +3,9 @@ class Solution:
         #  the brute force way is to just use two for loops
         res = 0
         for left in range(len(nums)):
+            minV = float(-inf), float(inf)
             for right in range(left, len(nums)):
+                minV = min(nums[left:right])
+                maxV = max(nums[left:right])
+                res += abs(minV - maxV)
+        return res
