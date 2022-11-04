9c0a01a694bc78ccdf8a80d5de4f58b02152c308
mdiallo98 <mamadou1237@gmail.com>
2022-11-03 23:36:43 -0400

    sum of subarray ranges

--- Amazon/SumofSubarrayRanges.py
+++ Amazon/SumofSubarrayRanges.py
@@ -5,7 +5,7 @@ class Solution:
         for left in range(len(nums)):
             minV, maxV = float(-inf), float(inf)
             for right in range(left, len(nums)):
-                minV = min(nums[left:right])
-                maxV = max(nums[left:right])
+                minV = min(minV, nums[right])
+                maxV = max(maxV, nums[right])
                 res += abs(minV - maxV)
         return res
