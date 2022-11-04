955aad7bc943e3b084a9f052272465445cbf77c5
mdiallo98 <mamadou1237@gmail.com>
2022-11-04 01:06:29 -0400

    minWindow

--- LeetcodeQuestions/SlidingWindow/mininum_window_substring.py
+++ LeetcodeQuestions/SlidingWindow/mininum_window_substring.py
@@ -1,5 +1,10 @@
+from collections import Counter
 from operator import index
 
 
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+
+        window = {}
+        countT = Counter(t)
+        res, resLen = [-1, -1], float(-inf)
