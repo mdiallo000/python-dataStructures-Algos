6af69126246fe06fd1adc92f77391fe73fb9b2ae
mdiallo98 <mamadou1237@gmail.com>
2022-11-03 23:32:11 -0400

    sum of subarray ranges

--- Amazon/SumofSubarrayRanges.py
+++ Amazon/SumofSubarrayRanges.py
@@ -1,6 +1,6 @@
 class Solution:
     def subArrayRanges(self, nums: List[int]) -> int:
         #  the brute force way is to just use two for loops
-
+        res = 0
         for left in range(len(nums)):
-            for right in range(left, len(nums))
+            for right in range(left, len(nums)):
