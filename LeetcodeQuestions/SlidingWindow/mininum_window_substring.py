64ab1bc23cabddcc107b17bed1ee25d4ff778724
mdiallo98 <mamadou1237@gmail.com>
2022-11-04 01:09:43 -0400

    minWindow

--- LeetcodeQuestions/SlidingWindow/mininum_window_substring.py
+++ LeetcodeQuestions/SlidingWindow/mininum_window_substring.py
@@ -9,3 +9,10 @@ class Solution:
         countT = Counter(t)
         res, resLen = [-1, -1], float(-inf)
         have, need = 0, len(countT)
+        for r in range(len(s)):
+            char = s[r]
+            #  we add this current character into our window
+            window[char] = 1 + window.get(char, 0)
+            #  after we have added the character we now look to see if we can update our have
+            if char in countT and window[char] == countT[char]:
+                have += 1
