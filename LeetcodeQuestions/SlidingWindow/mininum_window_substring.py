e053df9e44c0427e3c4941c8d9b6f26d7e3c1534
mdiallo98 <mamadou1237@gmail.com>
2022-11-04 01:05:04 -0400

    minWindow

--- LeetcodeQuestions/SlidingWindow/mininum_window_substring.py
+++ LeetcodeQuestions/SlidingWindow/mininum_window_substring.py
@@ -3,16 +3,3 @@ from operator import index
 
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-
-        if len(t) > len(s):
-            return ''
-        index1, index2 = 0, 0
-        res = ""
-        currMin = float(inf)
-        while index1 < len(s):
-
-            if s[index1] == t[index2]:
-                index2 += 1
-
-                if index2 == len(t):
-                    start, end = index1, index1 + 1
