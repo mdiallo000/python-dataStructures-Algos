3418f93f047472cff1675466aabe2c99db612dc8
mdiallo98 <mamadou1237@gmail.com>
2022-11-04 14:20:21 -0400

    dnaSequence

--- LeetcodeQuestions/SlidingWindow/dnaSequence.py
+++ LeetcodeQuestions/SlidingWindow/dnaSequence.py
@@ -1,2 +1,7 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        res = []
+        tmp = set()
+
+        for r in range((len(s) + 9)):
+            curr = [r:r+10]
