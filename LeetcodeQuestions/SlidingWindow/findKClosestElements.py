bbaefba24427669bb62711bfd5cb938b073f50e0
mdiallo98 <mamadou1237@gmail.com>
2022-11-05 17:45:25 -0400

    findKClosestElements

--- LeetcodeQuestions/SlidingWindow/findKClosestElements.py
+++ LeetcodeQuestions/SlidingWindow/findKClosestElements.py
@@ -1,7 +1,13 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        l, r = 0, len(nums) - 1
+        l, r = 0, len(arr) - k
 
         while l < r:
 
             m = (l+r - 1) // 2
+
+            if x - arr[m] > arr[m + k] - x:
+                l = mid + 1
+            else:
+                r = m
+        return arr[l: l + k]
