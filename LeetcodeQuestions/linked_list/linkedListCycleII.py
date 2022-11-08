1c5582a9d45e7686936278899165c032a6b51f7d
mdiallo98 <mamadou1237@gmail.com>
2022-11-08 13:11:04 -0500

    linkedListCycleII

--- LeetcodeQuestions/linked_list/linkedListCycleII.py
+++ LeetcodeQuestions/linked_list/linkedListCycleII.py
@@ -1,4 +1,11 @@
 class Solution:
     def detectCycleNaive(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
+        visited = set()
+        curr = head
+
+        while curr:
+            if curr in visited:
+                return curr
+
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
