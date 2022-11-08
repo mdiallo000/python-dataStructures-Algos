813bfb0c66cf9d0430459bce39db9f78b3b2936d
mdiallo98 <mamadou1237@gmail.com>
2022-11-07 22:14:57 -0500

    HappyNumber

--- LeetcodeQuestions/SlidingWindow/HappyNumber.py
+++ LeetcodeQuestions/SlidingWindow/HappyNumber.py
@@ -7,3 +7,9 @@ class Solution:
                 n, digit = divmod(n, 10)
                 sumT = digit**2
             return sumT
+        seen = set()
+
+        while n != 1 and n not in seen:
+            seen.add(curr)
+            n = get_Next(n)
+        return n == 1
