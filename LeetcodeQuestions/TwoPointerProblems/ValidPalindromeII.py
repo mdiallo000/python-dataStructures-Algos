62a47c755a800e9023c4e25795133428e4f25e68
mdiallo98 <mamadou1237@gmail.com>
2022-11-07 21:19:29 -0500

    ValidPalindromeII

--- LeetcodeQuestions/TwoPointerProblems/ValidPalindromeII.py
+++ LeetcodeQuestions/TwoPointerProblems/ValidPalindromeII.py
@@ -9,3 +9,13 @@ class Solution:
                 l += 1
                 r -= 1
             return True
+
+        l, r = 0, len(s) - 1
+
+        while l < r:
+
+            if s[l] != s[r]:
+                return palindrome(s, l+1, r) or palindrome(s, l, r+1)
+            l += 1
+            r -= 1
+        return True
