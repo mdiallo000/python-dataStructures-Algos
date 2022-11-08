9cc41cab751c8625329548dfa775fabe34fab123
mdiallo98 <mamadou1237@gmail.com>
2022-11-07 21:17:09 -0500

    ValidPalindromeII

--- LeetcodeQuestions/TwoPointerProblems/ValidPalindromeII.py
+++ LeetcodeQuestions/TwoPointerProblems/ValidPalindromeII.py
@@ -1,4 +1,8 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
 
-        def palindrome(wrd):
+        def palindrome(wrd, l, r):
+
+            if wrd[l] != wrd[r]:
+                return False
+            return True
