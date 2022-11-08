41b8f7bbff23088b40e2f254c22a8091fa95805d
mdiallo98 <mamadou1237@gmail.com>
2022-11-07 22:12:49 -0500

    HappyNumber

--- LeetcodeQuestions/SlidingWindow/HappyNumber.py
+++ LeetcodeQuestions/SlidingWindow/HappyNumber.py
@@ -1,2 +1,9 @@
 class Solution:
-    def isHappy(self, n: int) -> bool:
+    def isHappyNaive(self, n: int) -> bool:
+
+        def get_Next(n):
+            sumT = 0
+            while n > 0:
+                n, digit = divmod(n, 10)
+                sumT = digit**2
+            return sumT
