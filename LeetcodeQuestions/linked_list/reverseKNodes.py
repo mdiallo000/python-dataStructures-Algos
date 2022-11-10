c819c1fe14b3f523b1492c5495140a34caed8785
mdiallo98 <mamadou1237@gmail.com>
2022-11-10 18:59:01 -0500

    reverseKNodes

--- LeetcodeQuestions/linked_list/reverseKNodes.py
+++ LeetcodeQuestions/linked_list/reverseKNodes.py
@@ -1,5 +1,12 @@
 class Solution:
     def reverseKGroup(self, head, k):
+        # since this will be a recursive function we need to make sure the context is restate for each recursive call
+        curr = head
+        count = 0
+
+        while curr and count < k:
+            curr = curr.next
+            count += 1
 
     def reverseList(head, k):
         prev, curr = None, head
@@ -9,4 +16,4 @@ class Solution:
             prev = curr
             curr = nxt
             k -= 1
-        return
+        return prev
