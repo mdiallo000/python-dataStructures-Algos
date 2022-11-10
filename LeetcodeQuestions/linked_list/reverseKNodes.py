60d395c2de55d83866fc7bd94d2f9c2c59830a05
mdiallo98 <mamadou1237@gmail.com>
2022-11-10 18:56:33 -0500

    reverseKNodes

--- LeetcodeQuestions/linked_list/reverseKNodes.py
+++ LeetcodeQuestions/linked_list/reverseKNodes.py
@@ -1,13 +1,12 @@
 class Solution:
     def reverseKGroup(self, head, k):
 
-        def reverseList(head, k):
-            prev, curr = None, head
-            count = 0
-            while curr and count < k:
-                nxt = curr.next
-                curr.next = prev
-                prev = curr
-                curr = nxt
-                count += 1
-            return
+    def reverseList(head, k):
+        prev, curr = None, head
+        while k:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+            k -= 1
+        return
