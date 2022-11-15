8f673b42b7b3fc6fbb0ce03783a64639e34807bc
mdiallo98 <mamadou1237@gmail.com>
2022-11-15 18:53:55 -0500

    countCompleteTreeNode

--- LeetcodeQuestions/Tree/countCompleteTreeNode.py
+++ LeetcodeQuestions/Tree/countCompleteTreeNode.py
@@ -1,3 +1,8 @@
 class Solution:
     def countNodes(self, root):
         self.count = 0
+
+        def dfs(node):
+            if node:
+                self.count += 1
+                dfs(node.left)
