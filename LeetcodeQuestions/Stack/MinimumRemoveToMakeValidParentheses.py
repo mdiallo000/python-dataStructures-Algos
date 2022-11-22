5da3a8ad1e947fcd3101faa2c322734c2de7f392
mdiallo98 <mamadou1237@gmail.com>
2022-11-21 21:10:19 -0500

    MinimumRemoveToMakeValidParentheses

--- LeetcodeQuestions/Stack/MinimumRemoveToMakeValidParentheses.py
+++ LeetcodeQuestions/Stack/MinimumRemoveToMakeValidParentheses.py
@@ -1,15 +1,2 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        stack = []
-        oBrackets = 0
-        cBrackets = 0
-
-        for char in s:
-
-            if stack and oBrackets < cBrackets:
-                continue
-            if char == "(":
-                oBrackets += 1
-            if char == ")":
-                cBrackets += 1
-            stack.append(char)
