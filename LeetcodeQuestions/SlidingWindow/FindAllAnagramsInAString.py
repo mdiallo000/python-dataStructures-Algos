58f4ca1f074e99877c0cc841a531f40207e6f1fd
mdiallo98 <mamadou1237@gmail.com>
2022-11-21 19:58:20 -0500

    find all anagram in a string

--- LeetcodeQuestions/SlidingWindow/FindAllAnagramsInAString.py
+++ LeetcodeQuestions/SlidingWindow/FindAllAnagramsInAString.py
@@ -8,7 +8,11 @@ class Solution:
 
         pCount = Counter(p)
         window = defaultdict(0)
-
+        res = []
         for r in range(len(s)):
             #  i need to add each char into my window
             window[s[r]] += 1
+
+            #  we know when the window and pCount are the same then we can return
+            if window == pCount:
+                res.append(i - plen + 1)
