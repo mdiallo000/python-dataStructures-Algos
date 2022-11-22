c68ee726762889cc5673e5d176f520fabf3eb823
mdiallo98 <mamadou1237@gmail.com>
2022-11-21 20:54:01 -0500

    MinimumRemoveToMakeValidParentheses

--- LeetcodeQuestions/Stack/MinimumRemoveToMakeValidParentheses.py
+++ LeetcodeQuestions/Stack/MinimumRemoveToMakeValidParentheses.py
@@ -9,3 +9,6 @@ class Solution:
             if stack and oBrackets < cBrackets:
                 continue
             if char == "(":
+                oBrackets += 1
+            if char == ")":
+                cBrackets += 1
