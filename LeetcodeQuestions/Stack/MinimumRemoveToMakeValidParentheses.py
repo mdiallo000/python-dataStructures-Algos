767a338d5659da2a301b32a30bb50237d5fa462a
mdiallo98 <mamadou1237@gmail.com>
2022-11-21 21:19:10 -0500

    MinimumRemoveToMakeValidParentheses

--- LeetcodeQuestions/Stack/MinimumRemoveToMakeValidParentheses.py
+++ LeetcodeQuestions/Stack/MinimumRemoveToMakeValidParentheses.py
@@ -3,6 +3,7 @@ class Solution:
         stack = []
         builder = s
 
-        for i, val in enumerate(s):
+        for i, char in enumerate(s):
             if stack and stack[-1][0] in ["(", ")"]:
                 stack.pop()
+            elif
