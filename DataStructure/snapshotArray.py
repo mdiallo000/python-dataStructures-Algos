1780d3ee9fa2c58f649b98a4797bd2c3db83729e
mdiallo98 <mamadou1237@gmail.com>
2022-11-22 21:29:54 -0500

    snapshotArray

---
+++ DataStructure/snapshotArray.py
@@ -0,0 +1,9 @@
+class SnapshotArray:
+
+    def __init__(self, length: int):
+
+    def set(self, index: int, val: int) -> None:
+
+    def snap(self) -> int:
+
+    def get(self, index: int, snap_id: int) -> int:
