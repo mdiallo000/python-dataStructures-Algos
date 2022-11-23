5336067720b706515767546436608150378b08eb
mdiallo98 <mamadou1237@gmail.com>
2022-11-22 20:37:34 -0500

    flattenNestedListIterator

---
+++ LeetcodeQuestions/Stack/flattenNestedListIterator.py
@@ -0,0 +1,8 @@
+class NestedIterator:
+    def __init__(self, nestedList: [NestedInteger]):
+        self.stack = []
+        for n in nested
+
+    def next(self) -> int:
+
+    def hasNext(self) -> bool:
