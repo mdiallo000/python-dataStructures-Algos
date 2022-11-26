1303314d885bacaa2ad276ac8bdb5d7bdb5b8444
mdiallo98 <mamadou1237@gmail.com>
2022-11-25 21:20:01 -0500

    time based key value store

--- DataStructure/time-based-key-value.py
+++ DataStructure/time-based-key-value.py
@@ -4,11 +4,9 @@ from collections import defaultdict
 class TimeMap:
 
     def __init__(self):
-        self.store = defaultdict(dict)
+        self.store = defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-
-        self.store[key][timestamp] = value
 
     def get(self, key: str, timestamp: int) -> str:
 
