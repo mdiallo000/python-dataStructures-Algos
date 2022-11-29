4c21cebde7df73fffb71ef06633c2fbd3343ae5d
mdiallo98 <mamadou1237@gmail.com>
2022-11-28 20:44:28 -0500

    designHashMap

---
+++ LeetcodeQuestions/Hash_Table_Dict/designHashMap.py
@@ -0,0 +1,10 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.MyHashMap = set()
+
+    def put(self, key: int, value: int) -> None:
+
+    def get(self, key: int) -> int:
+
+    def remove(self, key: int) -> None:
