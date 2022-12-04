422fc9647b03bb8179a8fbe19de1d905a099e98f
mdiallo98 <mamadou1237@gmail.com>
2022-12-03 22:40:50 -0500

    132 pattern

---
+++ LeetcodeQuestions/Stack/132Pattern.py
@@ -0,0 +1,13 @@
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        # so all we need to determine is if there is a number that is both greather thab the previous and the
+        #  i am thinking of maybe three loops, one on
+
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                for k in range(j+1, len(nums)):
+                    if nums[i] < nums[j] and nums[k] < nums[j] and nums[k] > nums[i]:
+                        print(nums[i], nums[j], nums[k])
+                        return True
+
+        return False
