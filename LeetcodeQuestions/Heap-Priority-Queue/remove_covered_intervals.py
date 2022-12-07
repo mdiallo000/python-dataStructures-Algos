b063c6c05d27f1ec10a9b17e995c2ec4aa70eaba
mdiallo98 <mamadou1237@gmail.com>
2022-12-07 18:06:00 -0500

    minNumberofNodesToReachAllVertices

--- LeetcodeQuestions/Heap-Priority-Queue/remove_covered_intervals.py
+++ LeetcodeQuestions/Heap-Priority-Queue/remove_covered_intervals.py
@@ -2,3 +2,14 @@ class Solution:
     def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
 
         intervals.sort(key=lambda x: (x[0], -x[1]))
+        #  the above sort will make sure we not only sort by the smallest start time but also in the case of a tie, we give priotity for the bigger end time time
+        #  so a test case like this [[1,4], [1,2], [3,4]]
+        res = 0 
+        previous =  0
+        for _ , end in intervals: 
+            
+            if end > previous:
+                res +=1
+                previous = end 
+        return res 
+        
