332eece1d0a6c5479e9bf89d6c004c2fe0e263b2
mdiallo98 <mamadou1237@gmail.com>
2022-12-07 19:50:11 -0500

    keysAndRooms

--- LeetcodeQuestions/Graphs/keysAndRooms.py
+++ LeetcodeQuestions/Graphs/keysAndRooms.py
@@ -1,2 +1,8 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+
+        seen = [False] * len(rooms)
+        seen[0] = True
+        stack = [0]
+
+        while stack:
