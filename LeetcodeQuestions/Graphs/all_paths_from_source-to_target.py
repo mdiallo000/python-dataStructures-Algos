a5b35d5694280f05b5bd73787e7f2505f8883ce1
mdiallo98 <mamadou1237@gmail.com>
2022-12-08 21:01:57 -0500

    unionFind.

--- LeetcodeQuestions/Graphs/all_paths_from_source-to_target.py
+++ LeetcodeQuestions/Graphs/all_paths_from_source-to_target.py
@@ -1,2 +1,20 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        target = len(graph) - 1
+
+        result = []
+
+        def DFS(curr, path):
+
+            # best case for when we should return
+            if curr == target:
+                result.append(list(path))
+                return
+            for node in graph[curr]:
+                path.append(node)
+                DFS(node, path)
+                path.pop()
+        path = deque([0])
+
+        DFS(0, path)
+        return result
