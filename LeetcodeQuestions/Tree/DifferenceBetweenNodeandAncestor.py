5348d173ebe1ecf62cc373c889f6eff9febae793
mdiallo98 <mamadou1237@gmail.com>
2022-12-08 22:38:19 -0500

    DifferenceBetweenNodeandAncestor

--- LeetcodeQuestions/Tree/DifferenceBetweenNodeandAncestor.py
+++ LeetcodeQuestions/Tree/DifferenceBetweenNodeandAncestor.py
@@ -4,3 +4,9 @@ class Solution:
         self.res = 0
 
         def dfs(node, currMin, currMax):
+            if not node:
+                return
+
+            currMax = max(currMax, node.val)
+            currMin = min(currMin, node.val)
+            self.res = max(self.res, abs(currMax - node.val))
