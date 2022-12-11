2e63b7674a5192923cf71b258782674ac97067a0
mdiallo98 <mamadou1237@gmail.com>
2022-12-10 22:54:14 -0500

    mergeAccounts

--- LeetcodeQuestions/Graphs/mergeAccounts.py
+++ LeetcodeQuestions/Graphs/mergeAccounts.py
@@ -2,10 +2,11 @@ class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
 
         graph = defaultdict(set)
-        email_accounts = {}
+        email_to_name = {}
 
         for account in accounts:
             name = account[0]
             for email in account[1:]:
                 graph[email].add(account[1:])
                 graph[account[1:]].add(email)
+                email_to_name[email] = name
