7dcac70aa9859e354b034818c365a464a1b5b0a0
mdiallo98 <mamadou1237@gmail.com>
2022-12-11 21:37:19 -0500

    mergeAccounts

--- LeetcodeQuestions/Graphs/mergeAccounts.py
+++ LeetcodeQuestions/Graphs/mergeAccounts.py
@@ -10,6 +10,17 @@ class UnionFind:
             node = self.parent[node]
         return node
 
+    def union(self, x, y):
+        p1, p2 = self.find(x), self.find(y)
+        if p1 == p2:
+            return False
+        if self.rank[p1] > self.rank[p2]:
+            self.parent[p2] = p1
+            self.rank[p1] += self.rank[p2]
+        else:
+            self.parent[p1] = p2
+            self.rank[p2] += self.rank[p1]
+
 
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
