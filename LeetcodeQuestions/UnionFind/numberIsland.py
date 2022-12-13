d59e2ee32b25a8800172bb896a0f205827861678
mdiallo98 <mamadou1237@gmail.com>
2022-12-13 12:47:56 -0500

    numberIsland.

--- LeetcodeQuestions/UnionFind/numberIsland.py
+++ LeetcodeQuestions/UnionFind/numberIsland.py
@@ -21,4 +21,15 @@ class UnionFind:
             self.parent[node] = self.find(self.parent[node])
         return self.parent[node]
 
-    def union(self, )
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x != root_y:
+            if self.rank[root_x] > self.rank[root_y]:
+                self.parent[root_y] = root_x
+            elif self.rank[root_x] < self.rank[root_y]:
+                self.parent[root_x] = root_y
+            else:
+                self.parent[root_y] = root_x
+                self.rank[root_x] += 1
+            self.count -= 1
