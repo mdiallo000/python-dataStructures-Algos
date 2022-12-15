e065715878c4bd87a4abdcbb31fa3f5c5973ce0f
mdiallo98 <mamadou1237@gmail.com>
2022-12-15 17:43:36 -0500

    LCS

--- LeetcodeQuestions/Dynamic Programing/2-D dp/longestCommonSubsequence.py
+++ LeetcodeQuestions/Dynamic Programing/2-D dp/longestCommonSubsequence.py
@@ -6,3 +6,4 @@ class Solution:
             for col in range(len(text2) - 1, -1, - 1):
                 #  we are only interested in two conditions
                 #  1 if the two charaters at this position matches for both words than the value will be 1 + whatever we found diagonally
+                if text1[row][col] == text2[row][col]
