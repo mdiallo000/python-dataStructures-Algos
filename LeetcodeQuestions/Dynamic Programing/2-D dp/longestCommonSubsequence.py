e140d63474a20dfd467344f64f8920de5c17e37d
mdiallo98 <mamadou1237@gmail.com>
2022-12-15 17:40:26 -0500

    LCS

--- LeetcodeQuestions/Dynamic Programing/2-D dp/longestCommonSubsequence.py
+++ LeetcodeQuestions/Dynamic Programing/2-D dp/longestCommonSubsequence.py
@@ -1,3 +1,4 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        dp_table = [[0 for i in range(len(text2) + 1) for i in range(len(text1) + 1)]
+        dp_table = [[0 for i in range(len(text2) + 1)]
+                    for i in range(len(text1) + 1)]
