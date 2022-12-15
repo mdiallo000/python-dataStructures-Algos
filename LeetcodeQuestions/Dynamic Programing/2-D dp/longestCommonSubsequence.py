a385eddf3e864821422e7e740c3b507fbf15d4cb
mdiallo98 <mamadou1237@gmail.com>
2022-12-15 17:48:12 -0500

    LCS

--- LeetcodeQuestions/Dynamic Programing/2-D dp/longestCommonSubsequence.py
+++ LeetcodeQuestions/Dynamic Programing/2-D dp/longestCommonSubsequence.py
@@ -1,9 +1,14 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        dp_table = [[0 for i in range(len(text2) + 1)]
+        dp_table = [[0 for j in range(len(text2) + 1)]
                     for i in range(len(text1) + 1)]
         for row in range(len(text1)-1, -1, -1):
             for col in range(len(text2) - 1, -1, - 1):
                 #  we are only interested in two conditions
                 #  1 if the two charaters at this position matches for both words than the value will be 1 + whatever we found diagonally
-                if text1[row][col] == text2[row][col]
+                if text1[row] == text2[col]:
+                    dp_table[row][col] = 1 + dp_table[row+1][col+1]
+                else:
+                    dp_table[row][col] = max(
+                        dp_table[row+1][col], dp_table[row][col+1])
+        return dp_table[0][0]
