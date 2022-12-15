9f4e13928e0df83a318190e719236216bbdd665a
mdiallo98 <mamadou1237@gmail.com>
2022-12-14 23:40:11 -0500

    count sub Islands

--- LeetcodeQuestions/Graphs/count_sub_islands.py
+++ LeetcodeQuestions/Graphs/count_sub_islands.py
@@ -1,2 +1,8 @@
 class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
+        ROWS = len(grid2)
+        COLUMNS = len(grid2[0])
+
+        def dfs(r, c):
+
+            if r == ROWS or r < 0
