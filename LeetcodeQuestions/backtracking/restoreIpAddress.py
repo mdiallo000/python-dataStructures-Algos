83e0d54d0e4577ae01c1817af5e0147f9c8acf12
mdiallo98 <mamadou1237@gmail.com>
2022-12-17 14:05:54 -0500

    restoreIpAddress

--- LeetcodeQuestions/backtracking/restoreIpAddress.py
+++ LeetcodeQuestions/backtracking/restoreIpAddress.py
@@ -1,3 +1,13 @@
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
         res = []
+        if len(s) > 12:
+            return res
+
+        def backtrack(idx, dots, currIP):
+
+            if dots == 4 or idx == len(s):
+                res.append(currIP[:-1])
+                return
+            if dots > 4:
+                return
