ec8f171d2928418410594bebe3f9b3b1537135c9
mdiallo98 <mamadou1237@gmail.com>
2022-12-19 19:57:33 -0500

    buyingStockWithCoolDown

--- LeetcodeQuestions/Dynamic Programing/1-D dp/buyingStockWithCoolDown.py
+++ LeetcodeQuestions/Dynamic Programing/1-D dp/buyingStockWithCoolDown.py
@@ -2,3 +2,7 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
 
         dp = {}
+
+        def dfs(idx, buying):
+            if idx >= len(prices):
+                return dp[(idx, buying)]
