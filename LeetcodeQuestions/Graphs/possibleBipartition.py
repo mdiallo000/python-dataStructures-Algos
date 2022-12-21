809bc95f6b9903e496ce13f3f6a524762971e76d
mdiallo98 <mamadou1237@gmail.com>
2022-12-20 20:08:54 -0500

    possibleBipartiton

--- LeetcodeQuestions/Graphs/possibleBipartition.py
+++ LeetcodeQuestions/Graphs/possibleBipartition.py
@@ -5,3 +5,7 @@ class Solution:
         for u, v in dislikes:
             graph[u].append(v)
             graph[v].append(u)
+
+        groups = {i: None for i in range(1, n + 1)}
+
+        def dfs(node, state):
