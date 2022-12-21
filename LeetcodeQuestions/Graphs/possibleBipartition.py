b58af47db30bf47d65819c90ad73b24076807ce3
mdiallo98 <mamadou1237@gmail.com>
2022-12-20 20:08:17 -0500

    possibleBipartiton

--- LeetcodeQuestions/Graphs/possibleBipartition.py
+++ LeetcodeQuestions/Graphs/possibleBipartition.py
@@ -1,2 +1,7 @@
 class Solution:
     def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        graph = defaultdict(list)
+
+        for u, v in dislikes:
+            graph[u].append(v)
+            graph[v].append(u)
