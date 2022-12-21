8bdc255e36c2a5776cc95776817d5bdcc9cd9940
mdiallo98 <mamadou1237@gmail.com>
2022-12-20 20:10:42 -0500

    possibleBipartiton

--- LeetcodeQuestions/Graphs/possibleBipartition.py
+++ LeetcodeQuestions/Graphs/possibleBipartition.py
@@ -9,3 +9,13 @@ class Solution:
         groups = {i: None for i in range(1, n + 1)}
 
         def dfs(node, state):
+            if not groups[node]:
+                groups[node] = state
+            else:
+                return groups[node]
+
+            for neigbor in graph[node]:
+
+                if not dfs(neigbor, 2 if state == 1 else 1):
+                    return False
+            return True
