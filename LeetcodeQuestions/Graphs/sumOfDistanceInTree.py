24f87f53ee0e4c76974860cc6c5a4abdd3949368
mdiallo98 <mamadou1237@gmail.com>
2022-12-21 21:29:55 -0500

    sdt

--- LeetcodeQuestions/Graphs/sumOfDistanceInTree.py
+++ LeetcodeQuestions/Graphs/sumOfDistanceInTree.py
@@ -6,3 +6,7 @@ class Solution:
             graph[t].append(s)
         # initial the result for the distance of from each point is unknown so we will store zero in our result
         res = [0]*n
+        # every node will start off with a score of one and as we find out how many nodes are below it we will then add that to the initial score
+        score = [1] * n
+
+        def dfs_count(curr, parent, )
