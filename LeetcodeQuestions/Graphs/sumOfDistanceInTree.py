9eda5719fbf5e9a7f62d73024a07bd75e3077ef0
mdiallo98 <mamadou1237@gmail.com>
2022-12-21 21:26:59 -0500

    sdt

--- LeetcodeQuestions/Graphs/sumOfDistanceInTree.py
+++ LeetcodeQuestions/Graphs/sumOfDistanceInTree.py
@@ -1,2 +1,6 @@
 class Solution:
     def sumOfDistancesInTree(self, n: int, edges: List[List[int]]) -> List[int]:
+        graph = defaultdict(list)
+        for s, t in edges:
+            graph[s].append(t)
+            graph[t].append(s)
