878b551eafc642969df6bd608a00e27fde0997fb
mdiallo98 <mamadou1237@gmail.com>
2022-12-21 21:27:59 -0500

    sdt

--- LeetcodeQuestions/Graphs/sumOfDistanceInTree.py
+++ LeetcodeQuestions/Graphs/sumOfDistanceInTree.py
@@ -4,3 +4,5 @@ class Solution:
         for s, t in edges:
             graph[s].append(t)
             graph[t].append(s)
+        # initial the result for the distance of from each point is unknown so we will store zero in our resulr
+        res = [0]*n
