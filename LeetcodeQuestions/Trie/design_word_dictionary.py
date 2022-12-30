a4d0210c00ac2aa66832ed7b83a68a79977bb4b9
mdiallo98 <mamadou1237@gmail.com>
2022-12-29 22:51:31 -0500

    design a word dictionary

--- LeetcodeQuestions/Trie/design_word_dictionary.py
+++ LeetcodeQuestions/Trie/design_word_dictionary.py
@@ -10,5 +10,12 @@ class WordDictionary:
         self.root = Trie_Node()
 
     def addWord(self, word: str) -> None:
+        #  we need to add a word in the dictionary
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = Trie_Node()
+            curr = curr.children[c]
+        curr.end_of_word = True
 
     def search(self, word: str) -> bool:
