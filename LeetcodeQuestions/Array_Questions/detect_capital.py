5e4eb3ed9c86c3160d77c9b5c4613e7b19d3ffd9
mdiallo98 <mamadou1237@gmail.com>
2023-01-01 22:10:16 -0500

    detect_capital

--- LeetcodeQuestions/Array_Questions/detect_capital.py
+++ LeetcodeQuestions/Array_Questions/detect_capital.py
@@ -1,4 +1,6 @@
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
-        l = 0
-        r = len(word)-1
+
+        #  our goal is detect whether the usage of the capital letters is done correctly
+        #  for it to be correct all letters are capitalized or only the first letter is capitalize
+        #  what i am think is to have some counter
