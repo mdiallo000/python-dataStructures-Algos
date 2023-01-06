37b43862905a2ad6faa694e418ccc58e547ff70a
mdiallo98 <mamadou1237@gmail.com>
2023-01-05 20:58:17 -0500

    tastiness_of_candies

--- LeetcodeQuestions/Array_Questions/tastiness_of_candies.py
+++ LeetcodeQuestions/Array_Questions/tastiness_of_candies.py
@@ -7,4 +7,23 @@ class Solution:
         # Return the maximum tastiness of a candy basket.
         # ------------------------------------------------------------------------------------------------------------------------
         #  so we are given an list of numbers where the number at i represents the price of the ith candy
-        #  we are also given an interger k  where k represents the baskets of distinct candies sold at the store
+        #  we are also given an integer k where k represents the baskets of distinct candies sold at the store
+
+        #  The criteria for a candy to be tasty is the smallest absolute difference of the prices of any two candies in the basket
+        price.sort()
+
+        def check(x):
+            last, count, i = price[0], 1, 1
+            while count < k and i < len(price):
+                if price[i] - last >= x:
+                    last, count = price[i], count + 1
+                i += 1
+            return count == k
+        lo, hi = 0, 10 ** 9
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if check(mid):
+                lo = mid + 1
+            else:
+                hi = mid
+        return lo - 1
