17102cccc8b9b698ba88a3d5cb6a10b1d2f05ee8
mdiallo98 <mamadou1237@gmail.com>
2023-01-05 20:34:29 -0500

    tastiness_of_candies

--- LeetcodeQuestions/Array_Questions/tastiness_of_candies.py
+++ LeetcodeQuestions/Array_Questions/tastiness_of_candies.py
@@ -1,2 +1,7 @@
 class Solution:
     def maximumTastiness(self, price: List[int], k: int) -> int:
+        # You are given an array of positive integers price where price[i] denotes the price of the ith candy and a positive integer k.
+
+        # The store sells baskets of k distinct candies. The tastiness of a candy basket is the smallest absolute difference of the prices of any two candies in the basket.
+
+        # Return the maximum tastiness of a candy basket.
