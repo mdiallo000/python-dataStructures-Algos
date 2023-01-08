5afe2f2660a38513b713fbbac88a16109528582b
mdiallo98 <mamadou1237@gmail.com>
2023-01-07 23:08:48 -0500

    max profit

--- LeetcodeQuestions/SlidingWindow/max_profit.py
+++ LeetcodeQuestions/SlidingWindow/max_profit.py
@@ -4,11 +4,11 @@ class Solution:
         #  the rules are simple, our goal is to maximize our profits, we can do so by buying low and selling high
         #  we can easily come up with a naive solution where we see how much profit can be made by buy on each day and then selling on every subsequent day
         # This would yield an O(n^2) algorithm
-        # max_profit = 0
-        # for today in range(len(prices)):
-        #     for future in range(today, len(prices)):
-        #         max_profit =  max(max_profit, (prices[future] - prices[today]))
-        # return max_profit
+        max_profit = 0
+        for today in range(len(prices)):
+            for future in range(today, len(prices)):
+                max_profit = max(max_profit, (prices[future] - prices[today]))
+        return max_profit
 
         #  We can create a linear algorithm by applying the sliding window techinque were we only examine the window where we are generating profit and whenever we encouter a new low price we take that as our new starting point
         today, future = 0, 0
@@ -21,4 +21,4 @@ class Solution:
 
         return max_profit
 
-        #  with this technique we manage to effieciently finds the higest profit margin possible
+        #  with this technique we manage to effieciently finds the highest profit margin possible
