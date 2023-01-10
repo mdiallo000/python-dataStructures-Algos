3303ff7b22e17c0fe16bff48d22805ab271323ed
mdiallo98 <mamadou1237@gmail.com>
2023-01-09 23:31:34 -0500

    boats_to_save_people

---
+++ LeetcodeQuestions/Greedy/boats_to_save_people.py
@@ -0,0 +1,14 @@
+def numRescueBoats(self, people: List[int], limit: int) -> int:
+
+    l = 0
+    curr = 0
+    boats = 0
+     r = len(people)-1
+      people.sort()
+       while l <= r:
+            boats += 1
+            if people[l] + people[r] <= limit:
+                l += 1
+            r -= 1
+
+        return boats
