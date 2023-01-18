043a33a4779877612aeb6dc0a2d81370b59b0598
mdiallo98 <mamadou1237@gmail.com>
2023-01-17 22:33:30 -0500

    monotonic_stack

--- LeetcodeQuestions/Array_Questions/monotonic_stack.py
+++ LeetcodeQuestions/Array_Questions/monotonic_stack.py
@@ -3,3 +3,10 @@ class Solution:
         #  so we are given an array of integers, only positive integers, no floating point values. We want to determine whether or not this list is either monotonically increasing or decreasing.
         #  What shoudl my approach be?
         #  NOT sure if there is a silver bullet for this problem. We have to our disposal a couple of patterns
+        #  what if we do two checks one for increasing and another for deacreasing?
+        def checKp
+        #  In actuality all i need to check is if its not monotonic in its composition
+        #  So rather than attempting to see whether or not its monotonic lets see if it isnt
+        #  so how will i do it?
+     #  maybe i can use a stack?
+     #  Go through the list, if i find a number that is greater than the previous and future then we return false
