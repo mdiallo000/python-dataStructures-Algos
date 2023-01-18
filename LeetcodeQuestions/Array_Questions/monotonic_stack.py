f14f7179509ee9d7cf73d65f223315289a1fdff8
mdiallo98 <mamadou1237@gmail.com>
2023-01-17 22:33:00 -0500

    monotonic_stack

--- LeetcodeQuestions/Array_Questions/monotonic_stack.py
+++ LeetcodeQuestions/Array_Questions/monotonic_stack.py
@@ -1,2 +1,5 @@
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
+        #  so we are given an array of integers, only positive integers, no floating point values. We want to determine whether or not this list is either monotonically increasing or decreasing.
+        #  What shoudl my approach be?
+        #  NOT sure if there is a silver bullet for this problem. We have to our disposal a couple of patterns
