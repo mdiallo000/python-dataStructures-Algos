eb82906c063adc003b7c9235baac7f180027ee2b
mdiallo98 <mamadou1237@gmail.com>
2023-01-31 17:57:25 -0500

    best team with no conflicts

--- LeetcodeQuestions/Dynamic Programing/1-D dp/best_team_with_no_conflicts.py
+++ LeetcodeQuestions/Dynamic Programing/1-D dp/best_team_with_no_conflicts.py
@@ -1,2 +1,16 @@
 class Solution:
     def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
+        #  goal => choose team with highest overall score
+        #  score ==  the sum of scores of all the players in the team
+        #  no conflicts allowed
+        #  we have a conflict if a younger player has a higher score than an
+        #  older player
+
+        #  we get two lists, scores and ages
+        #  socre[i] == score for that player
+        #  ages[i] == age of that player
+        #   Potential pattern?
+        #  elimimate slow-fast pointers, two pointer, sliding window,
+        #  i am leaning towards a greedy solution
+        #  but lets examine how this would would by hand anyways
+        #  thus far we can pintpoint it down to sorting or some type of greedy?
