91bf3d4f271aa7746f0f7d03a8edbff139bf3aa8
mdiallo98 <mamadou1237@gmail.com>
2023-02-01 23:36:25 -0500

    sort_linked_list

--- LeetcodeQuestions/linked_list/sort_linked_list.py
+++ LeetcodeQuestions/linked_list/sort_linked_list.py
@@ -1,6 +1,13 @@
+class NodeList:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
 class Solution:
     def sortList(head):
         curr = head
         data = []
         while curr:
             data.append(curr.val)
+            curr = curr.next
