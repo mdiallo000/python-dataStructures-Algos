99f6ce03c4d5279ed7c3e0cb2fa931f8ccf02329
mdiallo98 <mamadou1237@gmail.com>
2023-02-02 23:56:15 -0500

    sort_linked_list

--- LeetcodeQuestions/linked_list/sort_linked_list.py
+++ LeetcodeQuestions/linked_list/sort_linked_list.py
@@ -15,7 +15,7 @@ class Solution:
 
         curr = head
         pointer = 0
-        while curr and pointer < len(data):
+        while curr:
             curr.val = data[pointer]
             pointer += 1
             curr = curr.next
