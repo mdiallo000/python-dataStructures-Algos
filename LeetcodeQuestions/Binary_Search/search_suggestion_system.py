f974d21ccf1237b579f7c91153a83127be0679a2
mdiallo98 <mamadou1237@gmail.com>
2023-02-04 23:26:14 -0500

    search_suggestion_system

--- LeetcodeQuestions/Binary_Search/search_suggestion_system.py
+++ LeetcodeQuestions/Binary_Search/search_suggestion_system.py
@@ -5,3 +5,5 @@ class Solution:
         # Design a system that suggests at most three product names from products after each character of searchWord is typed. Suggested products should have common prefix with searchWord. If there are more than three products with a common prefix return the three lexicographically minimums products.
 
         # Return a list of lists of the suggested products after each character of searchWord is typed.
+
+        #  the system needs to suggest not more than three product names from the list of products we are initially given
