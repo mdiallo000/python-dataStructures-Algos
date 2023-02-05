defac8a0a6995c70a90dfe82240fc76e18df5269
mdiallo98 <mamadou1237@gmail.com>
2023-02-04 23:59:14 -0500

    search_suggestion_system

--- LeetcodeQuestions/Binary_Search/search_suggestion_system.py
+++ LeetcodeQuestions/Binary_Search/search_suggestion_system.py
@@ -1,31 +1,32 @@
-class Solution:
-    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        # You are given an array of strings products and a string searchWord.
 
-        # Design a system that suggests at most three product names from products after each character of searchWord is typed. Suggested products should have common prefix with searchWord. If there are more than three products with a common prefix return the three lexicographically minimums products.
+def suggestedProducts(products: List[str], searchWord: str) -> List[List[str]]:
+    # You are given an array of strings products and a string searchWord.
 
-        # Return a list of lists of the suggested products after each character of searchWord is typed.
+    # Design a system that suggests at most three product names from products after each character of searchWord is typed. Suggested products should have common prefix with searchWord. If there are more than three products with a common prefix return the three lexicographically minimums products.
 
-        #  The system needs to suggest not more than three product names from the list of products we are initially given
+    # Return a list of lists of the suggested products after each character of searchWord is typed.
 
-        #  We are looking for products that have a common prefix with the search_word that was given as well
+    #  The system needs to suggest not more than three product names from the list of products we are initially given
 
-        #  ex: products = [mousepad, microphone, macbook, iphone, chromebook, watch]
-        #  searchword = macbook
+    #  We are looking for products that have a common prefix with the search_word that was given as well
 
-        #  sort of stuck as to how i may approach the problem.
-        #  Thus far i am thinking of a prefix tree that can help us derive the common products to the search_word.
+    #  ex: products = [mousepad, microphone, macbook, iphone, chromebook, watch]
+    #  searchword = macbook
 
-        #  so the way we solve the problem will involve using a bit of binary search indirectly
-        # We first sort the list of products, now that it sorted we can use two pointers, a left at the start of the first character of the first word in the list, and the last pointer at the first character of the last word in the list. Now we just check whether the char[l] and char[r] are equal to the character we are currently looking at in the search_word.
-        #  If no then we move the left pointer to the next word in the products and repeat the this process until we have a match.
-        #  The same will apply for the right pointer
-        #  If the both the left and right pointer happen to match with the search word that means every other word in between also matches and we can simply return at least three products that are withing this range
+    #  sort of stuck as to how i may approach the problem.
+    #  Thus far i am thinking of a prefix tree that can help us derive the common products to the search_word.
 
-        res = []
-        products.sort()
-        print(products)
+    #  so the way we solve the problem will involve using a bit of binary search indirectly
+    # We first sort the list of products, now that it sorted we can use two pointers, a left at the start of the first character of the first word in the list, and the last pointer at the first character of the last word in the list. Now we just check whether the char[l] and char[r] are equal to the character we are currently looking at in the search_word.
+    #  If no then we move the left pointer to the next word in the products and repeat the this process until we have a match.
+    #  The same will apply for the right pointer
+    #  If the both the left and right pointer happen to match with the search word that means every other word in between also matches and we can simply return at least three products that are withing this range
 
-    products = ["mobile", "mouse", "moneypot", "monitor", "mousepad"]
-    searchWord = "mouse"
-    suggestedProducts(products, searchWord)
+    res = []
+    products.sort()
+    print(products)
+
+
+products = ["mobile", "mouse", "moneypot", "monitor", "mousepad"]
+searchWord = "mouse"
+suggestedProducts(products, searchWord)
