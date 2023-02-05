1d356665609402c2775431e0593868464c893f88
mdiallo98 <mamadou1237@gmail.com>
2023-02-04 23:52:15 -0500

    search_suggestion_system

--- LeetcodeQuestions/Binary_Search/search_suggestion_system.py
+++ LeetcodeQuestions/Binary_Search/search_suggestion_system.py
@@ -17,4 +17,7 @@ class Solution:
         #  Thus far i am thinking of a prefix tree that can help us derive the common products to the searchword.
 
         #  so the way we solve the problem will involve using a bit of binary search indirectly
-        # We first sort
+        # We first sort the list of products, now that it sorted we can use two pointers, a left at the start of the first character of the first word in the list, and the last pointer at the first character of the last word in the list. Now we just check wheter the char[l] and char[r] are equal to the character we are currently looking at in the searchword.
+        #  If no then we move the left pointer to the next word in the products and repeat the this process until we have a match.
+        #  The same will apply for the right pointer
+        #  If the both the left and right pointer happen to match with the search word that means
