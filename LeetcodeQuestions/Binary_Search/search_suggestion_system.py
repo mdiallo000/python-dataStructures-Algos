e836eaf13560d31ecc125ec55dfcdbc83151271d
mdiallo98 <mamadou1237@gmail.com>
2023-02-04 23:25:26 -0500

    search_suggestion_system

--- LeetcodeQuestions/Binary_Search/search_suggestion_system.py
+++ LeetcodeQuestions/Binary_Search/search_suggestion_system.py
@@ -1,2 +1,7 @@
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        # You are given an array of strings products and a string searchWord.
+
+        # Design a system that suggests at most three product names from products after each character of searchWord is typed. Suggested products should have common prefix with searchWord. If there are more than three products with a common prefix return the three lexicographically minimums products.
+
+        # Return a list of lists of the suggested products after each character of searchWord is typed.
