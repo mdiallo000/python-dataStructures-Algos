e1886a8c82161cb6cf4f5054b9839149b020eb74
mdiallo98 <mamadou1237@gmail.com>
2023-02-11 14:02:38 -0500

    valid_anagram

--- LeetcodeQuestions/Hash_Table_Dict/valid_anagram.py
+++ LeetcodeQuestions/Hash_Table_Dict/valid_anagram.py
@@ -2,6 +2,8 @@
 
 # An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 
+from collections import defaultdict
+
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
