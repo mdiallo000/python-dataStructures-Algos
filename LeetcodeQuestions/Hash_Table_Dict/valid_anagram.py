0ef30c2e9a47f6aade8c55d5cc5b21193ed053fa
mdiallo98 <mamadou1237@gmail.com>
2023-02-11 13:58:42 -0500

    valid_anagram

---
+++ LeetcodeQuestions/Hash_Table_Dict/valid_anagram.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        # an anagram is defined as two words that have an equal amount of characters at the same frequency
+        # So we can optimally solve this problem by getting a count of the two words and if they do then we have determined that these are anagrams of one another.
+        #  simple pythonic solution:
+        return Counter(s) == Counter(t)
+    #  More fleshed out solution:
+        t_count = defaultdict(int)
+        s_count = defaultdict(int)
+
+        for s_char, t_char in zip(s, t):
+            s_count[s_char] += 1
+            t_count[t_char] += 1
+
+        return s_count == t_count
+    #  both solution use O(n) extra space to get a count of the characters. Time complexity is Linear O(n)
