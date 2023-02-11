ab236c1eb0bd418d94330131ac4ccd8e61f636c3
mdiallo98 <mamadou1237@gmail.com>
2023-02-11 13:59:05 -0500

    valid_anagram

--- LeetcodeQuestions/Hash_Table_Dict/valid_anagram.py
+++ LeetcodeQuestions/Hash_Table_Dict/valid_anagram.py
@@ -1,3 +1,8 @@
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
 
