c5beb4acdef2775f367acb0e72cbc41cec7590a2
mdiallo98 <mamadou1237@gmail.com>
2023-02-14 21:00:11 -0500

    baseball game

--- LeetcodeQuestions/Stack/baseball_game.py
+++ LeetcodeQuestions/Stack/baseball_game.py
@@ -5,3 +5,22 @@ class Solution:
         #  ex [5,2,c,d,+]
         #   [5] => 2[5,2] => c[ 5] => d[ 10] => +[5] => answer should 5
         #  5 => [5] => -2 [5,-2] =>4 [5,-2,4] => c [5, -2 ] => d [5 ,-4] => 9 [5,-4,9] => + [5,-4, 9, 5, 14] => + [18] => answer should be 18
+        stack = []
+
+        for char in operations:
+            if stack:
+                if char == "C":
+                    stack.pop()
+                elif char == "D":
+                    c = int(stack[-1]) * 2
+                    stack.append(str(c))
+                elif char == "+" and len(stack) > 1:
+                    stack.append(str(int(stack[-2]) + int(stack[-1])))
+                else:
+                    stack.append(char)
+            else:
+                stack.append(char)
+        sum_total = 0
+        for c in stack:
+            sum_total += int(c)
+        return sum_total
