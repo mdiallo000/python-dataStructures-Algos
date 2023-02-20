13c0d34fcd8b8ceaaba5fa33a7d68cf153b78820
mdiallo98 <mamadou1237@gmail.com>
2023-02-20 03:31:28 -0500

    Number_of_Sub-arrays_of_Size_K_and_Average_Greater_than_or_Equal_to_Threshold

--- LeetcodeQuestions/SlidingWindow/Number_of_Sub-arrays_of_Size_K_and_Average_Greater_than_or_Equal_to_Threshold.py
+++ LeetcodeQuestions/SlidingWindow/Number_of_Sub-arrays_of_Size_K_and_Average_Greater_than_or_Equal_to_Threshold.py
@@ -2,8 +2,12 @@ class Solution:
     def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
         #  we want to return  the sub arrays of size k whos average greater than or equal to the threeshold
         #  since we are looking for sub-arrays we should either do a two pointer or a sliding window
-        window = set()
         res = 0
-        l = 0
+        curSum = sum(arr[:k-1])
 
-        for r in range(len(arr)):
+        for L in range(len(arr) - k + 1):
+            curSum += arr[L + k - 1]
+            if (curSum / k) >= threshold:
+                res += 1
+            curSum -= arr[L]
+        return res
