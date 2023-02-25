bf6c33f067a0d2888cdd3d288ac736cca1b5009f
mdiallo98 <mamadou1237@gmail.com>
2023-02-24 23:52:24 -0500

    degree_in_array

--- LeetcodeQuestions/Hash_Table_Dict/degree_in_array.py
+++ LeetcodeQuestions/Hash_Table_Dict/degree_in_array.py
@@ -2,3 +2,4 @@
 # Your task is to find the smallest possible length of a (contiguous) subarray of nums, that has the same degree as nums.
 class Solution:
     def findShortestSubArray(self, nums: List[int]) -> int:
+        #  we need to find a contigous portion of the array that will contain the same degree of elems
