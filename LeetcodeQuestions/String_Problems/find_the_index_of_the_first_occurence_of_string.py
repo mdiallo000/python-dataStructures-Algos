e0eae762e9d844381ed327e32f02aab3b80401ec
mdiallo98 <mamadou1237@gmail.com>
2023-03-03 23:57:46 -0500

    find_the_index_of_the_first_occurence_of_string

--- LeetcodeQuestions/String_Problems/find_the_index_of_the_first_occurence_of_string.py
+++ LeetcodeQuestions/String_Problems/find_the_index_of_the_first_occurence_of_string.py
@@ -11,3 +11,8 @@ class Solution:
         #  we are only interested in return the first occurence of sad which is index 0 rather than 6
 
         #  one edge case we can handle right away is if needle is ever larger than haystack in that case we should just return -1
+
+        if len(needle) > len(haystack):
+            return -1
+        # we are essentially trying to seee if the word needle exists inside of haystack. I think a trie may work here but it might be overkill for this probelm. Since with a trie we can do some string matching
+        #  the other idea that comes to mind is to use a sliding window and we can just check starting from each character if a window of len(needle) inside of haystack matches the word we are looking for
