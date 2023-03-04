d7ba242eb53627b51c8ab91601e0e056d95c172b
mdiallo98 <mamadou1237@gmail.com>
2023-03-03 23:54:16 -0500

    find_the_index_of_the_first_occurence_of_string

--- LeetcodeQuestions/String_Problems/find_the_index_of_the_first_occurence_of_string.py
+++ LeetcodeQuestions/String_Problems/find_the_index_of_the_first_occurence_of_string.py
@@ -3,3 +3,11 @@
 
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
+
+        # so we are getting two strings, needle and haystack
+        #  the goal is to find where needle will occur inside of haystrack
+        #  ie haystack = sadbustsad; needle = sad,
+        # `    here we see that there are two occurences of sad in haystack but w
+        #  we are only interested in return the first occurence of sad which is index 0 rather than 6
+
+        #  one edge case we can handle right away is if needle is ever larger than haystack in that case we should just return -1
