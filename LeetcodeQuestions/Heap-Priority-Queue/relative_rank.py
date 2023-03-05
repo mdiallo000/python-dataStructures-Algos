0c307e195a7a5ce3e69d9a5f3f5b3146e3a7d08a
mdiallo98 <mamadou1237@gmail.com>
2023-03-04 22:54:33 -0500

    relative_rank

--- LeetcodeQuestions/Heap-Priority-Queue/relative_rank.py
+++ LeetcodeQuestions/Heap-Priority-Queue/relative_rank.py
@@ -2,5 +2,6 @@ class Solution:
     def findRelativeRank(score):
 
         # medals =  ["Gold Medal", "Silver Medal"]
-
+        heap = []
         for i in range(len(score)):
+            heap.append([-score[i]], i)
