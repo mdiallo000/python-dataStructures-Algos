7540881e4f10b3c0b0ecb38d28527e70ce7148b6
mdiallo98 <mamadou1237@gmail.com>
2023-03-06 21:49:03 -0500

    delete_leaves_with_given_value

--- LeetcodeQuestions/Tree/delete_leaves_with_given_value.py
+++ LeetcodeQuestions/Tree/delete_leaves_with_given_value.py
@@ -5,6 +5,9 @@ class TreeNode:
         self.val = val
         self.left = None
         self.right = None
+# Given a binary tree root and an integer target, delete all the leaf nodes with value target.
+
+# Note that once you delete a leaf node with value target, if its parent node becomes a leaf node and has the value target, it should also be deleted (you need to continue doing that until you cannot).
 
 
 class Solution:
