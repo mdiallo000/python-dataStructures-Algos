73c869df53c0b5d27c3f2427418c5639ed4e6f35
mdiallo98 <mamadou1237@gmail.com>
2023-03-06 21:48:06 -0500

    delete_leaves_with_given_value

--- LeetcodeQuestions/Tree/delete_leaves_with_given_value.py
+++ LeetcodeQuestions/Tree/delete_leaves_with_given_value.py
@@ -1,3 +1,10 @@
+
+
+class TreeNode:
+    def __init__(self, val) -> None:
+        self.val = val
+
+
 class Solution:
     def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
         #  we need to recursively continue to delete nodes with the target value until we can no longer do so.
