2d93fb389dac04ceeded86b6a00cd8577d872a60
mdiallo98 <mamadou1237@gmail.com>
2023-03-08 19:42:12 -0500

    n_tree_preoder_traversal

--- LeetcodeQuestions/Tree/n_tree_preoder_traversal.py
+++ LeetcodeQuestions/Tree/n_tree_preoder_traversal.py
@@ -1,1 +1,12 @@
 class Solution:
+    def preOderNthTree(root):
+
+        res = []
+
+        def dfs(node):
+            if not node:
+                return
+
+            res.append(node.val)
+            for child in node.children:
+                dfs(child)
