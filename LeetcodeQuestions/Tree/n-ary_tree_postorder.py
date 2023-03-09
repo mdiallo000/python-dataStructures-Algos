1a8388b58774bff5ddaea66217c3d601f282a928
mdiallo98 <mamadou1237@gmail.com>
2023-03-08 19:56:25 -0500

    n-ary_tree_postorder

--- LeetcodeQuestions/Tree/n-ary_tree_postorder.py
+++ LeetcodeQuestions/Tree/n-ary_tree_postorder.py
@@ -1,6 +1,6 @@
 
 class Solution:
-    #     Given the root of an n-ary tree, return the postorder traversal of its nodes' values.
+    #     Given the root of an n-ary tree, return the post_order traversal of its nodes' values.
 
     # Nary-Tree input serialization is represented in their level order traversal. Each group of children is separated by the null value (See examples)
 
