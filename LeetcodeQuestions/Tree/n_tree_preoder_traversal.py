c2559f1bb00f978017294aa2a056283d8e11a7dc
mdiallo98 <mamadou1237@gmail.com>
2023-03-08 19:44:13 -0500

    n_tree_preoder_traversal

--- LeetcodeQuestions/Tree/n_tree_preoder_traversal.py
+++ LeetcodeQuestions/Tree/n_tree_preoder_traversal.py
@@ -3,7 +3,7 @@ class TreeNode:
         self.val = val
         self.children = children
 
-# Given the root of an n-ary tree, return the postorder traversal of its nodes' values.
+# Given the root of an n-ary tree, return the preorder traversal of its nodes' values.
 
 # Nary-Tree input serialization is represented in their level order traversal. Each group of children is separated by the null value (See examples)
 
@@ -12,12 +12,12 @@ class TreeNode:
 
 
 # Input: root = [1,null,3,2,4,null,5,6]
-# Output: [5,6,3,2,4,1]
+# Output: [1,3,5,6,2,4]
 # Example 2:
 
 
 # Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
-# Output: [2,6,14,11,7,3,12,8,4,13,9,10,5,1]
+# Output: [1,2,3,6,7,11,14,4,8,12,5,9,13,10]
 class Solution:
     def preOderNthTree(root):
 
