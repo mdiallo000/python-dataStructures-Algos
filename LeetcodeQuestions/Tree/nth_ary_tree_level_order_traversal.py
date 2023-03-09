0465ac78e7f67d80cf3733c97a38e332765368a3
mdiallo98 <mamadou1237@gmail.com>
2023-03-08 20:21:24 -0500

    nth_ary_tree_level_order_traversal

--- LeetcodeQuestions/Tree/nth_ary_tree_level_order_traversal.py
+++ LeetcodeQuestions/Tree/nth_ary_tree_level_order_traversal.py
@@ -6,3 +6,8 @@ class Solution:
     def levelOrder(root):
         res = []
         q = deque()
+        q.append(root)
+
+        while q:
+            size = len(q)
+            for _ in range(size):
