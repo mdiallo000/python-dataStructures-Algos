07d1753ad6157926dc63affcbdd5632b8649ff0f
mdiallo98 <mamadou1237@gmail.com>
2023-03-13 14:47:12 -0400

    minimun_path_sum

--- LeetcodeQuestions/Dynamic Programing/2-D dp/minimun_path_sum.py
+++ LeetcodeQuestions/Dynamic Programing/2-D dp/minimun_path_sum.py
@@ -1,5 +1,7 @@
 class Solution:
-
+    #  this is a typical dp problem, we need to find the path withing the grid that we generate the minimum sum
+    #  the constraint we have is that we can only travel from the top to left to the bottom right corner of the grid
+    #
     def minPath(grid):
         row, column = len(grid), len(grid[0])
         dp = [[0] * column for _ in range(row)]
