5b849da5c54657ddc0aab0ddb64119fffd643bd9
mdiallo98 <mamadou1237@gmail.com>
2023-03-20 21:53:00 -0400

    sort people

--- LeetcodeQuestions/Hash_Table_Dict/sort_people.py
+++ LeetcodeQuestions/Hash_Table_Dict/sort_people.py
@@ -3,3 +3,10 @@ class Solution:
     def sortPeople(self, heights, names):
 
         container = []
+
+        for i in range(len(names)):
+            container.append([heights[i], names[i]])
+
+        container = sorted(container, reverse=True)
+
+        res = []
