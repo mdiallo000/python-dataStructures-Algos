8b89e7e937b97f2a9a76a50243e115de160a1674
mdiallo98 <mamadou1237@gmail.com>
2023-03-21 22:20:34 -0400

    maximum_Number_of_Words_You_Can_Type

--- LeetcodeQuestions/Hash_Table_Dict/maximum_Number_of_Words_You_Can_Type.py
+++ LeetcodeQuestions/Hash_Table_Dict/maximum_Number_of_Words_You_Can_Type.py
@@ -1,3 +1,13 @@
+from collections import defaultdict
+
+
 class Solution:
 
     def canBeTypedWord(self, text, brokenLetters):
+
+        map_words = defaultdict(set)
+        text = text.split(" ")
+
+        for w in text:
+            for c in w:
+                map_words[w].add(c)
