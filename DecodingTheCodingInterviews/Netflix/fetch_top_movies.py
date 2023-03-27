63507d80ad612b55832ad1998f9ade5c7b1f4b17
mdiallo98 <mamadou1237@gmail.com>
2023-03-26 23:44:06 -0400

    fetch_top_movies

--- DecodingTheCodingInterviews/Netflix/fetch_top_movies.py
+++ DecodingTheCodingInterviews/Netflix/fetch_top_movies.py
@@ -1,1 +1,2 @@
 class Solution:
+    # We’ll be given n lists that are all sorted in ascending order of popularity rank. We have to combine these lists into a single list that will be sorted by rank in ascending order, meaning from best to worst.
