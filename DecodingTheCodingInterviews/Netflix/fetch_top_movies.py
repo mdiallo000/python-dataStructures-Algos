6a1d269f9ab7725ba8d1d2ce6f2a1c9a2556a0eb
mdiallo98 <mamadou1237@gmail.com>
2023-03-26 23:45:38 -0400

    fetch_top_movies

--- DecodingTheCodingInterviews/Netflix/fetch_top_movies.py
+++ DecodingTheCodingInterviews/Netflix/fetch_top_movies.py
@@ -1,2 +1,19 @@
 class Solution:
-    # We’ll be given n lists that are all sorted in ascending order of popularity rank. We have to combine these lists into a single list that will be sorted by rank in ascending order, meaning from best to worst.
+    #     # We’ll be given n lists that are all sorted in ascending order of popularity rank. We have to combine these lists into a single list that will be sorted by rank in ascending order, meaning from best to worst.
+    # Since our task involves multiple lists, you should divide the problem into multiple tasks, starting with the problem of combining two lists at a time. Then, you should combine the result of those first two lists with the third list, and so on, until the very last one is reached.
+
+    # Let’s discuss how we will implement this process:
+
+    # Consider the first list as the result, and store it in a variable.
+
+    # Traverse the list of lists, starting from the second list, and combine it with the list we stored as a result. The result should get stored in the same variable.
+
+    # When combining the two lists, like l1 and l2, maintain a prev pointer that points to a dummy node.
+
+    # If the value for list l1 is less than or equal to the value for list l2, connect the previous node to l1 and increment l1. Otherwise, do the same but for list l2.
+
+    # Keep repeating the above step until one list points to a null value.
+
+    # Connect the non-null list to the merged one and return.
+
+    # # Let’s look at the code for the solution below:
