5779262cf9262f9d0ed78904eb81591059df6895
mdiallo98 <mamadou1237@gmail.com>
2023-04-06 23:14:04 -0400

    left_and_right_sum_difference

--- LeetcodeQuestions/Array_Questions/left_and_right_sum_difference.py
+++ LeetcodeQuestions/Array_Questions/left_and_right_sum_difference.py
@@ -1,3 +1,12 @@
 class Solution:
 
     def sumDifference(nums):
+        # so we need to return a LIST of a similar size as nums that has the answer where answer[i] =  leftsum[i] -  rightsum[i]
+
+        #  ex [10,4,8,3]
+        # ex [15,1,11,22]
+
+        #       Input: nums = [10,4,8,3]
+        # Output: [15,1,11,22]
+        # Explanation: The array leftSum is [0,10,14,22] and the array rightSum is [15,11,3,0].
+        # The array answer is [|0 - 15|,|10 - 11|,|14 - 3|,|22 - 0|] = [15,1,11,22].
