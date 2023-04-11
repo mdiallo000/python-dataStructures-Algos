4d609878597afa23b8d93805f89e7bfa51da75cc
mdiallo98 <mamadou1237@gmail.com>
2023-04-10 23:54:54 -0400

    Array With Elements Not Equal to Average of Neighbors

---
+++ LeetcodeQuestions/Array_Questions/script.py
@@ -0,0 +1,11 @@
+import math as m
+
+x = int(input("Enter a number"))
+
+factorial = m.factorial(x)
+natural_log = m.log(x)
+base_ten_log = m.log10(x)
+
+print(factorial)
+print(natural_log)
+print(base_ten_log)
