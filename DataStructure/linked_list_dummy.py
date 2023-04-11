ca78f632c66aebe89538d7314f99b177478c7007
mdiallo98 <mamadou1237@gmail.com>
2023-04-11 00:20:15 -0400

    linked list implementation

---
+++ DataStructure/linked_list_dummy.py
@@ -0,0 +1,8 @@
+
+#  the goal is to create a linked list data structure as wells as the methods associated with it
+
+class Node:
+
+    def __init__(self) -> None:
+        self.val = val
+        self.next = None
