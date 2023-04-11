774eb4980346a8447f8ea53ebeb17d832c2595dc
mdiallo98 <mamadou1237@gmail.com>
2023-04-10 23:56:09 -0400

    Array With Elements Not Equal to Average of Neighbors

--- LeetcodeQuestions/TwoPointerProblems/array_With_Elements_Not_Equal_to_Average_of_Neighbors.py
+++ LeetcodeQuestions/TwoPointerProblems/array_With_Elements_Not_Equal_to_Average_of_Neighbors.py
@@ -5,3 +5,12 @@ class Solution:
         # More formally, the rearranged array should have the property such that for every i in the range 1 <= i < nums.length - 1, (nums[i-1] + nums[i+1]) / 2 is not equal to nums[i].
 
         # Return any rearrangement of nums that meets the requirement
+
+        # Example 1:
+
+        # Input: nums = [1,2,3,4,5]
+        # Output: [1,2,4,5,3]
+        # Explanation:
+        # When i=1, nums[i] = 2, and the average of its neighbors is (1+4) / 2 = 2.5.
+        # When i=2, nums[i] = 4, and the average of its neighbors is (2+5) / 2 = 3.5.
+        # When i=3, nums[i] = 5, and the average of its neighbors is (4+3) / 2 = 3.5.
