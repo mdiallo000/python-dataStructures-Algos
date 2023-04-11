64d58ffeedd6acd34cbecf6cf6529dfb464c4b21
mdiallo98 <mamadou1237@gmail.com>
2023-04-11 00:22:28 -0400

    linked list implementation

--- DataStructure/linked_list_dummy.py
+++ DataStructure/linked_list_dummy.py
@@ -2,7 +2,7 @@
 #  the goal is to create a linked list data structure as wells as the methods associated with it
 
 class Node:
-
-    def __init__(self) -> None:
+    #  the node will be the object that will be the foundational building block for our linked list data structure. It will hold info on the data it is storing within the val variable and a reference pointer to the next object in the chain
+    def __init__(self, val) -> None:
         self.val = val
         self.next = None
