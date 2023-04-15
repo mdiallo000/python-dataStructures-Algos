6adb09a633033ff021c4c8331391f16d3e75d02f
mdiallo98 <mamadou1237@gmail.com>
2023-04-15 00:58:09 -0400

    practice

--- index.py
+++ index.py
@@ -9,16 +9,22 @@ def bubbleSort(nums):
 
     for r in range(len(nums)):
 
-        for j in range(len(nums)):
-
-            if nums[r] > nums[j]:
-                nums[r], nums[j] = nums[j], nums[r]
-
+        for j in range(r-1):
+            print(r, j)
+            if nums[j] > nums[j+1]:
+                tmp = nums[j]
+                nums[j] = nums[j+1]
+                nums[j+1] = tmp
+        print(nums)
     return nums
 
 
 lst = [14, 7, 8, 12, 68, 45, 12, 32, 89, 41, 23]
 
-res = bubbleSort(lst)
+# res = bubbleSort(lst)
+
+
+def MergeSort(nums):
+
 
 print(res)
