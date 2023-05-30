4ee78557e063363d9e0e014cb8dd75e740ff50b6
mdiallo98 <mamadou1237@gmail.com>
2023-05-29 21:54:24 -0400

    sorting list using mergesort

---
+++ LeetcodeQuestions/linked_list/insertion_sort_in_a_list.py
@@ -0,0 +1,10 @@
+# Given the head of a singly linked list, sort the list using insertion sort, and return the sorted list's head.
+
+# The steps of the insertion sort algorithm:
+
+# Insertion sort iterates, consuming one input element each repetition and growing a sorted output list.
+# At each iteration, insertion sort removes one element from the input data, finds the location it belongs within the sorted list and inserts it there.
+# It repeats until no input elements remain.
+# The following is a graphical example of the insertion sort algorithm. The partially sorted list (black) initially contains only the first element in the list. One element (red) is removed from the input data and inserted in-place into the sorted list with each iteration
+class Solution:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
