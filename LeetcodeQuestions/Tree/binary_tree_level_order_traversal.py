548c2028a66c5e046b70eeb246d0c0b0543aac6e
mdiallo98 <mamadou1237@gmail.com>
2023-06-05 23:17:55 -0400

    level order traversal

--- LeetcodeQuestions/Tree/binary_tree_level_order_traversal.py
+++ LeetcodeQuestions/Tree/binary_tree_level_order_traversal.py
@@ -7,3 +7,5 @@ class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         #  the level order traversal of a binary is one of the essential traversal algorithms
         #  we will use a queue to simulate the level order traversal of the tree
+        #  first we will add the root node into the queue
+        #  once we began
