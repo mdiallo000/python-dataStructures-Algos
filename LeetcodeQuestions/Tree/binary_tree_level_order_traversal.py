285b6d305f998a625c23b55f58c05fb0e1b5711c
mdiallo98 <mamadou1237@gmail.com>
2023-06-05 23:22:34 -0400

    level order traversal

--- LeetcodeQuestions/Tree/binary_tree_level_order_traversal.py
+++ LeetcodeQuestions/Tree/binary_tree_level_order_traversal.py
@@ -8,4 +8,7 @@ class Solution:
         #  the level order traversal of a binary is one of the essential traversal algorithms
         #  we will use a queue to simulate the level order traversal of the tree
         #  first we will add the root node into the queue
-        #  once we began
+        #  we will run the traversal as long as there are items inside of the queue
+        #  we will iterate over the queue and pop nodes out
+        #  once we pop a node out will then check whether the node has a left and right child node. If it does we add the children node inside of the quue
+        #  the queue essenetially keeps the order of the nodes based  on their levels
