3daa3c2464ad208b0fd7ac231cd19c5d71e80010
mdiallo98 <mamadou1237@gmail.com>
2023-06-05 23:16:50 -0400

    level order traversal

--- LeetcodeQuestions/Tree/binary_tree_level_order_traversal.py
+++ LeetcodeQuestions/Tree/binary_tree_level_order_traversal.py
@@ -5,23 +5,5 @@ import collections
 
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        result = []
-        queue = collections.deque()
-        queue.append(root)
-        while queue:
-            #  measure the length of our queue currently, we will use this to loop through the elements at the current level only
-            lenght_queue = len(queue)
-            # at each level we will store the nodes into a list
-            levels = []
-            for i in range(lenght_queue):
-                #  now we store the node currently at the front of the queue by poping it out
-                curr_node = queue.popleft()
-            # Once we pop it out we can store it into the levels array and then we will add its left and child node into the queue
-                if curr_node is not None:
-                    levels.append(curr_node.val)
-                    queue.append(curr_node.left)
-                    queue.append(curr_node.right)
-            # now we can append the levels into our results array
-            if levels:
-                result.append(levels)
-        return result
+        #  the level order traversal of a binary is one of the essential traversal algorithms
+        #  we will use a queue to simulate the level order traversal of the tree
