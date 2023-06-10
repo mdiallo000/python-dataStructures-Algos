70c66445752acfc3541387962f86a2ecb39c4237
mdiallo98 <mamadou1237@gmail.com>
2023-06-09 20:52:20 -0400

    implementing spa

--- LeetcodeQuestions/Graphs/dijkstra_shortest_path_algorithm.py
+++ LeetcodeQuestions/Graphs/dijkstra_shortest_path_algorithm.py
@@ -1,2 +1,8 @@
 # Dijkstra's shortest path algorithm is one of the most famous algos out there
 # If you're given a graph with weighted edges and was tasked to find the shortest path from one node to another than dijkstra's algo is one of the best options you have
+#  the algo uses a minheap in order to continously pick the shortest path to the target destination
+
+import heapq
+
+
+def dijkstra()
