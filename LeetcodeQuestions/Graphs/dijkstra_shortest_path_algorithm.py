d03a5fb766ff8877c7bb4d3dfc5720cb1046dc46
mdiallo98 <mamadou1237@gmail.com>
2023-06-09 20:51:10 -0400

    implementing spa

---
+++ LeetcodeQuestions/Graphs/dijkstra_shortest_path_algorithm.py
@@ -0,0 +1,2 @@
+# Dijkstra's shortest path algorithm is one of the most famous algos out there
+# If you're given a graph with weighted edges and was tasked to find the shortest path from one node to another than dijkstra's algo is one of the best options you have
