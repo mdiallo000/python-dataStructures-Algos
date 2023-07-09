b2f8fdb161c0e45ae38a9366c4fb3f62653fee8f
mdiallo98 <mamadou1237@gmail.com>
2023-07-09 15:32:41 -0400

    seat reservation manager

---
+++ LeetcodeQuestions/Heap-Priority-Queue/seat_reservation_manager.py
@@ -0,0 +1,10 @@
+class SeatManager:
+
+    #  for this question what we want to do is use a heapq to simulate the behavior of the two functions that need to be implemented
+    #  the heapq will follow the min heap behavior
+
+    def __init__(self, n: int):
+
+    def reserve(self) -> int:
+
+    def unreserve(self, seatNumber: int) -> None:
